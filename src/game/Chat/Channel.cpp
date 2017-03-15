43452c0d9cdce841bef71c592a381c38fb68d37a
StadenElysium <StadenElysium@users.noreply.github.com>
2017-03-15 17:27:20 +0000

    License update (#27)

--- src/game/Chat/Channel.cpp
+++ src/game/Chat/Channel.cpp
@@ -1,6 +1,8 @@
 ﻿/*
  * Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>
  * Copyright (C) 2009-2011 MaNGOSZero <https://github.com/mangos/zero>
+ * Copyright (C) 2011-2016 Nostalrius <https://nostalrius.org>
+ * Copyright (C) 2016-2017 Elysium Project <https://github.com/elysium-project>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
