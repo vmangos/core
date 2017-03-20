db6c1794e8ba9640b8adabdfcdb00d2a006655aa
Gemt <skarsethkristian@gmail.com>
2017-03-20 21:50:00 +0100

    misc cthun sql updates. Dmg on giant tentacles, proper scaling on small claw and eye tentacle so code scaling is not necessary. Moved cthun slightly towards the door to reflect retail vanilla position

--- sql/AQ/CThun.sql
+++ sql/AQ/CThun.sql
@@ -11,10 +11,28 @@ UPDATE creature_template SET scale = 5 WHERE entry = 15334;
 UPDATE creature_template SET unit_flags = unit_flags | 32768 WHERE entry IN (15334, 15725, 15726, 15728, 15802);
 
   -- Bigger melee range for c'thun in p1
-  UPDATE `creature_model_info` set `combat_reach` = 15, `bounding_radius` = 15  where `modelid` = 15556;
+  UPDATE creature_model_info set `combat_reach` = 15, `bounding_radius` = 15  where `modelid` = 15556;
   
   -- flesh tentacle bigger melee-range (source: https://youtu.be/46yJL8eFxaQ?t=6m16s)
-  UPDATE `creature_model_info` set `combat_reach` = 10, `bounding_radius` = 10  where `modelid` = 15793;	
+  UPDATE creature_model_info set `combat_reach` = 10, `bounding_radius` = 10  where `modelid` = 15793;	
   
   -- Giant claw tentacle bigger melee-range 
-  UPDATE `creature_model_info` set `combat_reach` = 10, `bounding_radius` = 10  where `modelid` = 15790;	
+  UPDATE creature_model_info set `combat_reach` = 10, `bounding_radius` = 10  where `modelid` = 15790;	
+  
+  -- moved c'thun eye and body slightly towards entrance
+UPDATE creature SET `position_x`='-8572.290', `position_y`='1990.410' WHERE `guid`='87560';
+UPDATE creature SET `position_x`='-8572.290', `position_y`='1990.410' WHERE `guid`='87559';
+
+-- Set Giant claw dmg to around 2500 dmg per hit on a full t2 geared tank, with demo shout up. ~3200 without demo shout.
+UPDATE creature_template SET `mindmg`='8500', `maxdmg`='9500' WHERE `entry`='15728';
+
+-- Set Giant eye dmg to around 2500 dmg per hit on a full t2 geared tank, with demo shout up. ~3200 without demo shout.
+UPDATE creature_template SET `mindmg`='8500', `maxdmg`='9500', `baseattacktime`='2000' WHERE `entry`='15334';
+
+-- Setting correct scale of claw tentacle
+UPDATE creature_template SET `scale`='2' WHERE `entry`='15725';
+
+-- Setting correct scale of Eye Tentacle
+UPDATE creature_template SET `scale`='1' WHERE `entry`='15726';
+
+SELECT * from creature_template where entry = 15334;
