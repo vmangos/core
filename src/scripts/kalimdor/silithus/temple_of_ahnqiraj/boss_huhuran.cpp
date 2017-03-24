7a0ad98e7477e3f01dbcf082103de9be49a34671
Cgbd <mangos@ks359765.kimsufi.com>
2017-03-24 16:44:19 +0100

    Huhuran-s wyvernsting can only be casted on melees

--- src/scripts/kalimdor/silithus/temple_of_ahnqiraj/boss_huhuran.cpp
+++ src/scripts/kalimdor/silithus/temple_of_ahnqiraj/boss_huhuran.cpp
@@ -86,7 +86,7 @@ struct boss_huhuranAI : public ScriptedAI
         // Wyvern Timer
         if (Wyvern_Timer < diff)
         {
-            if (Unit *target = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+            if (Unit *target = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0, SPELL_WYVERNSTING, SELECT_FLAG_IN_MELEE_RANGE))
             {
                 if (DoCastSpellIfCan(target, SPELL_WYVERNSTING) == CAST_OK)
                     Wyvern_Timer = urand(15000, 32000);
