a3d875a26ae92246b6733b0de8268941e10b4723
Cgbd <mangos@ks359765.kimsufi.com>
2017-03-24 16:37:55 +0100

    Update Viscidus sql

--- sql/AQ/Viscidus.sql
+++ sql/AQ/Viscidus.sql
@@ -2,19 +2,20 @@
 UPDATE creature_template SET unit_flags = unit_flags | 33587968 WHERE entry = 15922;
 
 -- Viscidus trigger aren't targettable and can't fight players
-UPDATE creature_template SET unit_flags = unit_flags | (33554432 + 512), AIName = "NullAI" WHERE entry = 15922;
+UPDATE creature_template SET minlevel = 63, maxlevel = 63, unit_flags = unit_flags | (33554432 + 512), scriptname = "boss_viscidus_trigger" WHERE entry = 15922;
 
 -- Viscidus texts from cmangos
 DELETE FROM script_texts WHERE entry BETWEEN -1531046 AND -1531041;
 INSERT INTO script_texts (entry, content_default, content_loc1, content_loc2, content_loc3, content_loc4, content_loc5, content_loc6, content_loc7, content_loc8, sound, type, language, emote, comment) VALUES
-(-1531046, "%s explodes!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, "viscidus EMOTE_EXPLODE"),
-(-1531045, "%s looks ready to shatter!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, "viscidus EMOTE_SHATTER"),
-(-1531044, "%s begins to crack!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, "viscidus EMOTE_CRACK"),
-(-1531043, "%s is frozen solid!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, "viscidus EMOTE_FROZEN"),
-(-1531042, "%s is freezing up!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, "viscidus EMOTE_FREEZE"),
-(-1531041, "%s begins to slow!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 2, 0, 0, "viscidus EMOTE_SLOW");
+(-1531046, "%s explodes!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 3, 0, 0, "viscidus EMOTE_EXPLODE"),
+(-1531045, "%s looks ready to shatter!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 3, 0, 0, "viscidus EMOTE_SHATTER"),
+(-1531044, "%s begins to crack!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 3, 0, 0, "viscidus EMOTE_CRACK"),
+(-1531043, "%s is frozen solid!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 3, 0, 0, "viscidus EMOTE_FROZEN"),
+(-1531042, "%s is freezing up!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 3, 0, 0, "viscidus EMOTE_FREEZE"),
+(-1531041, "%s begins to slow!", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 3, 0, 0, "viscidus EMOTE_SLOW");
 
 -- Summon glob of Viscidus spell target position
+DELETE FROM spell_target_position WHERE id BETWEEN 25865 AND 25884;
 INSERT INTO spell_target_position (id , target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
 (25865, 531, -8023.59, 964.772, -41.229, 5.376),
 (25866, 531, -8042.13, 911.263, -42.841, 6.2),
@@ -36,3 +37,6 @@ INSERT INTO spell_target_position (id , target_map, target_position_x, target_po
 (25882, 531, -7942.98, 918.616, -46.401, 3.306),
 (25883, 531, -7964.32, 967.879, -42.112, 4.087),
 (25884, 531, -8015.24, 976.553, -41.647, 4.947);
+
+-- correct glob of Viscidus modelid and unit flags
+UPDATE creature_template SET modelid_1 = 12349, unit_flags = 32768 WHERE entry = 15667;
