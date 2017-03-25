bf0ef084086462aeecdbc5499b49cbbcfb3557cd
GuybrushGit <turinpt3@gmail.com>
2017-03-25 20:13:22 +0000

    Improvements to the despawn mechanics. (#114)
    * Added TEMPSUMMON_TIMED_COMBAT_OR_CORPSE_DESPAWN and TEMPSUMMON_TIMED_COMBAT_OR_DEAD_DESPAWN summon types.
    
    * Ignore combat state when despawning summons by SPELL_EFFECT_SUMMON_WILD and SPELL_EFFECT_SUMMON_DEMON.
    
    * Prevent despawning when mob is in combat or recently died

--- src/game/Objects/Object.h
+++ src/game/Objects/Object.h
@@ -56,14 +56,17 @@
 
 enum TempSummonType
 {
-    TEMPSUMMON_TIMED_OR_DEAD_DESPAWN       = 1,             // despawns after a specified time OR when the creature disappears
-    TEMPSUMMON_TIMED_OR_CORPSE_DESPAWN     = 2,             // despawns after a specified time OR when the creature dies
-    TEMPSUMMON_TIMED_DESPAWN               = 3,             // despawns after a specified time
-    TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT = 4,             // despawns after a specified time after the creature is out of combat
-    TEMPSUMMON_CORPSE_DESPAWN              = 5,             // despawns instantly after death
-    TEMPSUMMON_CORPSE_TIMED_DESPAWN        = 6,             // despawns after a specified time after death
-    TEMPSUMMON_DEAD_DESPAWN                = 7,             // despawns when the creature disappears
-    TEMPSUMMON_MANUAL_DESPAWN              = 8              // despawns when UnSummon() is called
+    TEMPSUMMON_TIMED_OR_DEAD_DESPAWN          = 1,             // despawns after a specified time (out of combat) OR when the creature disappears
+    TEMPSUMMON_TIMED_OR_CORPSE_DESPAWN        = 2,             // despawns after a specified time (out of combat) OR when the creature dies
+    TEMPSUMMON_TIMED_DESPAWN                  = 3,             // despawns after a specified time
+    TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT    = 4,             // despawns after a specified time after the creature is out of combat
+    TEMPSUMMON_CORPSE_DESPAWN                 = 5,             // despawns instantly after death
+    TEMPSUMMON_CORPSE_TIMED_DESPAWN           = 6,             // despawns after a specified time after death
+    TEMPSUMMON_DEAD_DESPAWN                   = 7,             // despawns when the creature disappears
+    TEMPSUMMON_MANUAL_DESPAWN                 = 8,             // despawns when UnSummon() is called
+    TEMPSUMMON_TIMED_COMBAT_OR_DEAD_DESPAWN   = 9,             // despawns after a specified time (in or out of combat) OR when the creature disappears
+    TEMPSUMMON_TIMED_COMBAT_OR_CORPSE_DESPAWN = 10,            // despawns after a specified time (in or out of combat) OR when the creature dies
+
 };
 
 class WorldPacket;
