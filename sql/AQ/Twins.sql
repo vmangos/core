b3477af33c883ba64fd952319a07640a0511a88c
Gemt <skarsethkristian@gmail.com>
2017-04-01 16:33:49 +0200

    spell_mod change twins teleport stun duration to 1.5sec

--- sql/AQ/Twins.sql
+++ sql/AQ/Twins.sql
@@ -4,3 +4,6 @@ UPDATE `creature` SET `position_z`='-102.44', `orientation`='4.896' WHERE `guid`
 
 -- Added ScriptName, made unit non-attackable and experimented with some other flags for floating, but they should be back to what they were.
 UPDATE `creature_template`  SET `unit_flags`=(2) , `AIName`='EventAI', `MovementType` = '0',`InhabitType` = '3', `ScriptName`='mob_masters_eye' WHERE `entry`='15963';
+
+DELETE FROM spell_mod WHERE Id = 800;
+INSERT INTO spell_mod (`Id`, `DurationIndex`,`Comment`) VALUES ('800', '65','Spell twins teleport stun 1.5sec');
