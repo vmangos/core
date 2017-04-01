980e8f97ca95349f5a8461e8aeabadb8a082ffc4
brotalnia <brotalnia@gmail.com>
2017-04-01 14:46:32 +0300

    Hide buff from Tier 2 set. (#225)
    * hide priest tier 2 set 8 piece bonus buff
    
    * hide mage tier 2 set 8 piece bonus

---
+++ sql/migrations/20170331234934_world.sql
@@ -0,0 +1,7 @@
+INSERT INTO `migrations` VALUES ('20170331234934'); 
+
+-- Hide the buff from the Priest tier 2 set 8 piece bonus.
+UPDATE `spell_mod` SET `SpellVisual`=0, `Comment`='Priest Tier 2 Set 8 Piece Bonus' WHERE `Id`=22010;
+
+-- Hide the buff from the Mage tier 2 set 8 piece bonus.
+INSERT INTO `spell_mod` (`Id`, `procChance`, `procFlags`, `procCharges`, `DurationIndex`, `Category`, `CastingTimeIndex`, `StackAmount`, `SpellIconID`, `activeIconID`, `manaCost`, `Attributes`, `AttributesEx`, `AttributesEx2`, `AttributesEx3`, `AttributesEx4`, `Custom`, `InterruptFlags`, `AuraInterruptFlags`, `ChannelInterruptFlags`, `Dispel`, `Stances`, `StancesNot`, `SpellVisual`, `ManaCostPercentage`, `StartRecoveryCategory`, `StartRecoveryTime`, `MaxAffectedTargets`, `MaxTargetLevel`, `DmgClass`, `rangeIndex`, `RecoveryTime`, `CategoryRecoveryTime`, `SpellFamilyName`, `SpellFamilyFlags`, `Comment`) VALUES (22007, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 'Mage Tier 2 Set 8 Piece Bonus');
