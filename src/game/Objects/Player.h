e2bfaa7a822704cee127b4b8c6274d57f09067e2
eir <m-laniakea@users.noreply.github.com>
2017-04-04 20:06:04 -0700

    Scepter Blue: Implement Azuregos' Ledger Quest (#272)

--- src/game/Objects/Player.h
+++ src/game/Objects/Player.h
@@ -1021,7 +1021,7 @@ class MANGOS_DLL_SPEC Player final: public Unit
         /***                    STORAGE SYSTEM                 ***/
         /*********************************************************/
 
-        Item* AddItem(uint32 itemId, uint32 count);
+        Item* AddItem(uint32 itemId, uint32 count = 1);
 
         void InterruptSpellsWithCastItem(Item* item);
         void SetVirtualItemSlot( uint8 i, Item* item);
