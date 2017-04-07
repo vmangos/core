04250ace68395c8e28bacf02d60444d05a2693ab
Staden <bcr4kfwebgnnon@yandex.com>
2017-04-07 23:14:39 +0100

    Fix bad migrations

--- sql/migrations/20170405160849_world.sql
+++ sql/migrations/20170405160849_world.sql
@@ -1,4 +1,4 @@
 INSERT INTO `migrations` VALUES ('20170405160849');
 
 -- Little Timmy now has one White Kitten on a three hour restock
-update world.npc_vendor set maxcount = 1, incrtime = 10800 where entry = 8666 and item = 8489; 
+update npc_vendor set maxcount = 1, incrtime = 10800 where entry = 8666 and item = 8489; 
