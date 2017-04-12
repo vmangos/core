84c049db4191488f1eba99f03db534d0fd4f1faf
brotalnia <brotalnia@gmail.com>
2017-04-12 04:21:57 +0300

    fixed wrong column name (#373)

--- sql/migrations/20170410044436_world.sql
+++ sql/migrations/20170410044436_world.sql
@@ -1,7 +1,7 @@
 INSERT INTO `migrations` VALUES ('20170410044436'); 
 
 -- Fix Thundering Charm, Burning Charm and Cresting Charm
-UPDATE `spell_effect_mod` SET `Effect`=1 WHERE `entry`=4131;
+UPDATE `spell_effect_mod` SET `Effect`=1 WHERE `Id`=4131;
 INSERT INTO `spell_effect_mod` (`Id`, `EffectIndex`, `Effect`, `EffectDieSides`, `EffectBaseDice`, `EffectDicePerLevel`, `EffectRealPointsPerLevel`, `EffectBasePoints`, `EffectAmplitude`, `EffectPointsPerComboPoint`, `EffectChainTarget`, `EffectMultipleValue`, `EffectMechanic`, `EffectImplicitTargetA`, `EffectImplicitTargetB`, `EffectRadiusIndex`, `EffectApplyAuraName`, `EffectItemType`, `EffectMiscValue`, `EffectTriggerSpell`, `Comment`) VALUES (4130, 0, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 38, 0, -1, -1, -1, 0, -1, 'Arathi Cresting Charm');
 INSERT INTO `spell_effect_mod` (`Id`, `EffectIndex`, `Effect`, `EffectDieSides`, `EffectBaseDice`, `EffectDicePerLevel`, `EffectRealPointsPerLevel`, `EffectBasePoints`, `EffectAmplitude`, `EffectPointsPerComboPoint`, `EffectChainTarget`, `EffectMultipleValue`, `EffectMechanic`, `EffectImplicitTargetA`, `EffectImplicitTargetB`, `EffectRadiusIndex`, `EffectApplyAuraName`, `EffectItemType`, `EffectMiscValue`, `EffectTriggerSpell`, `Comment`) VALUES (4132, 0, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 38, 0, -1, -1, -1, 0, -1, 'Arathi Burning Charm');
 
