ea46c4b1a9d1a5d1dfabc86ae1250d374b4c9150
Staden <bcr4kfwebgnnon@yandex.com>
2017-04-14 00:38:16 +0100

    Viscidus: Should not be tauntable

--- sql/AQ/Viscidus.sql
+++ sql/AQ/Viscidus.sql
@@ -42,3 +42,6 @@ UPDATE creature_template SET MechanicImmuneMask = 617299803 WHERE entry = 15667;
 
 -- Viscidus teleport spell
 INSERT INTO spell_target_position (id, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES (25904, 531, -7992.36, 908.19, -52.62, 1.68);
+
+-- Not tauntable
+UPDATE creature_template SET flags_extra = 257 WHERE entry = 15299;
