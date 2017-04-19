075e9945aa1423a250b518fc6c3b19c9ebe2cf98
Gemt <skarsethkristian@gmail.com>
2017-04-19 17:51:25 +0200

    swapped event_param1 and event_param2 in creature_ai-scripts for Anubisath Defenders. Was incorrect

---
+++ sql/AQ/Trash.sql
@@ -0,0 +1,8 @@
+UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527732';
+UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527728';
+UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527724';
+UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527720';
+UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527716';
+UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527712';
+UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527708';
+UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527704';
