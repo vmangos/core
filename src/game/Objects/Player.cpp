938d6e0c6f7ebd973557c058bad187e1c7b95e08
Gemt <skarsethkristian@gmail.com>
2017-04-19 22:07:07 +0200

    formatting

--- src/game/Objects/Player.cpp
+++ src/game/Objects/Player.cpp
@@ -5992,7 +5992,7 @@ int32 Player::CalculateReputationGain(ReputationSource source, int32 rep, int32 
         percent *= repRate;
     }
 
-    return int32(sWorld.getConfig(CONFIG_FLOAT_RATE_REPUTATION_GAIN) * rep * percent / 100.0f);
+    return int32(sWorld.getConfig(CONFIG_FLOAT_RATE_REPUTATION_GAIN) * rep * percent / 100.0f); 
 }
 
 //Calculates how many reputation points player gains in victim's enemy factions
