b372ba1cefe8cbf7422653c7739fdb8ec39d635d
Staden <bcr4kfwebgnnon@yandex.com>
2017-04-25 21:47:55 +0100

    Trash: Detect invis on mobs based on submitted data

--- sql/AQ/Trash.sql
+++ sql/AQ/Trash.sql
@@ -7,3 +7,8 @@ UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WH
 UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527712';
 UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527708';
 UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527704';
+
+-- Add detect invis/stealth aura
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES ('15246', '0', '0', '1', '16', '0', '0', '18950');
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES ('15247', '0', '0', '1', '16', '0', '0', '18950');
+INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES ('15233', '0', '0', '1', '16', '0', '0', '18950');
