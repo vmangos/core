de154ada9027348e2bbe7dd2ffedfe0ef64a8d74
Staden <bcr4kfwebgnnon@yandex.com>
2017-04-25 22:09:58 +0100

    Trash: Trash that pulls together stays together

--- sql/AQ/Trash.sql
+++ sql/AQ/Trash.sql
@@ -12,3 +12,8 @@ UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WH
 INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES ('15246', '0', '0', '1', '16', '0', '0', '18950');
 INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES ('15247', '0', '0', '1', '16', '0', '0', '18950');
 INSERT INTO `creature_template_addon` (`entry`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES ('15233', '0', '0', '1', '16', '0', '0', '18950');
+
+-- Position adjustments
+UPDATE `creature` SET `position_y`='1759.77' WHERE `guid`='88027';
+UPDATE `creature` SET `position_y`='1780.08', `position_z`='-42.6571' WHERE `guid`='88024';
+UPDATE `creature` SET `position_x`='-900.787109', `position_y`='1916.039063', `position_z`='-43.814262' WHERE `guid`='88063';
