fcc32dec04d582a31b3db184914150cce77c9c74
Gemt <skarsethkristian@gmail.com>
2017-04-25 20:56:02 +0200

    comment added in trash.sql

--- sql/AQ/Trash.sql
+++ sql/AQ/Trash.sql
@@ -1,3 +1,4 @@
+-- Meteors for anubisath defenders had event_param1 and 2 swapped
 UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527732';
 UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527728';
 UPDATE `creature_ai_scripts` SET `event_param1`='5000', `event_param2`='6000' WHERE `id`='1527724';
