dfab3607c969e696be5e147f114065d9f27059a8
Staden <bcr4kfwebgnnon@yandex.com>
2017-04-30 20:08:00 +0100

    C'Thun: Update P1 claw timer based on new data

--- src/scripts/kalimdor/silithus/temple_of_ahnqiraj/boss_cthun.cpp
+++ src/scripts/kalimdor/silithus/temple_of_ahnqiraj/boss_cthun.cpp
@@ -280,7 +280,7 @@ static constexpr int32  MAX_INITIAL_PULLER_HITS         = 3;     // How many tim
                                                                  // puller with green beam before random target.
 static constexpr int32  P1_GREEN_BEAM_COOLDOWN          = 3000;  // Green beam has a 2 sec cast time. If this number is > 2000, 
                                                                  // the cooldown will be P1_GREEN_BEAM_COOLDOWN - 2000
-static const uint32 clawTentacleSpanCooldownFunc()      { return urand(6000, 12000); }
+static const uint32 P1_CLAW_TENTACLE_RESPAWN_TIMER      = 5000;  // checked against old footage & current fight
 // =======================================================
 
 // ================= TRANSITION CONSTANTS ================
@@ -1328,7 +1328,7 @@ struct cthunAI : public ScriptedAI
             return;
         
         cthunEmergeTimer        = CTHUN_EMERGE_ANIM_DURATION;
-        clawTentacleTimer_p1    = clawTentacleSpanCooldownFunc();
+        clawTentacleTimer_p1    = P1_CLAW_TENTACLE_RESPAWN_TIMER;
         eyeTentacleTimer_p1     = P1_EYE_TENTACLE_RESPAWN_TIMER;
 
         ResetartUnvulnerablePhase(false);
@@ -1608,7 +1608,7 @@ struct cthunAI : public ScriptedAI
 
     void UpdateTentaclesP1(uint32 diff) {
         if (SpawnTentacleIfReady(diff, clawTentacleTimer_p1, 0, MOB_CLAW_TENTACLE)) {
-            clawTentacleTimer_p1 = clawTentacleSpanCooldownFunc();
+            clawTentacleTimer_p1 = P1_CLAW_TENTACLE_RESPAWN_TIMER;
         }
 
         if (eyeTentacleTimer_p1 < diff) {
