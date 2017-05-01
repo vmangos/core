3404ac31f50f9b09e1b83665304b08466b35d84c
Staden <bcr4kfwebgnnon@yandex.com>
2017-05-01 18:32:41 +0100

    C'Thun: Make tentacles immune to blind too

--- sql/AQ/CThun.sql
+++ sql/AQ/CThun.sql
@@ -86,4 +86,4 @@ UPDATE creature_template SET flags_extra = flags_extra & ~512 WHERE entry = 1572
 UPDATE creature_template SET flags_extra = flags_extra | 256 where entry in (15589, 15727);
 
 -- Fix mechanic immunities for adds
-UPDATE creature_template SET MechanicImmuneMask = MechanicImmuneMask | (16 | 4096 | 512) WHERE entry IN (15728, 15334, 15725, 15726, 15802);
+UPDATE creature_template SET MechanicImmuneMask = MechanicImmuneMask | (2 | 16 | 4096 | 512) WHERE entry IN (15728, 15334, 15725, 15726, 15802);
