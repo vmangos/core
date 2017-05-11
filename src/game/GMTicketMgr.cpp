4341a75388b084710d89439d84a74cdee566a39d
Faruk Mašinović <paytime@users.noreply.github.com>
2017-05-11 22:04:35 +0200

    Fixed some typos and give the players the possibility to create ticket without any confusion. (#678)
    Each time, a player creates a new ticket, he will receive a note about
    GM's and some useful links. The only Stuck/Harassment option has been
    removed as it confuses most players and thus causes more work for the
    support than it should(like constantly telling them on how to actually
    make a ticket etc.).

--- src/game/GMTicketMgr.cpp
+++ src/game/GMTicketMgr.cpp
@@ -122,7 +122,7 @@ void GmTicket::WritePacket(WorldPacket& data) const
     if (IsCompleted())
     {
         displayedMessage << "\n\n-----------------------------------------------------------------------\n";
-        displayedMessage << "Nostalrius customer ticket #" << GetId() << " completed.\n";
+        displayedMessage << "Elysium customer ticket #" << GetId() << " completed.\n";
         if (!_response.empty())
         {
             displayedMessage << "\n    GM answer:\n";
