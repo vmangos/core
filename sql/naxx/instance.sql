698b1475e25c689c290231fac4d8f820b6d18c17
Gemt <skarsethkristian@gmail.com>
2017-05-14 00:23:56 +0200

    slightly moved anubRekhans door to avoid depthbuffer fighting causing door be visible through the walls on the sides when player standing far away

--- sql/naxx/instance.sql
+++ sql/naxx/instance.sql
@@ -1,4 +1,5 @@
 -- Anub'rekhan door
-INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `spawnFlags`) VALUES ('533000', '181126', '533', '3202.7', '-3476.32', '287', '3.14159', '0', '0', '0', '0', '6380', '100', '1', '0');
+DELETE from`gameobject` WHERE id = 181126;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `spawnFlags`) VALUES ('533000', '181126', '533', '3202.60', '-3476.32', '287', '3.14159', '0', '0', '0', '0', '6380', '100', '1', '0');
 -- script added to anub'rekhans door so we can do anubs welcome msg in onUse for the door
 UPDATE `gameobject_template` SET `ScriptName`='go_anub_door' WHERE `entry`='181126';
