03dc4cf98a5153d28d5929986d897e56d32718cf
Gemt <skarsethkristian@gmail.com>
2017-05-13 11:37:57 +0200

    anubrekhan door added

---
+++ sql/naxx/instance.sql
@@ -0,0 +1,2 @@
+-- Anub'rekhan door
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `spawnFlags`) VALUES ('533000', '181126', '533', '3202.7', '-3476.32', '287', '3.14159', '0', '0', '0', '0', '6380', '100', '1', '0');
