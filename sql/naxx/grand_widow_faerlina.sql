f20ac44fe754b358499e0ed146bfe4b81064d449
Gemt <skarsethkristian@gmail.com>
2017-05-14 15:32:25 +0200

    Faerlina: Worshippers and Followers removed from creature table. Instead spawned from Faerlinas script. creature_template for Worshippers now has widows embrace spell, allowing players who MC worshippers to use that ability. When Faerlina is hit by Widows Embrace, she stops casting poison volley for 30 seconds and removes enrage from herself. Various timer updates with research.

--- sql/naxx/grand_widow_faerlina.sql
+++ sql/naxx/grand_widow_faerlina.sql
@@ -1,2 +1,9 @@
 -- Set her base attackspeed to 2.5sec, based on https://www.youtube.com/watch?v=pVjB7pCX3XM
 UPDATE `creature_template` SET `baseattacktime`=2500 WHERE `entry`=15953;
+
+-- remove worshippers and followers. They are spawned by faerlina
+DELETE FROM `creature` WHERE `id`='16505';
+DELETE FROM `creature` WHERE `id`='16506';
+
+-- give Worshippers widows embrace
+UPDATE `creature_template` SET `spell1`=28732 WHERE `entry`=16506;
