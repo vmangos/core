vmangos/core

commit 1b76fc7ee7620a1cebe4607f9106e8a950e60fc6

Fixes to Ritual of Summoning. (#541)
* Set warlock summons to use the player targeted at spell cast

* Summoning Portal click animation

* Summoning Portal click animation

* Allow summoning flying targets

* Newline

src/game/Objects/GameObject.cpp
@@ -68,6 +68,7 @@ GameObject::GameObject() : WorldObject(),
     m_model = NULL;
     m_rotation = 0;
     m_playerGroupId = 0;
+    m_summonTarget = ObjectGuid();
 }
 
 GameObject::~GameObject()
@@ -1524,6 +1525,18 @@ void GameObject::Use(Unit* user)
             if (owner)
                 owner->FinishSpell(CURRENT_CHANNELED_SPELL);
 
+            // finish clickers spell
+            for (GuidsSet::const_iterator itr = m_UniqueUsers.begin(); itr != m_UniqueUsers.end(); ++itr)
+            {
+                if (Player* user = GetMap()->GetPlayer(*itr))
+                    if (user && user != owner)
+                    {
+                        Spell *channeled = user->GetCurrentSpell(CURRENT_CHANNELED_SPELL);
+                        if (channeled && channeled->m_spellInfo->Id == info->summoningRitual.animSpell)
+                            user->FinishSpell(CURRENT_CHANNELED_SPELL);
+                    }
+            }
+
             // can be deleted now, if
             if (!info->summoningRitual.ritualPersistent)
                 SetLootState(GO_JUST_DEACTIVATED);
@@ -1766,9 +1779,18 @@ void GameObject::Use(Unit* user)
 
     Spell *spell = new Spell(spellCaster, spellInfo, triggered, GetObjectGuid());
 
-    // spell target is user of GO
+    // spell target is user of GO 
     SpellCastTargets targets;
-    targets.setUnitTarget(user);
+
+    // If summoning ritual GO use the summon target instead
+    Player* summonTarget = nullptr;
+    if (GetGoType() == GAMEOBJECT_TYPE_SUMMONING_RITUAL && getSummonTarget())
+        summonTarget = sObjectMgr.GetPlayer(getSummonTarget());
+
+    if (summonTarget)
+        targets.setUnitTarget(summonTarget);
+    else
+        targets.setUnitTarget(user);
 
     spell->prepare(&targets);
 }

sql/migrations/20170428051708_world.sql
@@ -1,0 +1,4 @@
+INSERT INTO `migrations` VALUES ('20170428051708');
+
+-- Summoning Portal click animation
+UPDATE gameobject_template SET data2 = 23017 WHERE entry = 36727;

src/game/Objects/GameObject.h
@@ -706,6 +706,10 @@ class MANGOS_DLL_SPEC GameObject : public WorldObject
 
         bool IsVisible() const { return m_visible; }
         void SetVisible(bool b);
+
+        // Used for GAMEOBJECT_TYPE_SUMMONING_RITUAL
+        ObjectGuid getSummonTarget() const { return m_summonTarget; }
+        void SetSummonTarget(ObjectGuid o) { m_summonTarget = o; }
     protected:
         bool        m_visible;
         uint32      m_spellId;
@@ -725,6 +729,7 @@ class MANGOS_DLL_SPEC GameObject : public WorldObject
         // collected only for GAMEOBJECT_TYPE_SUMMONING_RITUAL
         ObjectGuid m_firstUser;                             // first GO user, in most used cases owner, but in some cases no, for example non-summoned multi-use GAMEOBJECT_TYPE_SUMMONING_RITUAL
         GuidsSet m_UniqueUsers;                             // all players who use item, some items activated after specific amount unique uses
+        ObjectGuid m_summonTarget;                          // The player who is being summoned
 
         uint64 m_rotation;
         GameObjectInfo const* m_goInfo;

src/game/Spells/Spell.cpp
@@ -2680,9 +2680,8 @@ void Spell::SetTargetMap(SpellEffectIndex effIndex, uint32 targetMode, UnitList&
                         targetUnitMap.push_back(m_caster);
                     break;
                 case SPELL_EFFECT_SUMMON_PLAYER:
-                    if (m_caster->GetTypeId() == TYPEID_PLAYER && ((Player*)m_caster)->GetSelectionGuid())
-                        if (Player* target = sObjectMgr.GetPlayer(((Player*)m_caster)->GetSelectionGuid()))
-                            targetUnitMap.push_back(target);
+                    if (m_targets.getUnitTarget())
+                        targetUnitMap.push_back(m_targets.getUnitTarget());
                     break;
                 case SPELL_EFFECT_RESURRECT_NEW:
                     if (m_targets.getUnitTarget())
@@ -4712,8 +4711,8 @@ SpellCastResult Spell::CheckCast(bool strict)
 
         if (non_caster_target)
         {
-            // Not allow casting on flying player
-            if (target->IsTaxiFlying())
+            // Not allow casting on flying player unless its a ritual of summoning
+            if (target->IsTaxiFlying() && m_spellInfo->Id != 7720)
                 return SPELL_FAILED_BAD_TARGETS;
 
             if (!m_IsTriggeredSpell)
@@ -5589,10 +5588,12 @@ SpellCastResult Spell::CheckCast(bool strict)
             {
                 if (m_caster->GetTypeId() != TYPEID_PLAYER)
                     return SPELL_FAILED_BAD_TARGETS;
-                if (!((Player*)m_caster)->GetSelectionGuid())
+                if (!m_targets.getUnitTarget())
+                    return SPELL_FAILED_BAD_TARGETS;
+                if (!m_targets.getUnitTarget()->IsPlayer())
                     return SPELL_FAILED_BAD_TARGETS;
 
-                Player* target = sObjectMgr.GetPlayer(((Player*)m_caster)->GetSelectionGuid());
+                Player* target = (Player*)m_targets.getUnitTarget();
                 if (!target || ((Player*)m_caster) == target || !target->IsInSameRaidWith((Player*)m_caster))
                     return SPELL_FAILED_BAD_TARGETS;
 

src/game/Spells/SpellEffects.cpp
@@ -5646,6 +5646,10 @@ void Spell::EffectTransmitted(SpellEffectIndex eff_idx)
         {
             if (m_caster->GetTypeId() == TYPEID_PLAYER)
             {
+                // Set the summoning target
+                if (m_caster->GetTypeId() == TYPEID_PLAYER && ((Player*)m_caster)->GetSelectionGuid())
+                    pGameObj->SetSummonTarget(((Player*)m_caster)->GetSelectionGuid());
+
                 pGameObj->AddUniqueUse((Player*)m_caster);
                 m_caster->AddGameObject(pGameObj);          // will removed at spell cancel
             }

commit df958d338c9a7bd48679f7367717f39641a081cb

Uldaman altar event left broken after #541 (#745)
* add gohello function

* check ritualPersistent before finish uses

src/game/Objects/GameObject.cpp
@@ -1520,7 +1520,7 @@ void GameObject::Use(Unit* user)
             for (GuidsSet::const_iterator itr = m_UniqueUsers.begin(); itr != m_UniqueUsers.end(); ++itr)
             {
                 if (Player* user = GetMap()->GetPlayer(*itr))
-                    if (user && user != owner)
+                    if (user && user != owner && !info->summoningRitual.ritualPersistent)
                     {
                         Spell *channeled = user->GetCurrentSpell(CURRENT_CHANNELED_SPELL);
                         if (channeled && channeled->m_spellInfo->Id == info->summoningRitual.animSpell)

src/scripts/eastern_kingdoms/uldaman/boss_archaedas.cpp
@@ -383,6 +383,20 @@ SDComment: Needs 1 person to activate the Archaedas script
 SDCategory: Uldaman
 EndScriptData */
 
+
+bool GOHello_go_altar_of_archaedas(Player* pPlayer, GameObject* pGo)
+{
+    ScriptedInstance* pInstance = (ScriptedInstance*)pGo->GetInstanceData();
+
+    if (!pInstance)
+        return false;
+
+    if (pPlayer)
+        pPlayer->CastSpell(nullptr, SPELL_ALTAR_SUMMONING_VISUAL, true);
+
+    return true;
+}
+
 class go_altar_of_archaedasAI : public GameObjectAI
 {
 public:
@@ -500,5 +514,6 @@ void AddSC_boss_archaedas()
     newscript = new Script;
     newscript->Name = "go_altar_of_archaedas";
     newscript->GOGetAI = &GetAI_go_altar_of_archaedas;
+    newscript->pGOHello = &GOHello_go_altar_of_archaedas;
     newscript->RegisterSelf();
 }

src/scripts/eastern_kingdoms/uldaman/uldaman.cpp
@@ -280,6 +280,19 @@ bool OnTrigger_at_map_chamber(Player* pPlayer, const AreaTriggerEntry *at)
     return true;
 }
 
+bool GOHello_go_altar_of_the_keepers(Player* pPlayer, GameObject* pGo)
+{
+    ScriptedInstance* pInstance = (ScriptedInstance*)pGo->GetInstanceData();
+
+    if (!pInstance)
+        return false;
+
+    if (pPlayer)
+        pPlayer->CastSpell(nullptr, SPELL_ALTAR_SUMMONING_VISUAL, true);
+
+    return true;
+}
+
 class go_altar_of_the_keepersAI : public GameObjectAI
 {
 public:
@@ -522,6 +535,7 @@ void AddSC_uldaman()
     newscript = new Script;
     newscript->Name = "go_altar_of_the_keepers";
     newscript->GOGetAI = &GetAI_go_altar_of_the_keepers;
+    newscript->pGOHello = &GOHello_go_altar_of_the_keepers;
     newscript->RegisterSelf();
 
     newscript = new Script;