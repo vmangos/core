6c463f365643cfcb1f1071b24dfab2e158b96edc
Gemt <skarsethkristian@gmail.com>
2017-05-19 17:44:11 +0200

    maexxna: hp, armor and dmg updated to relfect picture found at http://www.kurai.com/wow/naxx/maexxna.jpg

---
+++ sql/naxx/maexxna.sql
@@ -0,0 +1,3 @@
+
+-- source: http://www.kurai.com/wow/naxx/maexxna.jpg
+UPDATE `creature_template` SET `minhealth`=1498950, `maxhealth`=1498950, `armor`=3731, `mindmg`=5491, `maxdmg`=7281 WHERE `entry`=15952;
