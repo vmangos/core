efc705e0aa287834f043bc92afd9f484e515e01d
Gemt <skarsethkristian@gmail.com>
2017-05-20 00:23:15 +0200

    remove  maexxna webs from creature table

--- sql/naxx/maexxna.sql
+++ sql/naxx/maexxna.sql
@@ -1,3 +1,6 @@
 
 -- source: http://www.kurai.com/wow/naxx/maexxna.jpg
 UPDATE `creature_template` SET `minhealth`=1498950, `maxhealth`=1498950, `armor`=3731, `mindmg`=5491, `maxdmg`=7281 WHERE `entry`=15952;
+
+-- Should be no web-wraps spawned at encounter-start.
+DELETE FROM creature where id = 16486;
