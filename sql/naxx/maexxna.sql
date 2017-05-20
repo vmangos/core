8d2e3e93441b5a2c41072dabe8d7ab571dc5ea01
Gemt <skarsethkristian@gmail.com>
2017-05-20 19:45:47 +0200

    maexxna: added creature with entry 17286 to creature_template (invisible man). Removed poison shock necrotic poison and scriptname from spiderlings. Adjusted hp of spiderlings.

--- sql/naxx/maexxna.sql
+++ sql/naxx/maexxna.sql
@@ -6,8 +6,18 @@ UPDATE `creature_template` SET `minhealth`=1498950, `maxhealth`=1498950, `armor`
 DELETE FROM `creature` where id = 16486;
 DELETE FROM `creature` where id = 17055;
 
--- give spiderlings correct faction
-UPDATE `creature_template` SET faction_A = 16, faction_H = 16 WHERE entry = 17055;
-
 -- give web-wrap correct faction
 UPDATE `creature_template` SET faction_A = 16, faction_H = 16 WHERE entry = 16486;
+
+-- adding invisible man creature_template. Used by maexxnas web-wrap logic. Was missing in db.
+ DELETE from `creature_template` where entry = 17286;
+ insert into `creature_template` (entry, modelid_1, name) values(17286, 15435, "Invisible Man");
+ 
+ /* Spiderlings:
+ had spells 28741 (poison shock) and 28776 (necrotic poison). These are maexxna spells, not spiderling spells.
+ corrected faction
+ hp corection based on http://imgur.com/a/wxlbI from this video: https://www.youtube.com/watch?v=rHZPMqAOJAY
+ Had generic_spell_ai as scriptname, not needed with no spells.
+ */
+ Update `creature_template` SET spell1 = 0, spell2 = 0, faction_A = 16, faction_H = 16, minhealth = 4175, maxhealth = 4175, ScriptName = ""
+ where entry = 17055;
