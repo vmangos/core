a6f2f0d34599c23db02b7e7cdf5302cb86a7cac7
Gemt <skarsethkristian@gmail.com>
2017-05-21 00:27:15 +0200

    anub attackspeed update

--- sql/naxx/anubrekhan.sql
+++ sql/naxx/anubrekhan.sql
@@ -5,3 +5,6 @@ DELETE FROM `creature` WHERE `guid`='88348';
 
 -- Remove all the corpse scarabs that were next to the crypt guards
 DELETE FROM `creature` where id = 16698;
+
+-- Increasing anubs attackspeed from 2.4 to 2.2
+UPDATE `creature_template` SET `baseattacktime`=2000 WHERE `entry`=15956;
