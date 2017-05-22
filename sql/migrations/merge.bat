d63bf04b76aab7346b54e8dbbbaf57d7d2fd0ccc
brotalnia <brotalnia@gmail.com>
2017-05-22 13:36:12 +0300

    Change batch script for merging migrations to create a separate sql file for each database (#818)

--- sql/migrations/merge.bat
+++ sql/migrations/merge.bat
@@ -1,8 +1,38 @@
 @echo off
 setlocal EnableDelayedExpansion
-set updates=classicdb_updates.sql
+set updates=world_db_updates.sql
 if exist %updates% del %updates%
-for %%a in (*.sql) do (
+for %%a in (*_world.sql) do (
+echo /* >>%updates%
+echo * %%a >>%updates%
+echo */ >>%updates%
+copy/b %updates%+"%%a" %updates%
+echo. >>%updates%
+echo. >>%updates%)
+
+set updates=characters_db_updates.sql
+if exist %updates% del %updates%
+for %%a in (*_characters.sql) do (
+echo /* >>%updates%
+echo * %%a >>%updates%
+echo */ >>%updates%
+copy/b %updates%+"%%a" %updates%
+echo. >>%updates%
+echo. >>%updates%)
+
+set updates=logon_db_updates.sql
+if exist %updates% del %updates%
+for %%a in (*_logon.sql) do (
+echo /* >>%updates%
+echo * %%a >>%updates%
+echo */ >>%updates%
+copy/b %updates%+"%%a" %updates%
+echo. >>%updates%
+echo. >>%updates%)
+
+set updates=logs_db_updates.sql
+if exist %updates% del %updates%
+for %%a in (*_logs.sql) do (
 echo /* >>%updates%
 echo * %%a >>%updates%
 echo */ >>%updates%
