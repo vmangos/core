809e6e40832e0e9aaf30b54c896c0491aba7a78e
GuybrushGit <turinpt3@gmail.com>
2017-05-23 23:04:43 +0100

    Prevent stacking multiple shadow vulns (#830)

--- src/game/Spells/SpellMgr.cpp
+++ src/game/Spells/SpellMgr.cpp
@@ -434,10 +434,6 @@ SpellSpecific GetSpellSpecific(uint32 spellId)
             if (spellInfo->Id == 13161)
                 return SPELL_ASPECT;
 
-            // Shadow Vulnerability
-            if (spellInfo->SpellIconID == 213 && spellInfo->EffectApplyAuraName[0] == SPELL_AURA_MOD_DAMAGE_PERCENT_TAKEN)
-                return SPELL_SHADOW_VULN;
-
             // Food / Drinks (mostly)
             if (spellInfo->AuraInterruptFlags & AURA_INTERRUPT_FLAG_NOT_SEATED)
             {
@@ -600,7 +596,6 @@ bool IsSingleFromSpellSpecificSpellRanksPerTarget(SpellSpecific spellSpec1, Spel
         case SPELL_AURA:
         case SPELL_CURSE:
         case SPELL_ASPECT:
-        case SPELL_SHADOW_VULN:
             return spellSpec1 == spellSpec2;
         default:
             return false;
