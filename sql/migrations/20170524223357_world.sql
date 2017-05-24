f0238cc65346bf503ee6f480fa5532bacd314bbc
GuybrushGit <turinpt3@gmail.com>
2017-05-24 23:14:13 +0100

    Added invisible wall to WSG Alliance fence (#847)

---
+++ sql/migrations/20170524223357_world.sql
@@ -0,0 +1,6 @@
+INSERT INTO migrations VALUES ('20170524223357');
+
+INSERT INTO gameobject (guid, id, map, position_x, position_y, position_z, orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecs, animprogress, state, spawnFlags) VALUES 
+(90056, 180322, 489, 1395.56, 1461.37, 347.4, 5.83547, 0, 0, 0.0165919, -0.999862, 25, 100, 1, 0),
+(90057, 180322, 489, 1416, 1508, 347.8, 5.81193, 0, 0, 0.233454, -0.972368, 25, 100, 1, 0),
+(90058, 180322, 489, 1404.19, 1487.03, 347.5, 5.83547, 0, 0, 0.221993, -0.975048, 25, 100, 1, 0);
