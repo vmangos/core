ffad2b779b49200042b8810c8ad7b62e65b13406
Gemt <skarsethkristian@gmail.com>
2017-05-29 00:02:21 +0200

    grobbull poison cloud sql changes

--- sql/naxx/grobbulus.sql
+++ sql/naxx/grobbulus.sql
@@ -1,3 +1,6 @@
 
 -- Fallout should do a lot more dmg (no exact sources, but videos show tanks dropping quite a bit)
 UPDATE `creature_template` SET `mindmg`=5045, `maxdmg`=6115 WHERE `entry`=16290;
+
+-- Giving grobbulus cloud creature a scriptname and make it invisible and give it a proper level so the cloud spell dosent resist
+UPDATE `creature_template` SET `ScriptName`="boss_grobbulus_cloud", `unit_flags` = unit_flags | (33554432 + 512), `minlevel`='63', `maxlevel`='63' WHERE `entry`=16363;
