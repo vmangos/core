06cad2c93eb89b18f70733aa76fd3c5e1da5def6
Gemt <skarsethkristian@gmail.com>
2017-05-28 16:03:06 +0200

    grobbulus: fallout slime increased dmg

---
+++ sql/naxx/grobbulus.sql
@@ -0,0 +1,3 @@
+
+-- Fallout should do a lot more dmg (no exact sources, but videos show tanks dropping quite a bit)
+UPDATE `creature_template` SET `mindmg`=5045, `maxdmg`=6115 WHERE `entry`=16290;
