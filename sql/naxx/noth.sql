24a579a58e738065b3b6e94c609ddd48367b03f4
Gemt <Gemt>
2017-05-30 23:51:00 +0200

    removed creatures from noths room. These creatures should be spawned by him. Some dmg and hp adjustments to noth and his adds

--- sql/naxx/noth.sql
+++ sql/naxx/noth.sql
@@ -7,3 +7,12 @@ INSERT INTO `spell_target_position` (`id`, `target_map`, `target_position_x`, `t
 
 -- Noth HP update
 UPDATE `creature_template` SET `minhealth`=1626363, `maxhealth`=1626363 WHERE `entry`=15954;
+
+
+-- Plagued warrior dmg increase (might still need to be much higher, this is from a screenshot ingame, so probably AFTER armor reduction
+UPDATE `creature_template` SET `mindmg`=2000, `maxdmg`=3012 WHERE `entry`=16983;
+
+-- Removing the static adds from creature table, all should be spawned during combat by noth
+DELETE FROM creature where id in (16984, 16981,  16982, 16983);
+
+-- todo: Plagued warrior missing axe weapon
