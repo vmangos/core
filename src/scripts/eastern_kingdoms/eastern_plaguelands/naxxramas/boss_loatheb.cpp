a856741a1924d690e7ce96f4dce4976e711708a7
Gemt <Gemt>
2017-06-09 19:59:28 +0200

    loatheb full script rewrite

--- src/scripts/eastern_kingdoms/eastern_plaguelands/naxxramas/boss_loatheb.cpp
+++ src/scripts/eastern_kingdoms/eastern_plaguelands/naxxramas/boss_loatheb.cpp
@@ -14,69 +14,80 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/* ScriptData
-SDName: Boss_Loatheb
-SD%Complete: 100
-SDComment:
-SDCategory: Naxxramas
-EndScriptData */
 
 #include "scriptPCH.h"
 #include "naxxramas.h"
 
 enum
 {
-    EMOTE_AURA_BLOCKING = -1533143, // todo: add use
-    EMOTE_AURA_FADING   = -1533145, // todo: add use
-    EMOTE_AURA_WANE     = -1533144, // todo: add use
+    // No emotes in vanilla afaik
+    //EMOTE_AURA_BLOCKING = -1533143,
+    //EMOTE_AURA_FADING   = -1533145,
+    //EMOTE_AURA_WANE     = -1533144,
 
-    SPELL_CORRUPTED_MIND  = 29198,
+    SPELL_CORRUPTED_MIND  = 29201, // this triggers the following spells on targets (based on class): 29185, 29194, 29196, 29198
     SPELL_POISON_AURA     = 29865,
     SPELL_INEVITABLE_DOOM = 29204,
-    SPELL_REMOVE_CURSE    = 30281
+    SPELL_REMOVE_CURSE    = 30281, // He periodically removes all curses on himself
+
+    NPC_SPORE             = 16286,
 };
 
-#define ADD_1X 2957.040f
-#define ADD_1Y -3997.590f
-#define ADD_1Z 274.280f
+enum Events
+{
+    EVENT_SUMMON_SPORE = 1,
+    EVENT_CORRUPTED_MIND,
+    EVENT_POISON_AURA,
+    EVENT_INEVITABLE_DOOM,
+    EVENT_REMOVE_CURSE
+};
 
-#define ADD_2X 2909.130f
-#define ADD_2Y -4042.970f
-#define ADD_2Z 274.280f
-
-#define ADD_3X 2861.102f
-#define ADD_3Y -3997.901f
-#define ADD_3Z 274.280f
+// Can't really see much of a system in where the spores spawn.
+// In guides it say "oposite side of where the majority of the raid stands".
+// Unless this is a snapshot check by the boss on pull, which it dosent seem to be based on videos,
+// it simply seems like one of 2 (potentially 4) location is chosed at random on pull, after that it's
+// constantly spawning there throughout the fight.
+// Presumably spell 29234 was used, it has a radius of 70yd. Can't figure out exactly how it would have been used though.
+static constexpr float SporeLocs[2][3] = 
+{
+    {2951.0f, -4016.0f, 274.0f},
+    {2870.0f, -3978.0f, 274.0f}
+};
 
 struct boss_loathebAI : public ScriptedAI
 {
     boss_loathebAI(Creature* pCreature) : ScriptedAI(pCreature)
     {
+        int randLoc = urand(0, 1);
+        for (int i = 0; i < 3; i++)
+            SporeLoc[i] = SporeLocs[randLoc][i];
+
         m_pInstance = (instance_naxxramas*)pCreature->GetInstanceData();
         Reset();
     }
 
     instance_naxxramas* m_pInstance;
 
-    uint32 m_uiCorruptedMindTimer;
-    uint32 m_uiPoisonAuraTimer;
-    uint32 m_uiInevitableDoomTimer;
-    uint32 m_uiInevitableDoom5minsTimer;
-    uint32 m_uiRemoveCurseTimer;
     uint32 m_uiSummonTimer;
+
+    EventMap events;
+    uint32 numDooms;
+    float SporeLoc[3];
 
     void Reset()
     {
-        m_uiCorruptedMindTimer = 4000;
-        m_uiPoisonAuraTimer = 2500;
-        m_uiInevitableDoomTimer = 120000;
-        m_uiInevitableDoom5minsTimer = 300000;
-        m_uiRemoveCurseTimer = 30000;
-        m_uiSummonTimer = 8000;
+        events.Reset();
+        numDooms = 0;
     }
 
     void Aggro(Unit* pWho)
     {
+        numDooms = 0;
+        events.ScheduleEvent(EVENT_SUMMON_SPORE,    Seconds(13));
+        events.ScheduleEvent(EVENT_CORRUPTED_MIND,  Seconds(5));
+        events.ScheduleEvent(EVENT_POISON_AURA,     Seconds(5));
+        events.ScheduleEvent(EVENT_INEVITABLE_DOOM, Minutes(2));
+        events.ScheduleEvent(EVENT_REMOVE_CURSE,    Seconds(2));
         if (m_pInstance)
             m_pInstance->SetData(TYPE_LOATHEB, IN_PROGRESS);
     }
@@ -87,78 +98,70 @@ struct boss_loathebAI : public ScriptedAI
             m_pInstance->SetData(TYPE_LOATHEB, DONE);
     }
 
+    void JustReachedHome()
+    {
+        if (m_pInstance)
+            m_pInstance->SetData(TYPE_LOATHEB, FAIL);
+    }
+
     void UpdateAI(const uint32 uiDiff)
     {
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
 
-        // Corrupted Mind
-        if (m_uiCorruptedMindTimer < uiDiff)
+        events.Update(uiDiff);
+        while (uint32 eventId = events.ExecuteEvent())
         {
-            DoCastSpellIfCan(m_creature->getVictim(), SPELL_CORRUPTED_MIND);
-            m_uiCorruptedMindTimer = 62000;
-        }
-        else
-            m_uiCorruptedMindTimer -= uiDiff;
-
-        // Poison Aura
-        if (m_uiPoisonAuraTimer < uiDiff)
-        {
-            DoCastSpellIfCan(m_creature->getVictim(), SPELL_POISON_AURA);
-            m_uiPoisonAuraTimer = 60000;
-        }
-        else
-            m_uiPoisonAuraTimer -= uiDiff;
-
-        // Inevitable Doom
-        if (m_uiInevitableDoomTimer < uiDiff)
-        {
-            DoCastSpellIfCan(m_creature->getVictim(), SPELL_INEVITABLE_DOOM);
-            m_uiInevitableDoomTimer = 120000;
-        }
-        else
-            m_uiInevitableDoomTimer -= uiDiff;
-
-        // Inevitable Doom 5mins
-        if (m_uiInevitableDoom5minsTimer < uiDiff)
-        {
-            DoCastSpellIfCan(m_creature->getVictim(), SPELL_INEVITABLE_DOOM);
-            m_uiInevitableDoom5minsTimer = 15000;
-        }
-        else
-            m_uiInevitableDoom5minsTimer -= uiDiff;
-
-        // Remove Curse
-        if (m_uiRemoveCurseTimer < uiDiff)
-        {
-            DoCastSpellIfCan(m_creature, SPELL_REMOVE_CURSE);
-            m_uiRemoveCurseTimer = 30000;
-        }
-        else
-            m_uiRemoveCurseTimer -= uiDiff;
-
-        // Summon
-        if (m_uiSummonTimer < uiDiff)
-        {
-            Unit* pSummonedSpores = NULL;
-
-            pSummonedSpores = m_creature->SummonCreature(16286, ADD_1X, ADD_1Y, ADD_1Z, 0.0f, TEMPSUMMON_TIMED_OR_DEAD_DESPAWN, 80000);
-            pSummonedSpores = m_creature->SummonCreature(16286, ADD_2X, ADD_2Y, ADD_2Z, 0.0f, TEMPSUMMON_TIMED_OR_DEAD_DESPAWN, 80000);
-            pSummonedSpores = m_creature->SummonCreature(16286, ADD_3X, ADD_3Y, ADD_3Z, 0.0f, TEMPSUMMON_TIMED_OR_DEAD_DESPAWN, 80000);
-            if (pSummonedSpores)
+            switch (eventId)
             {
-                if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
-                    pSummonedSpores->AddThreat(pTarget);
+            case EVENT_SUMMON_SPORE:
+                if (Creature* pSpore = m_creature->SummonCreature(NPC_SPORE, SporeLoc[0], SporeLoc[1], SporeLoc[2], 0, TEMPSUMMON_TIMED_DESPAWN_OUT_OF_COMBAT, 5000))
+                {
+                    if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+                        pSpore->AddThreat(pTarget);
+                }
+                events.Repeat(Seconds(13));
+                break;
+            case EVENT_CORRUPTED_MIND:
+                // https://www.youtube.com/watch?v=a0z9qjLxD98&list=PLYsWP02PY54A3RkEJv_VaT-0ZhfMs5zxN&index=4
+                // shows it refreshing every ~10 sec
+                DoCastAOE(SPELL_CORRUPTED_MIND, false);
+                events.Repeat(Seconds(10));
+                break;
+            case EVENT_POISON_AURA:
+                DoCastAOE(SPELL_POISON_AURA, false);
+                events.Repeat(Seconds(12));
+                break;
+            case EVENT_INEVITABLE_DOOM:
+                DoCastAOE(SPELL_INEVITABLE_DOOM);
+                ++numDooms;
+                // 2, 2:30, 3, 3:30, 4, 4:30, 5
+                // after 7 dooms, or 5 minutes into the fight,
+                // the doom timer becomes 15 instead of 30 seconds.
+                if(numDooms > 6)
+                    events.Repeat(Seconds(15));
+                else
+                    events.Repeat(Seconds(30));
+                break;
+            case EVENT_REMOVE_CURSE:
+                // The remove curse part was added to prevent the boss from being super easy by stacking spriests.
+                // I have not seen any videos where curse of elements etc is dispelled. Its unknown if he should cast
+                // decurse regardless of vamperic embrace or not, but my assumption is he only does it if he has 
+                // vamperic embrace on, so curse of elements etc an still be up.
+                if (m_creature->HasAura(15286)) // vamperic embrace
+                {
+                    if (DoCastSpellIfCan(m_creature, SPELL_REMOVE_CURSE) == CAST_OK)
+                        events.Repeat(Seconds(1));
+                    else
+                        events.Repeat(100);
+                }
+                break;
             }
-
-            m_uiSummonTimer = 28000;
         }
-        else
-            m_uiSummonTimer -= uiDiff;
-
         DoMeleeAttackIfReady();
     }
 };
+
 CreatureAI* GetAI_boss_loatheb(Creature* pCreature)
 {
     return new boss_loathebAI(pCreature);
