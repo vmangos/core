34e12795f78b7cc8140df17d94143545b4c7605c
Staden <bcr4kfwebgnnon@yandex.com>
2017-07-09 21:33:26 +0100

    Remove UTF8 BOM from migrations
    Should ideally be removed by the merge script

--- sql/migrations/20170629130810_world.sql
+++ sql/migrations/20170629130810_world.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO `migrations` VALUES ('20170629130810');
+INSERT INTO `migrations` VALUES ('20170629130810');
 
 -- Fix Arcane Weakness
 INSERT INTO `spell_script_target` VALUES ('25181', '1', '15339');
--- sql/migrations/20170630160000_world.sql
+++ sql/migrations/20170630160000_world.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO `migrations` VALUES ('20170630160000');
+INSERT INTO `migrations` VALUES ('20170630160000');
 
 -- Fix spell on Plans: Dawnbringer Shoulders
 UPDATE `item_template` SET `spellid_1` = 16686, `spellid_2` = 0 WHERE `entry` = 12698;
