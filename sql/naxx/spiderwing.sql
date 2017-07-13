89616fd1c98e21f8056a5caec2e569ebb3792e03
Gemt <Gemt>
2017-07-13 13:55:58 +0200

    poisonous skitter scale halfed and movementspeed increased

--- sql/naxx/spiderwing.sql
+++ sql/naxx/spiderwing.sql
@@ -1,2 +1,5 @@
 -- Venom Stalker Poson charge repeats every 10-20 sec, and targets a random hostile, not current tank
 UPDATE `creature_ai_scripts` SET `event_flags`=3, `event_param3`=10000, `event_param4`=20000, `action1_param2`=4 WHERE `id`=1597601;
+
+-- Poisonous Skitterer scale reduced, increased movementspeed
+UPDATE `creature_template` SET `scale`='0.5', `speed_run`='1.75', `speed_walk`='1.5' WHERE `entry`=15977;
