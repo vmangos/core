e50a6f28eb301ccad8cacbab946ef1f062f14633
Gemt <Gemt>
2017-07-13 14:51:21 +0200

    naxxramas acoloyte spell updates

--- sql/naxx/spiderwing.sql
+++ sql/naxx/spiderwing.sql
@@ -3,3 +3,9 @@ UPDATE `creature_ai_scripts` SET `event_flags`=3, `event_param3`=10000, `event_p
 
 -- Poisonous Skitterer scale reduced, increased movementspeed
 UPDATE `creature_template` SET `scale`='0.5', `speed_run`='1.75', `speed_walk`='1.5' WHERE `entry`=15977;
+
+-- naxxramas acolyte arcane explosion spell changed to one with 1.5sec cast time, and timers randomized slightly 
+UPDATE `creature_ai_scripts` SET `event_param2`=3000, `event_param3`=7000, `event_param4`=9000, `action1_param1`=30096 WHERE `id`=1598101;
+
+-- naxxramas acolyte shadowbolt timer randomized more
+UPDATE `creature_ai_scripts` SET `event_param2`=5000, `event_param3`=7000, `event_param4`=9000 WHERE `id`=1598102;
