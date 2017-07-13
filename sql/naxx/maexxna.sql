8d229b9f789e1c88291deec4a32dce9ac602b1d3
Gemt <Gemt>
2017-07-13 12:41:45 +0200

    maexxna: now immune to taunt

--- sql/naxx/maexxna.sql
+++ sql/naxx/maexxna.sql
@@ -1,6 +1,7 @@
 
--- source: http://www.kurai.com/wow/naxx/maexxna.jpg
-UPDATE `creature_template` SET `minhealth`=1498950, `maxhealth`=1498950, `armor`=3731, `mindmg`=5491, `maxdmg`=7281 WHERE `entry`=15952;
+-- dmg/hp source: http://www.kurai.com/wow/naxx/maexxna.jpg
+-- should not be tauntable (flags_extra 257)
+UPDATE `creature_template` SET `minhealth`=1498950, `maxhealth`=1498950, `armor`=3731, `mindmg`=5491, `maxdmg`=7281, `flags_extra`=257 WHERE `entry`=15952;
 
 -- Should be no web-wraps or spiderlings spawned at encounter-start.
 DELETE FROM `creature` where id = 16486;
