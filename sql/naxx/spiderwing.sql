3acc86f0962b021fb35078501abee9c8646e29c3
Gemt <Gemt>
2017-07-15 17:32:36 +0200

    plagued ghoul cleave AI event

--- sql/naxx/spiderwing.sql
+++ sql/naxx/spiderwing.sql
@@ -62,3 +62,10 @@ UPDATE `creature_template` SET `mindmg`='3000', `maxdmg`='3500' WHERE `entry`=15
 
 -- Necro stalker poison charge now targets random person and repeats every 10-20s
 UPDATE `creature_ai_scripts` SET `event_flags`=3, `event_param3`=10000, `event_param4`=20000, `action1_param2`=4 WHERE `id`=1645301;
+
+-- Plagued Ghoul cleave
+DELETE FROM `creature_ai_scripts` where id = 1644704;
+INSERT INTO `creature_ai_scripts`
+(`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`)
+VALUES
+(1644704, 16447, 0, 0, 100, 1, 0, 0, 7000, 8000, 11, 26350, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Plagued Ghoul cleave');
