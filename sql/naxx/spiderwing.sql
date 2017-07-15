4a7ed57aef40cb053005def77092757f0969c7fd
Gemt <Gemt>
2017-07-15 17:36:28 +0200

    Updated timer of Necropolis Acolyte shadow bolt volley such that it happens at the same time as arcane explosion

--- sql/naxx/spiderwing.sql
+++ sql/naxx/spiderwing.sql
@@ -69,3 +69,7 @@ INSERT INTO `creature_ai_scripts`
 (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`)
 VALUES
 (1644704, 16447, 0, 0, 100, 1, 0, 0, 7000, 8000, 11, 26350, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Plagued Ghoul cleave');
+
+
+-- Updated timer of Necropolis Acolyte shadow bolt volley such that it happens at the same time as arcane explosion
+UPDATE `creature_ai_scripts` SET `event_param1`=2000, `event_param2`=2000, `event_param3`=5000, `event_param4`=5000 WHERE `id`=1636802;
