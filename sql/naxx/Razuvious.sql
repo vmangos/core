7b8d72d609eecee46ef0e2bb99d6bf484420650e
Gemt <Gemt>
2017-07-18 17:50:45 +0200

    deathknight understudy eventai entry for putting boss in combat when pulled

--- sql/naxx/Razuvious.sql
+++ sql/naxx/Razuvious.sql
@@ -22,3 +22,9 @@ INSERT INTO `creature_ai_scripts`
 (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`)
 VALUES
 (1680301, 16803, 1, 0, 100, 1, 0, 5000, 5000, 10000, 5, 36, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Deathknight Understudy attack emote');
+
+DELETE FROM `creature_ai_scripts` where id = 1680302;
+INSERT INTO `creature_ai_scripts`
+(`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`)
+VALUES
+(1680302, 16803, 4, 0, 100, 0, 0, 0, 0, 0, 39, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Deathknight Understudiy calls for help on aggro');
