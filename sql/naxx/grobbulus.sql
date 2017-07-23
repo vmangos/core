04228d894f78d20f88dac4207e577573cc8ce09b
Gemt <Gemt>
2017-07-23 17:24:35 +0200

    grobbbulus now throws slime 3 times, and the sewage slimes that spawn moves randomly

--- sql/naxx/grobbulus.sql
+++ sql/naxx/grobbulus.sql
@@ -10,8 +10,15 @@ UPDATE `creature_template` SET `scale`=1.8, `MovementType`=2 WHERE `entry`=15931
 REPLACE INTO spell_target_position (id, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
 (28280, 533, 3125.44, -3309.74, 293.251, 3.14929);
 
-select * from creature_movement_scripts where id = 1593101;
-REPLACE INTO creature_movement_scripts (id, delay, command, datalong, comments) VALUES
+DELETE FROM creature_movement_scripts where id = 1593101;
+INSERT INTO creature_movement_scripts (id, delay, command, datalong, comments) VALUES
+(1593101, 1, 15, 28280, "cast summon sewage slime");
+INSERT INTO creature_movement_scripts (id, delay, command, datalong, comments) VALUES
+(1593101, 3, 15, 28280, "cast summon sewage slime");
+INSERT INTO creature_movement_scripts (id, delay, command, datalong, comments) VALUES
 (1593101, 5, 15, 28280, "cast summon sewage slime");
 
 UPDATE creature set MovementType=2, currentWaypoint=5 where id = 15931;
+
+-- make sewage slimes that grobbulus spawn move randomly
+UPDATE `creature_template` SET `MovementType`=1 WHERE `entry`=16375;
