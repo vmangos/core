2c7a3e58489d191ce144053afe959eec0a904602
Gemt <Gemt>
2017-07-24 16:13:26 +0200

    prepared exports.sql which contains the selects needed to find the data we must move to elysium

---
+++ sql/naxx/exports.sql
@@ -0,0 +1,7 @@
+-- SELECT * FROM creature_movement_template where entry in (select id from creature where map = 533);
+
+-- select * from creature where map = 533;
+
+-- SELECT * FROM creature_movement where id in (select guid from creature where map = 533);
+
+-- select * from creature_groups where leaderGUID in (select guid from creature where map = 533);
