f6d158002f8931017deb20409b7da913174e4151
Gemt <Gemt>
2017-07-24 15:30:59 +0200

    patchwork golems now always has their disease aura

--- sql/naxx/constructwing.sql
+++ sql/naxx/constructwing.sql
@@ -16,12 +16,15 @@ UPDATE `creature_ai_scripts` SET `action1_param1`=28125 WHERE `id`=1601701;
 -- Patchwork Golem slightly reduced frequency on 360 cleave
 UPDATE `creature_ai_scripts` SET `event_param3`=7000, `event_param4`=8000 WHERE `id`=1601702;
 
+-- patchwork golem keeps positive auras on evade
+UPDATE `creature_template` SET `flags_extra`=4096 WHERE `entry`=16017;
+
 -- Patchwork Golem casts disease cloud on himself
 DELETE FROM `creature_ai_scripts` where id = 1601703;
 INSERT INTO `creature_ai_scripts`
 (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`)
 VALUES
-(1601703, 16017, 27, 0, 100, 1, 27793, 1, 1000, 1000, 11, 27793, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Patchwork Golem disease cloud');
+(1601703, 16017, 11, 0, 100, 1, 0, 0, 0, 0, 11, 27793, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 'Patchwork Golem disease cloud on spawn');
 
 
 -- Sludge Belcher periodically summons Bile Sludge 
