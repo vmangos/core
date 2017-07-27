561b9c60e58e138ab259f6ebf0723f2174c7f68c
Gemt <Gemt>
2017-07-27 11:59:48 +0200

    zombie chow replace old eventAI entry with one that cast infected wounds triggeraura on self at spawn

--- sql/naxx/Gluth.sql
+++ sql/naxx/Gluth.sql
@@ -16,3 +16,10 @@ VALUES ('28375', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1
 
 -- Gluth slightly larger hitbox
 UPDATE `creature_model_info` SET `combat_reach`='6' WHERE `modelid` = 16064;
+
+-- replace old manual infected wound spell with the version which triggers on successfull melee attack
+DELETE FROM `creature_ai_scripts` where id = 1636001;
+INSERT INTO `creature_ai_scripts`
+(`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`)
+VALUES
+(1636001, 16360, 11, 0, 100, 1, 0, 0, 0, 0, 11, 29307, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 'Zombie Chow cast infected wound on self on spawn');
