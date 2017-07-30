cb54c74a2e56832d13d71ab3ee9f1d5cba9693f4
Gemt <Gemt>
2017-07-30 14:27:56 +0200

    unstoppable abomination mortal wounds now stack with itself such that if multiple aboms cast it on same target they get stacks instead of the debuff replacing itself

--- sql/naxx/kelthuzad.sql
+++ sql/naxx/kelthuzad.sql
@@ -22,3 +22,10 @@ VALUES ('29879', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1
 
 -- giving shadow fissure creature script
 UPDATE `creature_template` SET `AIName`='', `ScriptName`='mob_shadow_fissure', `minlevel`='63', `maxlevel`='63', `scale`=2 WHERE `entry`=16129;
+
+
+
+-- Mortal Wound stack with self
+DELETE FROM spell_mod where Id = 28467;
+INSERT INTO `spell_mod` (`Id`, `procChance`, `procFlags`, `procCharges`, `DurationIndex`, `Category`, `CastingTimeIndex`, `StackAmount`, `SpellIconID`, `activeIconID`, `manaCost`, `Attributes`, `AttributesEx`, `AttributesEx2`, `AttributesEx3`, `AttributesEx4`, `Custom`, `InterruptFlags`, `AuraInterruptFlags`, `ChannelInterruptFlags`, `Dispel`, `Stances`, `StancesNot`, `SpellVisual`, `ManaCostPercentage`, `StartRecoveryCategory`, `StartRecoveryTime`, `MaxAffectedTargets`, `MaxTargetLevel`, `DmgClass`, `rangeIndex`, `RecoveryTime`, `CategoryRecoveryTime`, `SpellFamilyName`, `SpellFamilyFlags`, `Mechanic`, `Comment`) 
+VALUES ('28467', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '-1', '0', '-1', 'Mortal Wound stack with self');
