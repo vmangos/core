35b44b9837acaacb4a67172193d1898150bf614e
Gemt <Gemt>
2017-08-05 19:59:10 +0200

    fix scale of all 4hm

--- sql/naxx/Four_Horsemen.sql
+++ sql/naxx/Four_Horsemen.sql
@@ -1,5 +1,5 @@
-UPDATE creature_template SET faction_a = 21, faction_h = 21, scale = 0, unit_flags = 33554432, dynamicflags = 0 WHERE entry BETWEEN 16775 AND 16778;
-
+UPDATE creature_template SET faction_a = 20, faction_h = 20, scale = 0, unit_flags = 33554432, dynamicflags = 0 WHERE entry BETWEEN 16775 AND 16778;
+UPDATE creature_template SET scale = 0 WHERE entry BETWEEN 16062 AND 16065;
 UPDATE creature_template SET scriptname = "boss_highlord_mograine" WHERE entry = 16775;
 UPDATE creature_template SET scriptname = "boss_lady_blaumeux" WHERE entry = 16776;
 UPDATE creature_template SET scriptname = "boss_thane_korthazz" WHERE entry = 16778;
