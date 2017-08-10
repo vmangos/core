481540e43b9c58bacd07efd7ba013d8ccaeea50a
kampeador <kampeador@gmail.com>
2017-08-10 18:04:53 +0300

    Resupplying the excavation (#1671)
    * Quest event implementation: (Resupplying the Excavation id:273).
    
    * cleanup
    
    * typo.

--- src/scripts/eastern_kingdoms/loch_modan/loch_modan.cpp
+++ src/scripts/eastern_kingdoms/loch_modan/loch_modan.cpp
@@ -176,6 +176,167 @@ CreatureAI* GetAI_npc_miran(Creature* pCreature)
     return new npc_miranAI(pCreature);
 }
 
+//-----------------------------------------------------------------------------
+// Quest event implementation (Resupplying the Excavation id:273).
+// Author: Kampeador
+//-----------------------------------------------------------------------------
+
+enum
+{
+    QUEST_RESUPPLYING_THE_EXCAVATION = 273,
+ 
+    NPC_SAEAN              = 1380,
+    NPC_MIRAN              = 1379,
+    NPC_HULDAR             = 2057,
+    NPC_DARK_IRON_AMBUSHER = 1981,
+
+    FACTION_HOSTILE        = 54
+};
+
+struct point3o { float x, y, z, o; };
+
+static const point3o darkIronAmbusherSpawns[] =
+{
+    { -5759.852051f, -3441.279053f, 305.573212f, 2.174024f },
+    { -5757.629883f, -3437.680908f, 304.265106f, 2.610265f }
+};
+
+struct npc_saeanAI : public ScriptedAI
+{
+    Creature* m_darkIronAmbusher[2];
+    // num summoned dark iron ambushers used to guarantee that there will be no duplicate spawns.
+    uint32 m_numSummonedAmbushers;
+    // this one is modified inside area-trigger script.
+    bool m_eventStarted;
+
+    npc_saeanAI(Creature* pCreature) : ScriptedAI(pCreature)
+    {
+        // zero init required to prevent crash
+        for (ptrdiff_t i = 0; i < 2; ++i)
+            m_darkIronAmbusher[i] = nullptr;
+        m_numSummonedAmbushers = 0;
+
+        m_eventStarted = false;
+    }
+
+    void Reset() { }
+
+    // called when Saean summons his guards
+    void JustSummoned(Creature* pSummoned)
+    {
+        // optimization: safely assume, that m_numSummonedAmbushers don't go outside range
+        m_darkIronAmbusher[m_numSummonedAmbushers] = pSummoned;
+        ++m_numSummonedAmbushers;
+    }
+
+    void JustDied(Unit* pVictim)
+    {
+        // It is safe to forget about ambushers, they will despawn automatically.
+        for (ptrdiff_t i = 0; i < 2; ++i)
+            m_darkIronAmbusher[i] = nullptr;
+        m_numSummonedAmbushers = 0;
+
+        m_eventStarted = false;
+    }
+};
+
+CreatureAI* GetAI_npc_saean(Creature* pCreature)
+{
+    return new npc_saeanAI(pCreature);
+}
+
+//-----------------------------------------------------------------------------
+// Miran and Huldar are ambushed when Area Trigger id:171 is triggered and if quest 273 is active
+bool AreaTrigger_at_huldar_miran(Player* pPlayer, AreaTriggerEntry const* /*pAt*/)
+{
+    // If player is dead, GM mode is ON, quest complete or no quest
+    if (!pPlayer->isAlive() || pPlayer->isGameMaster() ||
+        pPlayer->GetQuestStatus(QUEST_RESUPPLYING_THE_EXCAVATION) == QUEST_STATUS_COMPLETE ||
+        pPlayer->GetQuestStatus(QUEST_RESUPPLYING_THE_EXCAVATION) == QUEST_STATUS_NONE)
+    return false;
+
+    // rare case: set quest completed on directly trigger,
+    // because if NPCs are not available, killed by someone or died in previous event,
+    // then, after returning home/respawn, quest will be still incompleted.
+    pPlayer->CompleteQuest(QUEST_RESUPPLYING_THE_EXCAVATION);
+    pPlayer->SendQuestCompleteEvent(QUEST_RESUPPLYING_THE_EXCAVATION);
+
+    // Check if Huldar is available.
+    Creature* huldar = GetClosestCreatureWithEntry(pPlayer, NPC_HULDAR, 60.0f);
+    if (huldar)
+    {
+        if (!huldar->isAlive())
+            return false;
+    }
+    else
+        return false;
+
+    // Check if Miran is available.
+    Creature* miran = GetClosestCreatureWithEntry(pPlayer, NPC_MIRAN, 60.0f);
+    if (miran)
+    {
+        if (!miran->isAlive())
+            return false;
+        else
+        {
+            // rare case: handle possible collision with escort quest event: Protecting the Shipment.
+            // Since Dark Iron Ambushers are focusing Miran, he shouldn't be on escort druing this event.
+            npc_miranAI* miranAI = dynamic_cast<npc_miranAI*>(miran->AI());
+            if (miranAI)
+            {
+                if (miranAI->HasEscortState(STATE_ESCORT_ESCORTING))
+                    return false;
+            }
+        }
+    }
+    else
+        return false;
+
+    
+    // minor optimization: Quest NPCs are already in combat with someone, so skip further checks.
+    if (miran->isInCombat() || huldar->isInCombat())
+        return true;
+    
+    // Check if Saean is available.
+    Creature* saean = GetClosestCreatureWithEntry(pPlayer, NPC_SAEAN, 60.0f);
+    if (saean)
+    {
+        if (saean->isAlive())
+        {
+            npc_saeanAI* saeanAI = dynamic_cast<npc_saeanAI*>(saean->AI());
+            if (saeanAI)
+            {
+                // very rare case: Miran and Huldar are not in combat. Wrong faction in DB, etc.
+                if (!saeanAI->m_eventStarted)
+                {
+                    // set event started
+                    saeanAI->m_eventStarted = true;
+
+                    // set temporary faction to hostile
+                    saean->SetFactionTemporary(FACTION_HOSTILE, TEMPFACTION_RESTORE_RESPAWN);
+
+                    // rare case: prevent any possible duplicate spawns
+                    // 1 player pulled ambushers too far away, while the second one triggers this event
+                    if (!saeanAI->m_numSummonedAmbushers)
+                    {
+                        // summon ambushers
+                        saean->SummonCreature(NPC_DARK_IRON_AMBUSHER, darkIronAmbusherSpawns[0].x, darkIronAmbusherSpawns[0].y, darkIronAmbusherSpawns[0].z, darkIronAmbusherSpawns[0].o, TEMPSUMMON_TIMED_OR_DEAD_DESPAWN, 25000);
+                        saean->SummonCreature(NPC_DARK_IRON_AMBUSHER, darkIronAmbusherSpawns[1].x, darkIronAmbusherSpawns[1].y, darkIronAmbusherSpawns[1].z, darkIronAmbusherSpawns[1].o, TEMPSUMMON_TIMED_OR_DEAD_DESPAWN, 25000);
+                        // Saean focuses Miran.
+                        saeanAI->AttackStart(miran);
+                    }
+                    else
+                    {
+                        // ambushers were summoned, so event is already started
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    
+    return true;
+}
 
 void AddSC_loch_modan()
 {
@@ -191,6 +352,16 @@ void AddSC_loch_modan()
     newscript->Name = "npc_miran";
     newscript->GetAI = &GetAI_npc_miran;
     newscript->pQuestAcceptNPC = &QuestAccept_npc_miran;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "npc_saean";
+    newscript->GetAI = &GetAI_npc_saean;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "at_huldar_miran";
+    newscript->pAreaTrigger = &AreaTrigger_at_huldar_miran;
     newscript->RegisterSelf();
 }
 
