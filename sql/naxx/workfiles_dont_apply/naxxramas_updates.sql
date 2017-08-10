95c26b0c4799bc95ed7e25da58f23a5d86aa7b35
Gemt <Gemt>
2017-08-10 19:16:08 +0200

    full naxxramas.sql patch containing all previously separate sql files

--- sql/naxx/workfiles_dont_apply/naxxramas_updates.sql
+++ sql/naxx/workfiles_dont_apply/naxxramas_updates.sql
@@ -117,55 +117,6 @@ INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inv
 (1614202, 16142, 1, 0, 100, 1, 30000, 30000, 30000, 30000, 41, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'bile sludge despawn after 30 sec ooc'); 
  
 /* 
-* creature_linking.sql 
-*/ 
-
---
--- Table structure for table `creature_linking`
---
-
-DROP TABLE IF EXISTS `creature_linking`;
-CREATE TABLE `creature_linking` (
-  `guid` int(10) unsigned NOT NULL COMMENT 'creature.guid of the slave mob that is linked',
-  `master_guid` int(10) unsigned NOT NULL COMMENT 'master to trigger events',
-  `flag` mediumint(8) unsigned NOT NULL COMMENT 'flag - describing what should happen when',
-  PRIMARY KEY (`guid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Creature Linking System';
-
---
--- Dumping data for table `creature_linking`
---
-
-LOCK TABLES `creature_linking` WRITE;
-/*!40000 ALTER TABLE `creature_linking` DISABLE KEYS */;
-/*!40000 ALTER TABLE `creature_linking` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `creature_linking_template`
---
-
-DROP TABLE IF EXISTS `creature_linking_template`;
-CREATE TABLE `creature_linking_template` (
-  `entry` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'creature_template.entry of the slave mob that is linked',
-  `map` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT 'Id of map of the mobs',
-  `master_entry` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'master to trigger events',
-  `flag` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'flag - describing what should happen when',
-  `search_range` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'search_range - describing in which range (spawn-coords) master and slave are linked together',
-  PRIMARY KEY (`entry`,`map`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Creature Linking System';
-
---
--- Dumping data for table `creature_linking_template`
---
-
-LOCK TABLES `creature_linking_template` WRITE;
-/*!40000 ALTER TABLE `creature_linking_template` DISABLE KEYS */;
-/*!40000 ALTER TABLE `creature_linking_template` ENABLE KEYS */;
-UNLOCK TABLES;
- 
- 
-/* 
 * deathknight_wing.sql 
 */ 
 
@@ -393,7 +344,7 @@ INSERT INTO creature_equip_template (entry, equipentry1, equipentry2, equipentry
 UPDATE `creature_template` SET `AIName`='', `equipment_id`=16156, `ScriptName`='dark_touched_warriorAI' WHERE `entry`=16156;
 
 -- give AreaTrigger at start of DK wing script name
-DELETE FROM `creature_ai_scripts` WHERE entry = 4117;
+DELETE FROM `scripted_areatrigger` WHERE entry = 4117;
 INSERT INTO `scripted_areatrigger` (`entry`, `ScriptName`) VALUES ('4117', 'at_naxxramas');
 
 -- deathknight vindicator faster aura of agony
