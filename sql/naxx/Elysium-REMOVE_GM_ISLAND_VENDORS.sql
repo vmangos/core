94f2d48858bbbdcce4ce96f0b6a4ed2725ad77ff
Gemt <Gemt>
2017-08-13 02:32:24 +0200

    merged removal and adding sqls for ptr creatures and renamed file

--- sql/naxx/Elysium-REMOVE_GM_ISLAND_VENDORS.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM `creature_template` WHERE `entry` BETWEEN 90000 AND 90028;
-DELETE FROM `creature_ai_scripts` WHERE `creature_id` IN (90023, 90024, 90025);
-DELETE FROM `npc_trainer` WHERE `entry` BETWEEN 90000 AND 90028;
-DELETE FROM `npc_vendor` WHERE `entry` BETWEEN 90000 AND 90028;
-DELETE FROM `creature` WHERE (`id` BETWEEN 90000 AND 90028) || (`id`=20008);
-DELETE FROM `npc_text` WHERE `ID`=9000;
-DELETE FROM `npc_gossip` WHERE `textid`=9000;
--- sql/naxx/Elysium_PTR_NPCs.sql
+++ sql/naxx/Elysium_PTR_NPCs.sql
@@ -1,6 +1,14 @@
 -- GM Island Vendors for Elysium by brotalnia.
 -- CREATURE_TEMPLATE
 
+DELETE FROM `creature_template` WHERE `entry` BETWEEN 90000 AND 90028;
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` IN (90023, 90024, 90025);
+DELETE FROM `npc_trainer` WHERE `entry` BETWEEN 90000 AND 90028;
+DELETE FROM `npc_vendor` WHERE `entry` BETWEEN 90000 AND 90028;
+DELETE FROM `creature` WHERE (`id` BETWEEN 90000 AND 90028) || (`id`=20008);
+DELETE FROM `npc_text` WHERE `ID`=9000;
+DELETE FROM `npc_gossip` WHERE `textid`=9000;
+
 UPDATE item_template SET requiredhonorrank=0 WHERE requiredhonorrank>0;
 
 
