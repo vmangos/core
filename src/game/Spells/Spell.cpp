fa1db99f82085ab98f8e86e2351b7659c88a35ad
GuybrushGit <turinpt3@gmail.com>
2017-08-19 19:11:01 +0100

    Fix to Judgement of Command scaling. (#2152)

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -1191,13 +1191,6 @@ void Spell::DoAllEffectOnTarget(TargetInfo *target)
         else
         {
             caster->CalculateSpellDamage(&damageInfo, m_damage, m_spellInfo, m_attackType, this);
-            // Judgement of Command
-            if (m_spellInfo->SpellIconID == 561)
-            {
-                // damage halved if target not stunned.
-                if (!unitTarget->hasUnitState(UNIT_STAT_STUNNED | UNIT_STAT_PENDING_STUNNED))
-                     damageInfo.damage = int32(damageInfo.damage * 0.5f);
-            }
         }
 
         unitTarget->CalculateAbsorbResistBlock(caster, &damageInfo, m_spellInfo, BASE_ATTACK, this);
