363b2480071b42e335cc45274931cf3ef092fdfe
Gemt <Gemt>
2017-09-09 15:11:19 +0200

    living monstrosity lightning totem timers

--- sql/naxx/naxxramas_patch01.sql
+++ sql/naxx/naxxramas_patch01.sql
@@ -1,2 +1,8 @@
 -- Fate of Ramaladni 1% droprate
 update creature_loot_template set ChanceOrQuestChance = -1 where Item = 22708;
+
+-- Lightning totem chain lightning aura application timer
+UPDATE `creature_ai_scripts` SET `event_type`=0, `event_param1`=2000, `event_param2`=2500 WHERE `id`=1638501;
+
+-- Living Monstrosity lightning totem spawntimer
+UPDATE `creature_ai_scripts` SET `event_param3`=10000, `event_param4`=15000 WHERE `id`=1602103;
