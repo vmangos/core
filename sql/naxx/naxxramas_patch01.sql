c5a7642ea2b6f3de6ee3edae06dbacf6a721827a
Gemt <Gemt>
2017-09-09 20:01:22 +0200

    unholy axe correct targetting of mortal strike

--- sql/naxx/naxxramas_patch01.sql
+++ sql/naxx/naxxramas_patch01.sql
@@ -6,3 +6,6 @@ UPDATE `creature_ai_scripts` SET `event_type`=0, `event_param1`=2000, `event_par
 
 -- Living Monstrosity lightning totem spawntimer
 UPDATE `creature_ai_scripts` SET `event_param3`=10000, `event_param4`=15000 WHERE `id`=1602103;
+
+-- Unholy Axe mortal strike on target instead of self
+UPDATE `creature_ai_scripts` SET `action1_param2`=1 WHERE `id`=1619402;
