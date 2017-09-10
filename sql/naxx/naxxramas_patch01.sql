54e22a934106fe459758bd4bdb11199b459471f8
Gemt <Gemt>
2017-09-10 18:57:28 +0200

    Make the shadow/ice guard repetable

--- sql/naxx/naxxramas_patch01.sql
+++ sql/naxx/naxxramas_patch01.sql
@@ -9,3 +9,7 @@ UPDATE `creature_ai_scripts` SET `event_param3`=10000, `event_param4`=15000 WHER
 
 -- Unholy Axe mortal strike on target instead of self
 UPDATE `creature_ai_scripts` SET `action1_param2`=1 WHERE `id`=1619402;
+
+-- Make the shadow/ice guard repetable
+UPDATE `quest_template` SET `SpecialFlags`='1' WHERE `entry`='9211';
+UPDATE `quest_template` SET `SpecialFlags`='1' WHERE `entry`='9213';
