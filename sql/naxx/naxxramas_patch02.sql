4fca4049ccd172f21b4d1a3c42ed42ec9ed2f23b
Gemt <Gemt>
2017-09-18 17:01:06 +0200

    naxxramas_patch02.sql formatting

--- sql/naxx/naxxramas_patch02.sql
+++ sql/naxx/naxxramas_patch02.sql
@@ -3,6 +3,4 @@
 UPDATE `creature_template` SET `mindmg`=2258, `maxdmg`=2759, `MechanicImmuneMask`=1019427442 WHERE `entry`=16441;
 
 -- misc missing creature linking
-insert into creature_linking (`guid`,`master_guid`,`flag`) 
-(2530344, 88496, 3072), -- carrion spider, faerlina
-(2530568, 88303, 3073); -- stitched giant, grobbulus
+insert into creature_linking (`guid`,`master_guid`,`flag`)  VALUES (2530344, 88496, 3072), /*carrion spider, faerlina*/ (2530568, 88303, 3073); /*stitched giant, grobbulus*/
