3dfb16c7716c68fb8ce21771b056e66a1bea3cc1
brotalnia <brotalnia@gmail.com>
2017-10-26 19:48:05 +0300

    Fix Divine Intervention.

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -851,7 +851,7 @@ void Spell::AddUnitTarget(Unit* pVictim, SpellEffectIndex effIndex)
     if (m_spellInfo->Effect[effIndex] == 0)
         return;
 
-    if (m_spellInfo->AttributesEx & SPELL_ATTR_EX_CANT_TARGET_SELF && pVictim->GetObjectGuid() == m_originalCaster->GetObjectGuid())
+    if ((m_spellInfo->AttributesEx & SPELL_ATTR_EX_CANT_TARGET_SELF) && (m_spellInfo->EffectImplicitTargetA[effIndex] != TARGET_SELF) && (m_spellInfo->EffectImplicitTargetB[effIndex] != TARGET_SELF) && (pVictim->GetObjectGuid() == m_originalCaster->GetObjectGuid()))
         return;
 
     // Check for effect immune skip if immuned
