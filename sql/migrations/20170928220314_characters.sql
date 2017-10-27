d28535e1a11865aeb9691d37e2c7e392ddf2ec6b
brotalnia <brotalnia@gmail.com>
2017-10-27 22:16:16 +0300

    Remove old migrations.

--- sql/migrations/20170928220314_characters.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `migrations` VALUES ('20170928220314');
-
-ALTER TABLE `mail_items`
-	CHANGE COLUMN `mail_id` `mail_id` INT(11) UNSIGNED NOT NULL DEFAULT '0' FIRST,
-	CHANGE COLUMN `item_guid` `item_guid` INT(11) UNSIGNED NOT NULL DEFAULT '0' AFTER `mail_id`,
-	CHANGE COLUMN `item_template` `item_template` INT(11) UNSIGNED NOT NULL DEFAULT '0' AFTER `item_guid`;
