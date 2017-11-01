f751e931ea286b5f708ff62daad65b314bc43b37
brotalnia <brotalnia@gmail.com>
2017-11-01 04:51:23 +0200

    Fix wrong column count in migration.

--- sql/migrations/20171012210802_world.sql
+++ sql/migrations/20171012210802_world.sql
@@ -13,8 +13,8 @@ INSERT INTO `migrations` VALUES ('20171012210802');
 -- https://github.com/elysium-project/server/issues/2776
 REPLACE INTO `gameobject_template` VALUES (180055, 2, 51, 'Mysterious Wailing Caverns Chest', 0, 4, 0.3, 93, 7944, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
 REPLACE INTO `gameobject_involvedrelation` (`id`, `quest`, `patch`) VALUES (180055, 7944, 4);
-REPLACE INTO `gameobject` VALUES (18926, 180055, 43, -105.449, 166.904, -78.9907, 4.91957, 0, 0, 0.966546, -0.256494, 0, 100, 1, 0);
-REPLACE INTO `gameobject` VALUES (18927, 180024, 36, -32.736, -374.034, 59, 3, 0, 0, 0.997495, 0.0707372, 0, 100, 1, 0);
+REPLACE INTO `gameobject` VALUES (18926, 180055, 43, -105.449, 166.904, -78.9907, 4.91957, 0, 0, 0.966546, -0.256494, 0, 100, 1, 0, 0);
+REPLACE INTO `gameobject` VALUES (18927, 180024, 36, -32.736, -374.034, 59, 3, 0, 0, 0.997495, 0.0707372, 0, 100, 1, 0, 0);
 UPDATE `gameobject_template` SET `size`=0.3 WHERE  `entry`=180024;
 
 
