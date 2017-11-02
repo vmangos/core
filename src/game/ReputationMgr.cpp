be9e70a8fa1d04263b5770c3c37711386362cfd8
brotalnia <brotalnia@gmail.com>
2017-11-02 22:57:38 +0200

    Fix client crash when using flight master.

--- src/game/ReputationMgr.cpp
+++ src/game/ReputationMgr.cpp
@@ -348,12 +348,7 @@ void ReputationMgr::SetAtWar(FactionState* faction, bool atWar)
     else
         faction->Flags &= ~FACTION_FLAG_AT_WAR;
 
-    WorldPacket data(SMSG_SET_FACTION_ATWAR, 4 + 1);
-    data << uint32(faction->ID);
-    data << uint8(faction->Flags & FACTION_FLAG_AT_WAR);    // the client tests only FACTION_FLAG_AT_WAR
-    m_player->SendDirectMessage(&data);
-
-    faction->needSend = false;
+    faction->needSend = true;
     faction->needSave = true;
 }
 
