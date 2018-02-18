8251a7c5ad2d7232be831c3925eb2b603a8c0db3
GuybrushGit <turinpt3@gmail.com>
2018-02-18 21:14:07 +0000

    Black dress patch number

--- sql/migrations/20180218154908_world.sql
+++ sql/migrations/20180218154908_world.sql
@@ -12,8 +12,8 @@ INSERT INTO `migrations` VALUES ('20180218154908');
 -- Items 10 day duration
 UPDATE `item_template` SET `Duration`=864000, `ExtraFlags`=1 WHERE entry in (21812,22239,22236,22238,22237,22218,22261,22200,22218,22178,22160,22159,22161,22163,22162,21975,22154,22155,22156,22157,22158,22170,22168,22167,22169,22172,22171,21981,21980,21979,22164,22166,22165);
 
--- Delete Black dress from forbidden list
-DELETE FROM `forbidden_items` WHERE `entry`=22279;
+-- Black dress patch number
+UPDATE `forbidden_items` SET `patch`=7 WHERE `entry`=22279;
 
 -- Remove citizen scripts
 UPDATE `creature_template` SET `ScriptName`='' WHERE `ScriptName` like '%npc%citizen%'
