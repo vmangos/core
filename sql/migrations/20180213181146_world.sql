202fe14a67cb8d731b7c07e6df1c694dbbda8e4a
Kittnz <frederik156@hotmail.com>
2018-02-24 11:02:34 +0100

    Scourge Invasion: Add gob guid to game_event_gameobject table

--- sql/migrations/20180213181146_world.sql
+++ sql/migrations/20180213181146_world.sql
@@ -12,6 +12,9 @@ DELETE FROM `gameobject` WHERE `guid` = 3998629;
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`, `spawnFlags`, `visibilitymod`, `patch_min`, `patch_max`) VALUES
 (3998629, 181254, 0, 1583.16, 240.44, -62.0772, 3.12447, 0, 0, 0.999963, 0.00855956, 25, 25, 100, 1, 0, 0, 0, 10);
 
+DELETE FROM `game_event_gameobject` WHERE `guid` = 3998629;
+INSERT INTO `game_event_gameobject` (`guid`, `event`) VALUES (3998629, 17);
+
 UPDATE `creature` SET `position_x`=1578.87, `position_y`=238.628, `position_z`=-62.0773, `orientation`=3.16373 WHERE `guid`=1242983;
 UPDATE `creature` SET `position_x`=1579.28, `position_y`=245.156, `position_z`=-62.0773, `orientation`=3.0577 WHERE `guid`=1242984;
 UPDATE `creature` SET `position_x`=1579.16, `position_y`=242.194, `position_z`=-62.0773, `orientation`=3.0577 WHERE `guid`=1243007;
