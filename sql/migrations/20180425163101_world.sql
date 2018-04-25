2abbb9511f182a465ae98cefb1950f24f68783ff
ratkosrb <ratkomladic2@abv.bg>
2018-04-25 19:32:08 +0300

    Make doors in Twin Emperors room active objects.

---
+++ sql/migrations/20180425163101_world.sql
@@ -0,0 +1,21 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20180425163101');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20180425163101');
+-- Add your query below.
+
+
+-- Make doors in Twin Emperors room active objects.
+UPDATE `gameobject` SET `spawnFlags`=1 WHERE `guid` IN (21783, 21784);
+
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
