de544fcd33679561a65efcb67ac7aadf39217457
ratkosrb <ratkomladic2@abv.bg>
2018-06-01 01:04:17 +0300

    Fix Spectral Tutor and Defias Trapper scripts.

---
+++ sql/migrations/20180531220241_world.sql
@@ -0,0 +1,46 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20180531220241');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20180531220241');
+-- Add your query below.
+
+
+-- Spectral Tutor
+DELETE FROM `creature_ai_events` WHERE `creature_id`=10498;
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1049801, 10498, 0, 0, 2, 100, 1, 4200, 19100, 8600, 26400, 1049801, 0, 0, 'Spectral Tutor - Cast Mana Burn (Phase 0)');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1049802, 10498, 0, 0, 2, 100, 1, 3300, 5300, 12000, 16300, 1049802, 0, 0, 'Spectral Tutor - Cast Silence (Phase 0)');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1049803, 10498, 0, 0, 2, 100, 1, 11800, 12700, 17700, 25300, 1049803, 0, 0, 'Spectral Tutor - Cast Image Projection (Phase 0)');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1049804, 10498, 0, 0, 1, 100, 1, 5500, 5500, 5500, 5500, 1049804, 0, 0, 'Spectral Tutor - Cast Image Projection (Phase 1)');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1049805, 10498, 0, 11, 0, 100, 1, 0, 0, 0, 0, 1049805, 0, 0, 'Spectral Tutor - Set Phase to 0 on Spawn');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1049806, 10498, 0, 7, 0, 100, 0, 0, 0, 0, 0, 1049806, 0, 0, 'Spectral Tutor - Remove Guardians on Evade');
+DELETE FROM `creature_ai_scripts` WHERE `id`=1049801;
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1049801, 0, 15, 17630, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Spectral Tutor - Cast Spell Mana Burn');
+DELETE FROM `creature_ai_scripts` WHERE `id`=1049802;
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1049802, 0, 15, 12528, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Spectral Tutor - Cast Spell Silence');
+DELETE FROM `creature_ai_scripts` WHERE `id`=1049803;
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1049803, 0, 15, 17653, 2, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Spectral Tutor - Cast Spell Summon Spectral Projections');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1049803, 0, 15, 17651, 1, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Spectral Tutor - Cast Spell Image Projection');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1049803, 0, 44, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Spectral Tutor - Set Phase to 1');
+DELETE FROM `creature_ai_scripts` WHERE `id`=1049804;
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1049804, 0, 15, 17652, 7, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Spectral Tutor - Cast Spell Image Projection');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1049804, 0, 44, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Spectral Tutor - Set Phase to 0');
+DELETE FROM `creature_ai_scripts` WHERE `id`=1049805;
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1049805, 0, 44, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Spectral Tutor - Set Phase to 0');
+DELETE FROM `creature_ai_scripts` WHERE `id`=1049806;
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1049806, 0, 56, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Spectral Tutor - Remove Guardians');
+UPDATE `creature_template` SET `AIName`='EventAI', `ScriptName`='' WHERE `entry`=10498;
+
+-- Defias trapper
+REPLACE INTO `creature_spells` (`entry`, `name`, `spellId_1`, `probability_1`, `castTarget_1`, `targetParam1_1`, `targetParam2_1`, `castFlags_1`, `delayInitialMin_1`, `delayInitialMax_1`, `delayRepeatMin_1`, `delayRepeatMax_1`, `scriptId_1`, `spellId_2`, `probability_2`, `castTarget_2`, `targetParam1_2`, `targetParam2_2`, `castFlags_2`, `delayInitialMin_2`, `delayInitialMax_2`, `delayRepeatMin_2`, `delayRepeatMax_2`, `scriptId_2`, `spellId_3`, `probability_3`, `castTarget_3`, `targetParam1_3`, `targetParam2_3`, `castFlags_3`, `delayInitialMin_3`, `delayInitialMax_3`, `delayRepeatMin_3`, `delayRepeatMax_3`, `scriptId_3`, `spellId_4`, `probability_4`, `castTarget_4`, `targetParam1_4`, `targetParam2_4`, `castFlags_4`, `delayInitialMin_4`, `delayInitialMax_4`, `delayRepeatMin_4`, `delayRepeatMax_4`, `scriptId_4`, `spellId_5`, `probability_5`, `castTarget_5`, `targetParam1_5`, `targetParam2_5`, `castFlags_5`, `delayInitialMin_5`, `delayInitialMax_5`, `delayRepeatMin_5`, `delayRepeatMax_5`, `scriptId_5`, `spellId_6`, `probability_6`, `castTarget_6`, `targetParam1_6`, `targetParam2_6`, `castFlags_6`, `delayInitialMin_6`, `delayInitialMax_6`, `delayRepeatMin_6`, `delayRepeatMax_6`, `scriptId_6`, `spellId_7`, `probability_7`, `castTarget_7`, `targetParam1_7`, `targetParam2_7`, `castFlags_7`, `delayInitialMin_7`, `delayInitialMax_7`, `delayRepeatMin_7`, `delayRepeatMax_7`, `scriptId_7`, `spellId_8`, `probability_8`, `castTarget_8`, `targetParam1_8`, `targetParam2_8`, `castFlags_8`, `delayInitialMin_8`, `delayInitialMax_8`, `delayRepeatMin_8`, `delayRepeatMax_8`, `scriptId_8`) VALUES (5040, 'Westfall - Defias Trapper', 12024, 100, 1, 0, 0, 1, 6, 14, 20, 31, 0, 7159, 100, 1, 0, 0, 0, 2, 4, 2, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
--- src/scripts/eastern_kingdoms/western_plaguelands/scholomance/scholo_trash.cpp
+++ src/scripts/eastern_kingdoms/western_plaguelands/scholomance/scholo_trash.cpp
@@ -144,109 +144,6 @@ enum
     SPELL_SUMMON_PROJECTION = 17653
 };
 
-struct npc_spectral_tutorAI : public ScriptedAI
-{
-    npc_spectral_tutorAI(Creature* pCreature) : ScriptedAI(pCreature) { Reset(); }
-
-    uint32 m_uiManaBurnTimer;
-    uint32 m_uiSilenceTimer;
-    uint32 m_uiImageTimer;
-    uint32 m_uiHideTimer;
-
-    bool m_isProjection;
-
-    void Reset() override
-    {
-        m_uiManaBurnTimer   = urand(4200, 19100);
-        m_uiSilenceTimer    = urand(0, 3300);
-        m_uiImageTimer      = urand(11800, 12700);
-
-        m_isProjection      = false;
-    }
-
-    void EnterEvadeMode() override
-    {
-        m_creature->RemoveGuardians();
-        ScriptedAI::EnterEvadeMode();
-    }
-
-    void UpdateAI(const uint32 uiDiff) override
-    {
-        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim()) return;
-
-        if (!m_isProjection)
-        {
-            // Mana Burn
-            if (m_uiManaBurnTimer < uiDiff)
-            {
-                if (Unit* target = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0, nullptr, SELECT_FLAG_PLAYER | SELECT_FLAG_POWER_MANA))
-                {
-                    if (DoCastSpellIfCan(target, SPELL_MANA_BURN) == CAST_OK)
-                    {
-                        m_uiManaBurnTimer = urand(8600, 26400);
-                    }                
-                }
-            }
-            else 
-                m_uiManaBurnTimer -= uiDiff;
-
-            // Silence
-            if (m_uiSilenceTimer < uiDiff)
-            {
-                if (Unit* target = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0, nullptr, SELECT_FLAG_PLAYER | SELECT_FLAG_POWER_MANA))
-                {
-                    if (DoCastSpellIfCan(m_creature->getVictim(), SPELL_SILENCE) == CAST_OK)
-                    {
-                        m_uiSilenceTimer = urand(12000, 26300);
-                    }          
-                }
-            }
-            else 
-                m_uiSilenceTimer -= uiDiff;
-
-            DoMeleeAttackIfReady();
-
-            // Image Projection
-            if (m_uiImageTimer < uiDiff)
-            {
-                if (DoCastSpellIfCan(m_creature, SPELL_SUMMON_PROJECTION, CF_INTERRUPT_PREVIOUS) == CAST_OK)
-                {
-                    DoCastSpellIfCan(m_creature, SPELL_IMAGE_PROJECTION, CF_TRIGGERED);
-                    m_uiImageTimer = urand(17700, 25300);
-                    m_uiHideTimer = 5800;
-                    m_isProjection = true;
-                }
-            }
-            else 
-                m_uiImageTimer -= uiDiff;           
-        }
-        else
-        {
-            // projected phase
-            if (m_uiHideTimer < uiDiff)
-            {
-                if (!m_creature->HasAura(SPELL_IMAGE_PROJECTION))
-                {
-                    if (DoCastSpellIfCan(m_creature, SPELL_PROJECTION_LEECH, CF_TRIGGERED | CF_INTERRUPT_PREVIOUS) == CAST_OK)
-                    {
-                        m_isProjection = false;
-                        return;
-                    }
-                }
-
-                m_uiHideTimer = 200;
-            }
-            else 
-                m_uiHideTimer -= uiDiff;            
-        }
-    }
-};
-
-CreatureAI* GetAI_npc_spectral_tutor(Creature* pCreature)
-{
-    return new npc_spectral_tutorAI(pCreature);
-}
-
 struct npc_spectral_projectionAI : public ScriptedAI
 {
     npc_spectral_projectionAI(Creature* pCreature) : ScriptedAI(pCreature) { npc_spectral_projectionAI::Reset(); }
@@ -285,11 +182,6 @@ void AddSC_scholo_trash()
     script = new Script;
     script->Name = "npc_reanimated_corpse";
     script->GetAI = &GetAI_npc_reanimated_corpse;
-    script->RegisterSelf();
-
-    script = new Script;
-    script->Name = "npc_spectral_tutor";
-    script->GetAI = &GetAI_npc_spectral_tutor;
     script->RegisterSelf();
 
     script = new Script;
