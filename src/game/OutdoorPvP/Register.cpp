16387d05e60812af91b710808408a4536c459c94
bladez <bladez.lightshope@gmail.com>
2018-06-16 21:01:01 +1000

    Re-add patch checks for outdoor PVP alongside config options at a lower level

--- src/game/OutdoorPvP/Register.cpp
+++ src/game/OutdoorPvP/Register.cpp
@@ -5,8 +5,8 @@ void AddSC_outdoorpvp_si();
 
 void RegisterZoneScripts()
 {
-    if (sWorld.getConfig(CONFIG_BOOL_OUTDOORPVP_EP_ENABLE))
+    if (sWorld.GetWowPatch() >= WOW_PATCH_112 && sWorld.getConfig(CONFIG_BOOL_OUTDOORPVP_EP_ENABLE))
         AddSC_outdoorpvp_ep();
-    if (sWorld.getConfig(CONFIG_BOOL_OUTDOORPVP_SI_ENABLE))
+    if (sWorld.GetWowPatch() >= WOW_PATCH_112 && sWorld.getConfig(CONFIG_BOOL_OUTDOORPVP_SI_ENABLE))
         AddSC_outdoorpvp_si();
 }
