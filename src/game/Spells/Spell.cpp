b608967a7e497c63feabd2d0a88025f57f2bacae
Nølin <20132236+Nolin7777@users.noreply.github.com>
2018-08-31 05:05:55 +0200

    Stoneform should be able to cast when disoriented, stunned, etc  (#4)
    * Stoneform should be able to cast when disoriented, stunned, etc
    
    * Tabs

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -5546,9 +5546,11 @@ SpellCastResult Spell::CheckCast(bool strict)
     
     // Nostalrius: impossible to cast spells while banned / feared / confused ...
     // Except divine shields, pvp trinkets for example
+    // Stoneform is explicitly excluded, it should be allowed to cast always 
     // TODO: This condition allows an antifear item to be used while stuned for example.
     if (!m_IsTriggeredSpell && !IsSpellAppliesAura(m_spellInfo, SPELL_AURA_SCHOOL_IMMUNITY) && !IsSpellAppliesAura(m_spellInfo, SPELL_AURA_MECHANIC_IMMUNITY) &&
-            m_caster->hasUnitState(UNIT_STAT_ISOLATED | UNIT_STAT_STUNNED | UNIT_STAT_PENDING_STUNNED | UNIT_STAT_CONFUSED | UNIT_STAT_FLEEING))
+         m_spellInfo->Id != 20594 && // Stoneform
+	 m_caster->hasUnitState(UNIT_STAT_ISOLATED | UNIT_STAT_STUNNED | UNIT_STAT_PENDING_STUNNED | UNIT_STAT_CONFUSED | UNIT_STAT_FLEEING))
         return SPELL_FAILED_DONT_REPORT;
 
     // zone check
