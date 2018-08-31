4310205563054882a5330bd774976969a5bf9293
Nølin <20132236+Nolin7777@users.noreply.github.com>
2018-08-31 04:58:23 +0200

    Config to enable/disable Player commands (#11)

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -709,6 +709,8 @@ void World::LoadConfigSettings(bool reload)
 
     setConfig(CONFIG_BOOL_KICK_PLAYER_ON_BAD_PACKET, "Network.KickOnBadPacket", false);
 
+    setConfig(CONFIG_BOOL_PLAYER_COMMANDS, "PlayerCommands", true);
+
     setConfig(CONFIG_UINT32_INSTANT_LOGOUT, "InstantLogout", SEC_MODERATOR);
 
     setConfigMin(CONFIG_UINT32_GROUP_OFFLINE_LEADER_DELAY, "Group.OfflineLeaderDelay", 300, 0);
