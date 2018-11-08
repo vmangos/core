318830ed396e46ccb1c5e21f14d1cceccc9f8502
ratkosrb <ratkomladic2@abv.bg>
2018-11-08 03:06:43 +0200

    Remove extra spaces.

--- src/game/Spells/SpellEntry.h
+++ src/game/Spells/SpellEntry.h
@@ -1,8 +1,8 @@
 /*
- * Copyright (C) 2005-2011 MaNGOS <http:   //getmangos.com/>
- * Copyright (C) 2009-2011 MaNGOSZero <https:   //github.com/mangos/zero>
- * Copyright (C) 2011-2016 Nostalrius <https:   //nostalrius.org>
- * Copyright (C) 2016-2017 Elysium Project <https:   //github.com/elysium-project>
+ * Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2009-2011 MaNGOSZero <https://github.com/mangos/zero>
+ * Copyright (C) 2011-2016 Nostalrius <https://nostalrius.org>
+ * Copyright (C) 2016-2017 Elysium Project <https://github.com/elysium-project>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@
 #include "DBCStructure.h"
 #include "SharedDefines.h"
 
-   // Diminishing Returns interaction with spells
+// Diminishing Returns interaction with spells
 bool IsDiminishingReturnsGroupDurationLimited(DiminishingGroup group);
 DiminishingReturnsType GetDiminishingReturnsGroupType(DiminishingGroup group);
 float GetDiminishingRate(uint32 type);
