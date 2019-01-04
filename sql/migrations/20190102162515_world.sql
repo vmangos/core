96eaf9d8b071163858a505790337a852fb4e26a6
ratkosrb <ratkomladic2@abv.bg>
2019-01-04 16:00:46 +0200

    New script for quest Protect Kaya.

--- sql/migrations/20190102162515_world.sql
+++ sql/migrations/20190102162515_world.sql
@@ -8,6 +8,7 @@ IF v=0 THEN
 INSERT INTO `migrations` VALUES ('20190102162515');
 -- Add your query below.
 
+
 -- Add Levitate aura to Dream Vision npc.
 INSERT INTO `creature_template_addon` (`entry`, `patch`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`, `auras`) VALUES (7863, 0, 0, 0, 1, 40, 0, 0, '27986');
 
