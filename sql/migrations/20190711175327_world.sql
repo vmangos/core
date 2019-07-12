860587e7727f6c24a3abc1b42c9a57d5cae25d7d
ratkosrb <ratkomladic2@abv.bg>
2019-07-12 22:56:04 +0300

    Correct school vulnerability values based on bestiary.

--- sql/migrations/20190711175327_world.sql
+++ sql/migrations/20190711175327_world.sql
@@ -11,14 +11,16 @@ INSERT INTO `migrations` VALUES ('20190711175327');
 
 -- Molten War Golem should have massive frost vulnerability.
 -- https://www.wowhead.com/npc=8908/molten-war-golem#comments
-UPDATE `creature_template` SET `frost_res`=-10000 WHERE `entry`=8908;
+-- Assuming the giant hits reported in wowhead comments were a
+-- wotlk bug, because they are all from 2009/2010.
+UPDATE `creature_template` SET `frost_res`=-140 WHERE `entry`=8908;
 
 -- Tar mobs should have fire vulnerability.
 -- https://classic.wowhead.com/npc=6517/tar-beast#comments
 -- https://classic.wowhead.com/npc=6527/tar-creeper#screenshots
 -- https://classic.wowhead.com/npc=6519/tar-lord#comments
 -- https://classic.wowhead.com/npc=6518/tar-lurker#comments
-UPDATE `creature_template` SET `fire_res`=-300 WHERE `entry` IN (6518, 6519, 6527, 6517);
+UPDATE `creature_template` SET `fire_res`=-51 WHERE `entry` IN (6518, 6519, 6527, 6517);
 
 -- Creatures with vulnerability in cmangos db.
 UPDATE `creature_template` SET `frost_res`=-52 WHERE `entry`=8909;
@@ -26,7 +28,6 @@ UPDATE `creature_template` SET `fire_res`=-58 WHERE `entry`=11461;
 UPDATE `creature_template` SET `fire_res`=-55 WHERE `entry`=11462;
 UPDATE `creature_template` SET `fire_res`=-56 WHERE `entry`=11464;
 UPDATE `creature_template` SET `fire_res`=-58 WHERE `entry`=11465;
-UPDATE `creature_template` SET `frost_res`=-40 WHERE `entry`=11502;
 UPDATE `creature_template` SET `frost_res`=-93 WHERE `entry`=11664;
 UPDATE `creature_template` SET `frost_res`=-93 WHERE `entry`=11666;
 UPDATE `creature_template` SET `frost_res`=-93 WHERE `entry`=11667;
