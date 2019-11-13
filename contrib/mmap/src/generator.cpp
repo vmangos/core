b00f3cfaa3604b76f9377cacf522e78495b1298e
ratkosrb <ratkomladic2@abv.bg>
2019-11-13 16:26:44 +0200

    Missed change from last commit.

--- contrib/mmap/src/generator.cpp
+++ contrib/mmap/src/generator.cpp
@@ -76,7 +76,7 @@ void printUsage()
     printf("--offMeshInput [file.*] : Path to file containing off mesh connections data.\n\n");
     printf("Example:\nmovemapgen (generate all mmap with default arg\n"
            "movemapgen 0 (generate map 0)\n"
-           "movemapgen --tile 34,46 (builds only tile 34,46 of map 0)\n\n");
+           "movemapgen 0 --tile 34,46 (builds only tile 34,46 of map 0)\n\n");
     printf("Please read readme file for more information and examples.\n");
 }
 
