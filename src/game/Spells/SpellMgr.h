060ee7ade64624152b38c812a7ed4eb766d7318d
ratkosrb <ratkomladic2@abv.bg>
2020-01-09 22:31:38 +0200

    Try to fix Travis build.

--- src/game/Spells/SpellMgr.h
+++ src/game/Spells/SpellMgr.h
@@ -38,6 +38,7 @@
 class Player;
 class Spell;
 class Unit;
+struct WorldLocation;
 
 // Spell affects related declarations (accessed using SpellMgr functions)
 typedef std::map<uint32, uint64> SpellAffectMap;
