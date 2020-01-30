aefea517531c6410d79575741f4997abd4bcad0d
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2020-01-30 19:01:41 +0200

    Add condition for gossip option

--- sql/migrations/20200129094000_world.sql
+++ sql/migrations/20200129094000_world.sql
@@ -8,17 +8,12 @@ IF v=0 THEN
 INSERT INTO `migrations` VALUES ('20200129094000');
 -- Add your query below.
 
--- Added gossip for npc 2239 (Drull) with english & german locales
-update `creature_template` set gossip_menu_id=262 where entry=2239;
-
--- Greeting
-insert into `gossip_menu` (`entry`, `text_id`, `condition_id`) values('262','759','0');
-
--- Gossip Option
-insert into `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `OptionBroadcastTextID`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `BoxBroadcastTextID`, `condition_id`) values('262','0','0','How can I free you?','2746','1','3','261','0','0','0','0',NULL,'0','0');
-
--- Gossip Option Page
-insert into `gossip_menu` (`entry`, `text_id`, `condition_id`) values('261','760','0');
+-- Added gossip for npc 2239 (Drull)
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (270, 9, 498, 0, 0, 0, 0);
+INSERT INTO `gossip_menu` (`entry`, `text_id`, `condition_id`) VALUES (262, 759, 0);
+INSERT INTO `gossip_menu` (`entry`, `text_id`, `condition_id`) VALUES (261, 760, 0);
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `OptionBroadcastTextID`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `BoxBroadcastTextID`, `condition_id`) VALUES (262, 0, 0, 'How can I free you?', 2743, 1, 1, 261, 0, 0, 0, 0, NULL, 0, 270);
+UPDATE `creature_template` SET `gossip_menu_id`=262 WHERE `entry`=2239;
 
 
 END IF;
