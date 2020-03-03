584f9a069a8caa9d1de47ecc3ec4911fb0c5420f
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2020-03-03 20:41:12 +0200

    Remove event ai data.

--- sql/migrations/20200303121200_world.sql
+++ sql/migrations/20200303121200_world.sql
@@ -8,7 +8,10 @@ IF v=0 THEN
 INSERT INTO `migrations` VALUES ('20200303121200');
 -- Add your query below.
 
-UPDATE `creature_template` SET ai_name="" WHERE entry=1502;  
+UPDATE `creature_template` SET `ai_name`='' WHERE `entry`=1502;
+DELETE FROM `creature_ai_scripts` WHERE `id` IN (150201);
+DELETE FROM `creature_ai_events` WHERE `creature_id`=1502;
+
 
 -- End of migration.
 END IF;
