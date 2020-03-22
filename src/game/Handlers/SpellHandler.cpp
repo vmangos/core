57d20a566243f52adc780475060a775ba9ad9fd2
bing <bing2008@126.com>
2020-03-22 16:06:49 +0800

    fix eluna OnUse handler.

--- src/game/Handlers/SpellHandler.cpp
+++ src/game/Handlers/SpellHandler.cpp
@@ -32,6 +32,10 @@
 #include "Totem.h"
 #include "SpellAuras.h"
 
+#ifdef ENABLE_ELUNA
+#include "LuaEngine.h"
+#endif /* ENABLE_ELUNA */
+
 using namespace Spells;
 
 void WorldSession::HandleUseItemOpcode(WorldPacket& recvPacket)
@@ -151,7 +155,18 @@ void WorldSession::HandleUseItemOpcode(WorldPacket& recvPacket)
         return;
     }
 
-    pUser->CastItemUseSpell(pItem, targets);
+#ifdef ENABLE_ELUNA
+	// Note: If script stop casting it must send appropriate data to client to prevent stuck item in gray state.
+	if (sEluna->OnUse(pUser, pItem, targets))
+	{
+		// no script or script not process request by self
+		pUser->CastItemUseSpell(pItem, targets);
+	}
+#else
+		pUser->CastItemUseSpell(pItem, targets);
+#endif
+
+
 }
 
 #define OPEN_CHEST 11437
