70cd34782a4c03873386d054866f62efa21f38a0
shenna <realshenna@gmail.com>
2020-04-05 04:04:54 +0200

    Added missing GO for Create House (TEST).
    * Source: Effect #1 (81) Create House (20668)
    * https://classicdb.ch/?spell=6757

---
+++ sql/migrations/20200405014848_world.sql
@@ -0,0 +1,18 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20200405014848');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20200405014848');
+-- Add your query below.
+
+replace into `gameobject_template` (`entry`, `patch`, `type`, `displayId`, `name`, `faction`, `flags`, `size`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `mingold`, `maxgold`, `script_name`) values (20668, 0, 14, 467, 'Player\'s House', 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
