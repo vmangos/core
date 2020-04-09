922d39656aac0f0df6bbe5bc733af0820ed5f622
NickTyrer <nickyty8@gmail.com>
2020-04-09 20:38:58 +0100

    add quest template update

--- sql/migrations/20200409171600_world.sql
+++ sql/migrations/20200409171600_world.sql
@@ -1,3 +1,14 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20200405163750');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20200405163750');
+-- Add your query below.
+
+
 --remove all incorrect trainers from An Imps Request (8419) Richard Kerwin (4565), Luther Pickman (4564), Ursula Deline (5495), Sandahl (5496), Thistleheart (5171), Alexander Calder (5173), Grol'dar (3324), Mirket (3325) and Kartosh (988)
 DELETE FROM mangos.creature_questrelation WHERE quest = 8419 AND id NOT IN(461,3326,4563);
 
@@ -18,3 +29,12 @@ DELETE FROM mangos.creature_questrelation WHERE quest = 4736 AND id = 3326;
 
 --add correct completion text for Gakin's Summons (1717)
 UPDATE quest_template SET OfferRewardText = "I'm glad someone was able to get a hold of you, $n. I was beginning to fear the worst. It would be a shame to lose you, after the time we've invested in you.$b$bWe can talk later, though. Your training is the only important thing now." WHERE entry = 1717;
+
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
+
