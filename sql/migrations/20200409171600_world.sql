9092b7431dd65e27df214945149c3c10627ebf96
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2020-04-17 09:41:59 +0300

    Missing space for comments

--- sql/migrations/20200409171600_world.sql
+++ sql/migrations/20200409171600_world.sql
@@ -9,49 +9,49 @@ INSERT INTO `migrations` VALUES ('20200409171600');
 -- Add your query below.
 
 
---remove all incorrect trainers from An Imps Request (8419) Richard Kerwin (4565), Luther Pickman (4564), Ursula Deline (5495), Sandahl (5496), Thistleheart (5171), Alexander Calder (5173), Grol'dar (3324), Mirket (3325) and Kartosh (988)
+-- remove all incorrect trainers from An Imps Request (8419) Richard Kerwin (4565), Luther Pickman (4564), Ursula Deline (5495), Sandahl (5496), Thistleheart (5171), Alexander Calder (5173), Grol'dar (3324), Mirket (3325) and Kartosh (988)
 DELETE FROM `creature_questrelation` WHERE `quest` = 8419 AND `id` NOT IN(461,3326,4563);
 
---add An Imps Request (8419) to Briarthorn (5172)
+-- add An Imps Request (8419) to Briarthorn (5172)
 INSERT INTO `creature_questrelation` (`id`, `quest`, `patch_min`, `patch_max`) VALUES (5172, 8419, 6, 10);
 
---add In Search of Menara Voidrender (4736) to Briarthorn (5172)
+-- add In Search of Menara Voidrender (4736) to Briarthorn (5172)
 INSERT INTO `creature_questrelation` (`id`, `quest`, `patch_min`, `patch_max`) VALUES (5172, 4736, 0, 10);
 
---remove quest Mor'zul Bloodbringer (7562) from Greshka (12807)
+-- remove quest Mor'zul Bloodbringer (7562) from Greshka (12807)
 DELETE FROM `creature_questrelation` WHERE `quest` = 7562 AND `id` = 12807;
 
---add Mor'zul Bloodbringer (7562) to Martha Strain (5753)
+-- add Mor'zul Bloodbringer (7562) to Martha Strain (5753)
 INSERT INTO `creature_questrelation` (`id`, `quest`, `patch_min`, `patch_max`) VALUES (5753, 7562, 2, 10);
 
---remove In Search of Menara Voidrender (3326) from Zervost (3326)
+-- remove In Search of Menara Voidrender (3326) from Zervost (3326)
 DELETE FROM `creature_questrelation` WHERE `quest` = 4736 AND `id` = 3326;
 
---add correct completion text for Gakin's Summons (1717)
+-- add correct completion text for Gakin's Summons (1717)
 UPDATE `quest_template` SET `OfferRewardText` = "I'm glad someone was able to get a hold of you, $n. I was beginning to fear the worst. It would be a shame to lose you, after the time we've invested in you.$b$bWe can talk later, though. Your training is the only important thing now." WHERE `entry` = 1717;
 
---remove Grimoire of Lash of Pain (Rank 6) from Cylina Darkheart (6374)
+-- remove Grimoire of Lash of Pain (Rank 6) from Cylina Darkheart (6374)
 DELETE FROM `npc_vendor` WHERE `entry` = 6374 AND `item` = 16374;
 
---remove Grimoire of Soothing Kiss (Rank 2) from Wren Darkspring (6376)
+-- remove Grimoire of Soothing Kiss (Rank 2) from Wren Darkspring (6376)
 DELETE FROM `npc_vendor` WHERE `entry` = 6376 AND `item` = 16376;
 
---remove Grimoire of Lash of Pain (Rank 5) from Dane Winslow (6373)
+-- remove Grimoire of Lash of Pain (Rank 5) from Dane Winslow (6373)
 DELETE FROM `npc_vendor` WHERE `entry` = 6373 AND `item` = 16373;
 
---add Devour Magic Rank 3 (16382) to Martha Strain (5753)
+-- add Devour Magic Rank 3 (16382) to Martha Strain (5753)
 INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `itemflags`) VALUES (5753, 16382, 0, 0, 0);
 
---add Devour Magic Rank 3 (16382) to Kurgul (5815)
+-- add Devour Magic Rank 3 (16382) to Kurgul (5815)
 INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `itemflags`) VALUES (5815, 16382, 0, 0, 0);
 
---add Devour Magic Rank 3 (16382) to Spackle Thornberry (5520)
+-- add Devour Magic Rank 3 (16382) to Spackle Thornberry (5520)
 INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `itemflags`) VALUES (5520, 16382, 0, 0, 0);
 
---add Helfire Rank 1 (5709) to Kartosh (988)
+-- add Helfire Rank 1 (5709) to Kartosh (988)
 INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`, `build_min`, `build_max`) VALUES (988, 5709, 6000, 0, 0, 30, 0, 5875);
 
---add missing spells to Richard Kerwin (4565)
+-- add missing spells to Richard Kerwin (4565)
 DELETE FROM `npc_trainer` WHERE `entry`=4565;
 INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`, `build_min`, `build_max`) VALUES
 (4565, 1197, 5000, 0, 0, 28, 0, 5875),
@@ -201,10 +201,10 @@ INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillv
 (4565, 20768, 15000, 0, 0, 50, 0, 5875),
 (4565, 20769, 26000, 0, 0, 60, 0, 5875);
 
---add correct completion text for Doomcaller's Robes (8661)
+-- add correct completion text for Doomcaller's Robes (8661)
 UPDATE `quest_template` SET `OfferRewardText` = "I did not expect you to return from your attempt, $n.$b$bYou've fulfilled a destiny greater than that of most immortal beings.$b$bBy slaying C'Thun you've not only saved all of Azeroth, you've also tipped the scales of cosmic events beyond your comprehension.$b$bAccept these robes as a symbol of the glory and burden that will come as a consequence of your actions.$b$bMay their power aid you in the challenges that await you, god-slayer!" WHERE `entry` = 8661;
 
---add correct completion text for Doomcaller's Robes (8661)
+-- add correct completion text for Doomcaller's Robes (8661)
 UPDATE `quest_template` SET `RequestItemsText` = "At his dwelling in Ahn'Qiraj, dread C'Thun awaits in slumber." WHERE `entry` = 8661;
 
 
