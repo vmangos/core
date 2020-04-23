e2360e11a0bfaa9b0d4c2eec983e55e14e0eac0b
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2020-04-23 21:24:25 +0300

    caps

--- sql/migrations/20200422203517_world.sql
+++ sql/migrations/20200422203517_world.sql
@@ -9,7 +9,7 @@ INSERT INTO `migrations` VALUES ('20200422203517');
 -- Add your query below.
 
 -- Required quest for 3981 must be 3906, not 3907. https://classic.wowhead.com/quest=3981/
-UPDATE `quest_template` SET `PrevQuestId` = 3906 where `entry` = 3981;
+UPDATE `quest_template` SET `PrevQuestId` = 3906 WHERE `entry` = 3981;
 
 -- End of migration.
 END IF;
