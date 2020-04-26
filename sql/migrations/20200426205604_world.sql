065102b8028b76d837c23f56ff415b03d5442d92
NickTyrer <nickyty8@gmail.com>
2020-04-26 21:05:50 +0100

    remove_unused_baner

---
+++ sql/migrations/20200426205604_world.sql
@@ -0,0 +1,19 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20200426205604');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20200426205604');
+-- Add your query below.
+
+-- remove unused event game object in ironforge
+DELETE FROM `game_event_gameobject` WHERE `guid` IN (4771, 5051);
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
