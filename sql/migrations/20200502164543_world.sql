5964fe80148545695a98187432eb53efcf9c6e00
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2020-05-02 21:43:13 +0300

    Correct positions

--- sql/migrations/20200502164543_world.sql
+++ sql/migrations/20200502164543_world.sql
@@ -10,10 +10,10 @@ INSERT INTO `migrations` VALUES ('20200502164543');
 
 
 -- Cenarion Emissary Blackhoof correct location
-UPDATE `creature` SET `position_x` = 1658.71, `position_y` = -4348.8, `position_z` = 29.2574, `orientation` = 4.39823 WHERE `guid` = 6504;
+UPDATE `creature` SET `position_x` = 1658.711, `position_y` = -4348.799, `position_z` = 29.25742, `orientation` = 4.39823 WHERE `guid` = 6504;
 
 -- Grinkle correct location
-UPDATE `creature` SET `position_x` = 1592.55, `position_y` = -4416.74, `position_z` = 9.3024, `orientation` = 3.373852 WHERE `guid` = 54742;
+UPDATE `creature` SET `position_x` = 1592.611, `position_y` = -4416.818, `position_z` = 9.398685, `orientation` = 2.949606 WHERE `guid` = 54742;
 
 
 -- End of migration.
