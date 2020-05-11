05dbe33ad2cdaed968db26e1851f32c24c4551f3
NickTyrer <nickyty8@gmail.com>
2020-05-11 16:33:48 +0100

    correct_more_trainers

--- sql/migrations/20200508153112_world.sql
+++ sql/migrations/20200508153112_world.sql
@@ -42,6 +42,40 @@ DELETE FROM `npc_trainer` WHERE `entry` = 11044 AND `spell` NOT IN (2275,3184, 2
 -- remove incorrect recipes from Ghak Healtouch
 DELETE FROM `npc_trainer` WHERE `entry` = 1470 AND `spell` NOT IN (2275,3184, 2339, 2340, 11536, 2341);
 
+-- remove mighty troll's blood potion from sylvanna forestmoon
+DELETE FROM `npc_trainer` WHERE `entry` = 11042 AND `spell` = 13030;
+
+-- remove mighty troll's blood potion from yelmak
+DELETE FROM `npc_trainer` WHERE `entry` = 3347 AND `spell` = 13030;
+
+-- add correct recipes to ainethil
+INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`, `build_min`, `build_max`) VALUES
+(4160, 2341, 1000, 171, 55, 0, 0, 5875),
+(4160, 3178, 5400, 171, 175, 0, 0, 5875),
+(4160, 11488, 8100, 171, 205, 0, 0, 5875),
+(4160, 11486, 7200, 171, 205, 0, 0, 5875),
+(4160, 22809, 9000, 171, 215, 0, 0, 5875),
+(4160, 11491, 9000, 171, 215, 0, 0, 5875);
+
+-- add correct recipes to doctor herbert halsey
+INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`, `build_min`, `build_max`) VALUES
+(4611, 2341, 1000, 171, 55, 0, 0, 5875),
+(4611, 3178, 5400, 171, 175, 0, 0, 5875),
+(4611, 11488, 8100, 171, 205, 0, 0, 5875),
+(4611, 11486, 7200, 171, 205, 0, 0, 5875),
+(4611, 22809, 9000, 171, 215, 0, 0, 5875),
+(4611, 11491, 9000, 171, 215, 0, 0, 5875);
+
+-- add correct recipes to rogvar
+INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`, `build_min`, `build_max`) VALUES
+(1386, 2341, 1000, 171, 55, 0, 0, 5875),
+(1386, 3178, 5400, 171, 175, 0, 0, 5875);
+
+-- add correct recipes to kylanna windwhisper
+INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`, `build_min`, `build_max`) VALUES
+(7948, 2341, 1000, 171, 55, 0, 0, 5875),
+(7948, 3178, 5400, 171, 175, 0, 0, 5875);
+
 
 -- End of migration.
 END IF;
