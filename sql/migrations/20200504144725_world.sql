1d6c4aa10d5ebd6ba3a9362dff861e46188f81f6
NickTyrer <nickyty8@gmail.com>
2020-05-13 07:35:19 +0100

    update_location

--- sql/migrations/20200504144725_world.sql
+++ sql/migrations/20200504144725_world.sql
@@ -10,7 +10,7 @@ INSERT INTO `migrations` VALUES ('20200504144725');
 
 
 -- Alowicious Czervik correct location
-UPDATE `creature` SET `position_x` = 1660.02, `position_y` = -4391.0, `position_z` = 23.2680, `orientation` = 4.771000 WHERE `guid` = 86237;
+UPDATE `creature` SET `position_x` = 1627.165, `position_y` = -4412.519, `position_z` = 15.453, `orientation` = 4.771000 WHERE `guid` = 86237;
 
 -- Opus correct location
 UPDATE `creature` SET `position_x` = -7468.840820, `position_y` = -2186.785156, `position_z` = 164.418686, `orientation` = 2.294904, `movement_type` = 0, `wander_distance` = 0 WHERE `guid` = 4567;
