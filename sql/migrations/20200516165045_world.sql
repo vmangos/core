2dc6295fd080b15886088e9470777691c5430419
NickTyrer <nickyty8@gmail.com>
2020-05-16 16:53:24 +0100

    add_movement

---
+++ sql/migrations/20200516165045_world.sql
@@ -0,0 +1,34 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20200516165045');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20200516165045');
+-- Add your query below.
+
+
+-- pathing and scripts for kogan forgestone
+UPDATE `creature` SET `movement_type`= 2 WHERE `id`= 1245;
+INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
+(1245, 1, -5575.4365, -430.8772, 397.326080, 3.9919, 35000, 0, 0),
+(1245, 2, -5576.37, -429.771, 397.326, 2.9147, 15000, 0, 1245),
+(1245, 3, -5576.37, -429.771, 397.326, 2.9147, 0, 0, 1246);
+INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1245, 5, 1, 233, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'New Action - Edit me!');
+INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1246, 0, 1, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'New Action - Edit me!');
+
+-- pathing and scripts for tognus flintfire
+UPDATE `creature` SET `movement_type`= 2 WHERE `id`= 1241;
+INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
+(1241, 1, -5582.41, -430.97, 397.326, 5.39307, 25000, 0, 0),
+(1241, 2, -5582.00, -429.721, 397.325, 0.8414, 17000, 0, 1245),
+(1241, 3, -5582.00, -429.721, 397.325, 0.8414, 0, 0, 1246);
+
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
