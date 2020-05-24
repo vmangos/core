285a01179fb3d6ab593467f0dea51f855eef88e1
NickTyrer <nickyty8@gmail.com>
2020-05-24 20:59:03 +0100

    add waypoints

--- sql/migrations/20200524180338_world.sql
+++ sql/migrations/20200524180338_world.sql
@@ -23,7 +23,8 @@ INSERT INTO `creature_movement_special` (`id`, `point`, `position_x`, `position_
 (504301, 3, -8754.53, 854.314, 82.5392, 100, 0, 0, 0),
 (504301, 4, -8762.12, 848.166, 86.1997, 100, 0, 0, 0),
 (504301, 5, -8764.43, 846.379, 87.2971, 100, 0, 0, 0),
-(504301, 6, -8772.04, 840.429, 90.9329, 100, 0, 0, 0);
+(504301, 6, -8772.04, 840.429, 90.9329, 100, 0, 0, 0),
+(504301, 7, -8780.68, 833.842, 95.0307, 100, 0, 0, 0);
 
 -- add left lane pathing
 INSERT INTO `creature_movement_special` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
@@ -32,7 +33,8 @@ INSERT INTO `creature_movement_special` (`id`, `point`, `position_x`, `position_
 (504302, 3, -8753.21, 852.692, 82.5292, 100, 0, 0, 0),
 (504302, 4, -8760.63, 846.642, 86.1184, 100, 0, 0, 0),
 (504302, 5, -8762.88, 844.698, 87.2342, 100, 0, 0, 0),
-(504302, 6, -8770.44, 838.699, 90.8653, 100, 0, 0, 0);
+(504302, 6, -8770.44, 838.699, 90.8653, 100, 0, 0, 0),
+(504302, 7, -8778.45, 831.800, 94.8518, 100, 0, 0, 0);
 
 -- add right lane pathing
 INSERT INTO `creature_movement_special` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
@@ -41,7 +43,8 @@ INSERT INTO `creature_movement_special` (`id`, `point`, `position_x`, `position_
 (504303, 3, -8755.53, 855.885, 82.4662, 100, 0, 0, 0),
 (504303, 4, -8763.28, 849.84, 86.1526, 100, 0, 0, 0),
 (504303, 5, -8765.64, 847.962, 87.2845, 100, 0, 0, 0),
-(504303, 6, -8773.36, 842.437, 90.8508, 100, 0, 0, 0);
+(504303, 6, -8773.36, 842.437, 90.8508, 100, 0, 0, 0),
+(504303, 7, -8781.85, 835.478, 94.9935, 100, 0, 0, 0);
 
 -- add event
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (5043, 5043, 0, 1, 0, 100, 1, 1800000, 1800000, 1800000, 1800000, 504301, 0, 0, 'start - stockade jailbreak');
