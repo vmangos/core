34ce8480ee11313ce7a5764e38108f1b3f475db7
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2020-05-25 16:25:02 +0300

    Correct exact positions

--- sql/migrations/20200516165045_world.sql
+++ sql/migrations/20200516165045_world.sql
@@ -12,9 +12,9 @@ INSERT INTO `migrations` VALUES ('20200516165045');
 -- pathing and scripts for kogan forgestone
 UPDATE `creature` SET `movement_type`= 2 WHERE `id`= 1245;
 INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
-(1245, 1, -5575.4365, -430.8772, 397.326080, 3.9919, 35000, 0, 0),
-(1245, 2, -5576.37, -429.771, 397.326, 2.9147, 15000, 0, 1245),
-(1245, 3, -5576.37, -429.771, 397.326, 2.9147, 0, 0, 1246);
+(1245, 1, -5575.67, -430.965, 397.325, 3.89208, 35000, 0, 0),
+(1245, 2, -5576.37, -429.771, 397.325, 2.9147, 15000, 0, 1245),
+(1245, 3, -5576.37, -429.771, 397.325, 2.9147, 0, 0, 1246);
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1245, 5, 1, 233, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Start work - mining');
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1246, 0, 1, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Stop work - mining');
 
@@ -22,8 +22,8 @@ INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `
 UPDATE `creature` SET `movement_type`= 2 WHERE `id`= 1241;
 INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
 (1241, 1, -5582.41, -430.97, 397.326, 5.39307, 25000, 0, 0),
-(1241, 2, -5582.00, -429.721, 397.325, 0.8414, 17000, 0, 1241),
-(1241, 3, -5582.00, -429.721, 397.325, 0.8414, 0, 0, 1242);
+(1241, 2, -5581.89, -429.616, 397.325, 100, 17000, 0, 1241),
+(1241, 3, -5581.89, -429.616, 397.325, 100, 0, 0, 1242);
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1242, 0, 1, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'tognus flintfire - stop work');
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1242, 0, 81, 10678, 26, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'tognus flintfire - despawn mace');
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1241, 0, 81, 10679, 18, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'tognus flintfire - despawn mace');
