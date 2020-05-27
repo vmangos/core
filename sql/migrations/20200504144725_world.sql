7a767fdf01cb6d1704967c6d1db78af3180ce315
NickTyrer <nickyty8@gmail.com>
2020-05-27 16:47:39 +0100

    remove opus fix

--- sql/migrations/20200504144725_world.sql
+++ sql/migrations/20200504144725_world.sql
@@ -12,9 +12,6 @@ INSERT INTO `migrations` VALUES ('20200504144725');
 -- Alowicious Czervik correct location
 UPDATE `creature` SET `position_x` = 1627.165, `position_y` = -4412.519, `position_z` = 15.453, `orientation` = 4.771000 WHERE `guid` = 86237;
 
--- Opus correct location
-UPDATE `creature` SET `position_x` = -7468.840820, `position_y` = -2186.785156, `position_z` = 164.418686, `orientation` = 2.294904, `movement_type` = 0, `wander_distance` = 0 WHERE `guid` = 4567;
-
 -- correct scale for Rumstag Proudstrider
 UPDATE `creature_template` SET `scale` = 1.35 WHERE `entry` = 14728;
 
