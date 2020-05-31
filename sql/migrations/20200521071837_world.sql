f4da666c800a17e1b2e0a581df4584f4184d3e89
NickTyrer <nickyty8@gmail.com>
2020-05-31 18:07:03 +0100

    fix error

--- sql/migrations/20200521071837_world.sql
+++ sql/migrations/20200521071837_world.sql
@@ -15,7 +15,7 @@ INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `posit
 (2790, 1, -5033.81, -1022.23, 508.876, 3.76026, 28000, 0, 279001),
 (2790, 2, -5033.81, -1022.23, 508.876, 3.76026, 0, 0, 279002),
 (2790, 3, -5028.37, -1019.84, 508.875, 5.32325, 24000, 0, 279001),
-(2790, 4, -5028.37, -1019.84, 508.875, 5.32325, 0, 0, 27902),
+(2790, 4, -5028.37, -1019.84, 508.875, 5.32325, 0, 0, 279002),
 (2790, 5, -5028.9, -1022.52, 508.876, 4.294884, 26000, 0, 279001),
 (2790, 6, -5028.9, -1022.52, 508.876, 4.294884, 0, 0, 279002);
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (279001, 1, 1, 233, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'grand mason marblesten - start work');
