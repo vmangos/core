79f8df62698c42796cc1bd12f2114fb81aa4a50d
NickTyrer <nickyty8@gmail.com>
2020-06-16 08:12:46 +0100

    update brewmaister timings

--- sql/migrations/20200609200850_world.sql
+++ sql/migrations/20200609200850_world.sql
@@ -13,7 +13,7 @@ INSERT INTO `migrations` VALUES ('20200609200850');
 -- brewmeister says hiccup
 UPDATE `creature` SET `movement_type` = 1, `wander_distance` = 3 WHERE `guid` = 15314;
 UPDATE `creature_template` SET `ai_name` = "EventAI" WHERE entry = 2705;
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (270502, 2705, 0, 0, 0, 100, 1, 0, 0, 60000, 60000, 270502, 0, 0, 'Brewmeister Bilger - Speak');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (270502, 2705, 0, 0, 0, 100, 1, 10000, 20000, 60000, 80000, 270502, 0, 0, 'Brewmeister Bilger - Speak');
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (270502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1953, 0, 0, 0, 0, 0, 0, 0, 0, 'Brewmeister Bilger - Speak');
 
 
