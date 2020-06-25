bb2c496d1cacc2aa701cdf0fe6abe9d91c79e93d
NickTyrer <nickyty8@gmail.com>
2020-06-25 15:01:07 +0100

    add myra tyrngaarde script

--- sql/migrations/20200608202421_world.sql
+++ sql/migrations/20200608202421_world.sql
@@ -21,6 +21,11 @@ INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_ty
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (797801, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4021, 4019, 4020, 0, 0, 0, 0, 0, 0, 'bimble longberry - speak');
 
 
+-- add myra tyrngaarde script
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (510901, 5109, 0, 1, 0, 100, 1, 1000, 15000, 40000, 60000, 510901, 0, 0, 'myra tyrngaarde - speak');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (510901, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4014, 4013, 4015, 0, 0, 0, 0, 0, 0, 'myra tyrngaarde - speak');
+
+
 -- refuge point
 -- add guard patrol
 UPDATE `creature` SET `position_x` = -1157.48, `position_y` = -2615.69, `position_z` = 55.6105, `movement_type` = 2 WHERE `guid` = 11217;
