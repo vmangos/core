b9568b9025527e45996b046687b8913f44e60ad2
NickTyrer <nickyty8@gmail.com>
2020-06-26 16:13:42 +0100

    initial

--- sql/migrations/20200611182015_world.sql
+++ sql/migrations/20200611182015_world.sql
@@ -18,9 +18,9 @@ UPDATE `creature` SET `movement_type` = 1, `wander_distance` = 0.5 WHERE `guid` 
 -- npc's should roam
 UPDATE `creature` SET `movement_type` = 1, `wander_distance` = 1 WHERE `guid` IN (5960, 4179, 4182, 4244, 4194, 4252, 4251);
 
--- Braelyn Firehand - speak
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (419802, 4198, 0, 1, 0, 100, 1, 1000, 15000, 150000, 180000, 419802, 0, 0, 'Braelyn Firehand - Say Text OOC');
-INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (419802, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 77, 79, 0, 0, 0, 0, 0, 0, 0, 'Braelyn Firehand - Say Text');
+-- Viktori Prism Antras - speak
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (419802, 276, 0, 1, 0, 100, 1, 1000, 15000, 150000, 180000, 419802, 0, 0, 'Viktori Prism Antras - Say Text OOC');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (419802, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 77, 79, 0, 0, 0, 0, 0, 0, 0, 'Viktori Prism Antras - Say Text');
 
 -- remove incorrect guards 
 DELETE FROM `creature` WHERE `guid` IN (6133, 6127);
