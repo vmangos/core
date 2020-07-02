d9c24b0697ad60b83cee5b6072f448f67e810b81
NickTyrer <nickyty8@gmail.com>
2020-07-02 21:00:29 +0100

    Update 20200624145229_world.sql

--- sql/migrations/20200624145229_world.sql
+++ sql/migrations/20200624145229_world.sql
@@ -10,13 +10,13 @@ INSERT INTO `migrations` VALUES ('20200624145229');
 
 
 -- update progress text for Freedom for All Creatures (2969)
-UPDATE `quest_template` SET `RequestItemsText` = "We cannot allow the Grimtotem clan to destroy these beautiful creatures. They must be released from captivity!" WHERE `entry` = 2969;
+UPDATE `quest_template` SET `RequestItemsText` = 'We cannot allow the Grimtotem clan to destroy these beautiful creatures. They must be released from captivity!' WHERE `entry` = 2969;
 
 -- update progress text for Agent of Hydraxis (6823)
-UPDATE `quest_template` SET `RequestItemsText` = "Your campaign is not yet done, $n.  Go out and defeat our enemies, the servants of air and earth and fire, and one day perhaps you will earn a place in the hall of the Waterlords." WHERE `entry` = 6823;
+UPDATE `quest_template` SET `RequestItemsText` = 'Your campaign is not yet done, $n.  Go out and defeat our enemies, the servants of air and earth and fire, and one day perhaps you will earn a place in the hall of the Waterlords.' WHERE `entry` = 6823;
 
 -- update progress text for The Battle for Alterac (7142)
-UPDATE `quest_template` SET `RequestItemsText` = "We will not suffer the dwarves in our home!  Fall upon our foes and defeat their leader, general Stormpike!" WHERE `entry` = 7142;
+UPDATE `quest_template` SET `RequestItemsText` = 'We will not suffer the dwarves in our home!  Fall upon our foes and defeat their leader, general Stormpike!' WHERE `entry` = 7142;
 
 
 -- End of migration.
