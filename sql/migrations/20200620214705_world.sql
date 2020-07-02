4f5cc2701f6fa11d42110c1cc3072179c16004d6
NickTyrer <nickyty8@gmail.com>
2020-07-02 20:59:27 +0100

    Update 20200620214705_world.sql

--- sql/migrations/20200620214705_world.sql
+++ sql/migrations/20200620214705_world.sql
@@ -10,19 +10,19 @@ INSERT INTO `migrations` VALUES ('20200620214705');
 
 
 -- farmer furlbrow
-UPDATE `creature_template` SET `ai_name` = "EventAI" WHERE entry = 237;
+UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry` = 237;
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (23701, 237, 0, 1, 0, 100, 1, 75000, 90000, 150000, 180000, 23701, 0, 0, 'farmer furlbrow - say');
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (23701, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 56, 57, 0, 0, 0, 0, 0, 0, 0, 'farmer furlbrow - say');
 
 
 -- verna furlbrow
-UPDATE `creature_template` SET `ai_name` = "EventAI" WHERE entry = 238;
+UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry` = 238;
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (23801, 238, 0, 1, 0, 100, 1, 1000, 15000, 150000, 180000, 23801, 0, 0, 'verna furlbrow - speak');
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (23801, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 54, 55, 0, 0, 0, 0, 0, 0, 0, 'verna furlbrow - speak');
 
 
 -- farmer saldean
-UPDATE `creature_template` SET `ai_name` = "EventAI" WHERE entry = 238;
+UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry` = 233;
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (23301, 233, 0, 1, 0, 100, 1, 1000, 15000, 150000, 180000, 23301, 0, 0, 'farmer saldean - speak');
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (23301, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 58, 59, 60, 0, 0, 0, 0, 0, 0, 'farmer saldean - speak');
 
@@ -49,7 +49,7 @@ INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `posit
 
 
 -- update protector dorana waypoints
-DELETE FROM creature_movement WHERE id = 89532;
+DELETE FROM `creature_movement` WHERE `id` = 89532;
 UPDATE `creature` SET `position_x` = -10197.4, `position_y` = 988.166, `position_z` = 33.0215 WHERE `id` = 869;
 INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
 (869, 1, -10197.4, 988.166, 33.0215, 100, 0, 0, 0),
@@ -111,8 +111,8 @@ INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `posit
 
 
 -- update protector korelor waypoints
-DELETE FROM creature_movement_template WHERE entry = 874;
-DELETE FROM creature_movement WHERE id = 89538;
+DELETE FROM `creature_movement_template` WHERE `entry` = 874;
+DELETE FROM `creature_movement` WHERE `id` = 89538;
 UPDATE `creature` SET `position_x` = -10900.7, `position_y` = 777.566, `position_z` = 30.7594 WHERE `id` = 874;
 INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
 (874, 1, -10900.7, 777.566, 30.7594, 100, 0, 0, 0),
