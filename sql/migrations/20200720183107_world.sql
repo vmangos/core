b54bf008a147dfbac81ac5bb73c00ff94dad4dc6
NickTyrer <nickyty8@gmail.com>
2020-07-21 16:07:37 +0100

    Update 20200720183107_world.sql

--- sql/migrations/20200720183107_world.sql
+++ sql/migrations/20200720183107_world.sql
@@ -26,13 +26,19 @@ INSERT INTO `creature_addon` (`guid`, `emote`)VALUES
 (47164, 234);
 
 -- add talk emote to scarlet lumberjack
-INSERT INTO `creature_addon` (`guid`, `emote`)VALUES
-(47162, 1),
-(46782, 1),
-(47163, 1),
-(47152, 1),
-(46781, 1),
-(46783, 1);
+UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry` = 1884;
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (4716201, 1884, 4716201, 1, 0, 100, 1, 2000, 2000, 2000, 2000, 4716201, 0, 0, 'Creature GUID 47162 - Emote Talk OOC');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4716201, 0, 39, 4716201, 4716202, 4716203, 4716201, 0, 0, 0, 0, 25, 25, 25, 25, 0, 0, 0, 0, 0, 'Creature GUID 47162 - Start Script');
+INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4716201, 0, 1, 1, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Creature GUID 47162 - Emote Talk');
+INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4716202, 0, 1, 1, 0, 0, 0, 46782, 0, 9, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Creature GUID 46782 - Emote Talk');
+INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4716203, 0, 1, 1, 0, 0, 0, 47163, 0, 9, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Creature GUID 47163 - Emote Talk');
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (4716201, 52, 47162, 0, 0, 0, 0);
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (4716202, 1884, 4716202, 1, 0, 100, 1, 2000, 2000, 2000, 2000, 4716202, 0, 0, 'Creature GUID 47152 - Emote Talk OOC ');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4716202, 0, 39, 4716204, 4716205, 4716206, 4716202, 0, 0, 0, 0, 25, 25, 25, 25, 0, 0, 0, 0, 0, 'Creature GUID 47152 - Start Script');
+INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4716204, 0, 1, 1, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Creature GUID 47152 - Emote Talk');
+INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4716205, 0, 1, 1, 0, 0, 0, 46781, 0, 9, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Creature GUID 46781 - Emote Talk');
+INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4716206, 0, 1, 1, 0, 0, 0, 46783, 0, 9, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Creature GUID 46783 - Emote Talk OOC ');
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (4716202, 52, 47152, 0, 0, 0, 0);
 
 
 -- End of migration.
