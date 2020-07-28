c5ce67b205a273fb39bb48deaa87f70d79b6e689
NickTyrer <nickyty8@gmail.com>
2020-07-28 08:23:19 +0100

    Update 20200724150812_world.sql

--- sql/migrations/20200724150812_world.sql
+++ sql/migrations/20200724150812_world.sql
@@ -54,6 +54,24 @@ INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillv
 DELETE FROM `npc_trainer` WHERE `entry` IN (1355, 1699, 5482) AND `spell` = 1290;
 
 
+-- engineering
+-- correct recipes for finbus grindgear
+INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`, `build_min`, `build_max`) VALUES
+(1676, 3984, 115, 202, 30, 0, 0, 5875),
+(1676, 3985, 130, 202, 30, 0, 0, 5875),
+(1676, 3986, 150, 202, 50, 0, 0, 5875),
+(1676, 3987, 150, 202, 50, 0, 0, 5875),
+(1676, 3988, 200, 202, 60, 0, 0, 5875),
+(1676, 3991, 225, 202, 65, 0, 0, 5875),
+(1676, 3992, 250, 202, 75, 0, 0, 5875),
+(1676, 4039, 10, 0, 0, 5, 0, 5875),
+(1676, 4040, 450, 202, 50, 10, 0, 5875),
+(1676, 7431, 150, 202, 50, 0, 0, 5875);
+
+-- correct recipes for buzzek bracketswing
+DELETE FROM `npc_trainer` WHERE `entry` = 8736 AND `spell` IN (23490, 23491, 23088, 19845, 19846);
+
+
 -- End of migration.
 END IF;
 END??
