294bdbacce6b573dc05d5f76b62980344b1603b8
NickTyrer <nickyty8@gmail.com>
2020-07-29 20:38:32 +0100

    Update 20200729201638_world.sql

--- sql/migrations/20200729201638_world.sql
+++ sql/migrations/20200729201638_world.sql
@@ -36,6 +36,33 @@ INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalo
 INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1066501, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5955, 5956, 5957, 5958, 0, 0, 0, 0, 0, 'Junior Apothecary Holland - Talk');
 INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1066502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5959, 5960, 5961, 5962, 0, 0, 0, 0, 0, 'Junior Apothecary Holland - Talk');
 
+-- add creature 44663 waypoints
+UPDATE `creature` SET `movement_type` = 2, `wander_distance` = 0 WHERE `guid` = 44663;
+INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
+(44663, 1, 2670.4, 299.348, 28.1209, 100, 0, 0, 0),
+(44663, 2, 2659, 308.383, 28.1209, 100, 10000, 0, 0),
+(44663, 4, 2694.44, 284.565, 28.1151, 100, 0, 0, 0),
+(44663, 5, 2711.32, 279.584, 28.1184, 100, 0, 0, 0),
+(44663, 6, 2727.12, 277.08, 28.1184, 100, 0, 0, 0),
+(44663, 7, 2740.19, 275.201, 28.1198, 100, 0, 0, 0),
+(44663, 8, 2754.3, 273.36, 28.1338, 100, 0, 0, 0),
+(44663, 9, 2768.79, 271.439, 28.1206, 100, 0, 0, 0),
+(44663, 10, 2782.44, 270.605, 28.1205, 100, 0, 0, 0),
+(44663, 11, 2793.84, 271.129, 28.1198, 100, 0, 0, 0),
+(44663, 12, 2802.25, 278.473, 28.1451, 100, 0, 0, 0),
+(44663, 13, 2806.9, 291.665, 29.6271, 100, 0, 0, 0),
+(44663, 14, 2804.57, 283.452, 28.1978, 100, 0, 0, 0),
+(44663, 15, 2798.09, 273.742, 28.1256, 100, 0, 0, 0),
+(44663, 16, 2787.49, 270.564, 28.1214, 100, 0, 0, 0),
+(44663, 17, 2774.76, 270.973, 28.1196, 100, 0, 0, 0),
+(44663, 18, 2761.13, 272.343, 28.1236, 100, 0, 0, 0),
+(44663, 19, 2746.7, 274.236, 28.145, 100, 0, 0, 0),
+(44663, 20, 2732.86, 276.238, 28.1288, 100, 0, 0, 0),
+(44663, 21, 2718.18, 278.28, 28.1206, 100, 0, 0, 0),
+(44663, 22, 2701.65, 282.296, 28.1206, 100, 0, 0, 0),
+(44663, 23, 2687.91, 287.727, 28.1183, 100, 0, 0, 0),
+(44663, 25, 2675.35, 295.453, 28.1183, 100, 0, 0, 0);
+
 
 -- End of migration.
 END IF;
