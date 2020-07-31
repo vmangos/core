dcfe0f899dc2bbf2f3c755a1ea322492dbff8148
NickTyrer <nickyty8@gmail.com>
2020-07-31 15:59:43 +0100

    Update 20200725172743_world.sql

--- sql/migrations/20200725172743_world.sql
+++ sql/migrations/20200725172743_world.sql
@@ -9,6 +9,7 @@ INSERT INTO `migrations` VALUES ('20200725172743');
 -- Add your query below.
 
 
+-- stormwind
 -- gyrochronatom - billibub cogspinner
 UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 10800 WHERE `entry` = 5519 AND `item` = 4389;
 
@@ -82,6 +83,79 @@ UPDATE `npc_vendor` SET `maxcount` = 2 WHERE `entry` = 2805 AND `item` = 1711;
 
 -- scroll of intellect 2 - deneb walker
 UPDATE `npc_vendor` SET `maxcount` = 2 WHERE `entry` = 2805 AND `item` = 2290;
+
+
+--stv
+-- Corporal Bluth
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES
+(734, 3137),
+(734, 3108),
+(734, 3107),
+(734, 3135),
+(734, 15327),
+(734, 15326);
+
+-- Gnaz Blunderflame - Bronze Tube
+UPDATE `npc_vendor` SET `maxcount` = 1 WHERE `entry` = 2687 AND `item` = 4371;
+
+-- Gnaz Blunderflame
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES
+(2687, 4389, 1, 10800),
+(2687, 4382, 1, 10800),
+(2687, 4404, 2, 10800),
+(2687, 4363, 1, 10800),
+(2687, 4361, 2, 10800);
+
+-- Knaz Blunderflame
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES
+(8679, 4389, 1, 10800),
+(8679, 4382, 1, 10800),
+(8679, 4404, 1, 10800),
+(8679, 4363, 1, 10800),
+(8679, 4371, 2, 10800),
+(8679, 4361, 1, 10800);
+
+-- Cowardly Crosby - Admirals Hat
+UPDATE `npc_vendor` SET `maxcount` = 1 WHERE `entry` = 2672 AND `item` = 10318;
+
+-- Haren Kanmae - Dense Shortbow
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES
+(2839, 11305, 1, 7200);
+
+-- Mazk Snipeshot - Bronze Tube
+UPDATE `npc_vendor` SET `maxcount` = 1 WHERE `entry` = 2685 AND `item` = 4371;
+
+-- Mazk Snipeshot
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES
+(2685, 4389, 1, 10800),
+(2685, 4382, 1, 10800),
+(2685, 4404, 3, 10800),
+(2685, 4363, 2, 10800);
+DELETE FROM `npc_vendor` WHERE `item` IN (3033, 2519, 2516, 2102, 11363) AND `entry` = 2685;
+
+-- Kizz Bluntstrike - Ironwood Maul
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES
+(2840, 4777, 2, 86400);
+
+-- Old Man Heming
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES
+(2626, 6533, 2, 7200),
+(2626, 6365, 1, 7200);
+
+-- Xizk Goodstitch - Pattern: Crimson Silk Cloak
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 10800 WHERE `entry` = 2670 AND `item` = 7087;
+
+-- Xizk Goodstitch - Pattern: Enchanter's Cowl
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 10800 WHERE `entry` = 2670 AND `item` = 14630;
+
+-- Fargon Mortalak - Pattern: Enchanter's Cowl
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 10800 WHERE `entry` = 2845 AND `item` = 12257;
+
+-- Nerrist 
+DELETE FROM `npc_vendor` WHERE `item` IN (18256, 14341, 5956) AND `entry` = 1148;
+
+-- Uthok 
+DELETE FROM `npc_vendor` WHERE `item` IN (3131, 2946) AND `entry` = 1149;
 
 
 -- End of migration.
