9eada82e5d8845baa79604d77d0058284c30c5b5
Simplaf234-ghost <69170385+Simplaf234-ghost@users.noreply.github.com>
2020-08-04 11:48:28 +0000

    Remove comment on nonexistent XFER opcodes

--- src/realmd/AuthCodes.h
+++ src/realmd/AuthCodes.h
@@ -35,7 +35,6 @@ enum eAuthCmd
     CMD_REALM_LIST                  = 0x10,
     CMD_XFER_INITIATE               = 0x30,
     CMD_XFER_DATA                   = 0x31,
-    // these opcodes no longer exist in currently supported client
     CMD_XFER_ACCEPT                 = 0x32,
     CMD_XFER_RESUME                 = 0x33,
     CMD_XFER_CANCEL                 = 0x34
