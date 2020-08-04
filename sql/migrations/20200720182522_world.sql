5a865a01a13dc092b097079c3f8d654fde9b60f2
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2020-08-04 10:50:13 +0000

    Remove not needed escape of percent sign.

--- sql/migrations/20200720182522_world.sql
+++ sql/migrations/20200720182522_world.sql
@@ -12,7 +12,7 @@ INSERT INTO `migrations` VALUES ('20200720182522');
 -- firebrand invoker cast nova in combat
 DELETE FROM `creature_ai_events` WHERE `creature_id`=9262;
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (926201, 9262, 0, 0, 0, 100, 1, 8000, 10000, 11000, 14000, 926201, 0, 0, 'Firebrand Invoker - Fire Nova');
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (926202, 9262, 0, 2, 0, 100, 0, 15, 0, 0, 0, 926202, 0, 0, 'Firebrand Invoker - Flee at 15\% HP');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (926202, 9262, 0, 2, 0, 100, 0, 15, 0, 0, 0, 926202, 0, 0, 'Firebrand Invoker - Flee at 15% HP');
 
 
 -- End of migration.
