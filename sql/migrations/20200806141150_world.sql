74a1dd22a97e495f249e11f6d7ead1ab822c2df7
ratkosrb <ratkomladic2@abv.bg>
2020-08-06 18:03:50 +0300

    Correct name of quest 236.

--- sql/migrations/20200806141150_world.sql
+++ sql/migrations/20200806141150_world.sql
@@ -13,7 +13,7 @@ INSERT INTO `migrations` VALUES ('20200806141150');
 UPDATE `quest_template` SET `ReqCreatureOrGOCount1`=12, `RewRepValue1`=75, `RewXP`=1250, `RewOrReqMoney`=1000, `RewMoneyMaxLevel`=780 WHERE `entry`=171 && `patch`=0;
 
 -- Correct XP of Watcher Callahan to match Eight-legged Menaces.
-UPDATE `quest_template` SET `RewXP`=1250, `RewMoneyMaxLevel`=780 WHERE `entry`=236 && `patch`=0;
+UPDATE `quest_template` SET `Title`='Awaiting Word', `RewXP`=1250, `RewMoneyMaxLevel`=780 WHERE `entry`=236;
 
 
 -- End of migration.
