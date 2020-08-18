48975c1f4e4cc918d17f6bf7907978903b43d078
NickTyrer <nickyty8@gmail.com>
2020-08-18 09:20:48 +0100

    Update 20200728160635_world.sql

--- sql/migrations/20200728160635_world.sql
+++ sql/migrations/20200728160635_world.sql
@@ -19,6 +19,7 @@ INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, 
 INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (136701, 45, 0, 0, 0, 0, 0, 79702, 0, 9, 2, 144, 145, 147, 0, 0, 0, 0, 0, 0, 'Adam - Talk');
 
 -- add ol emma ooc event
+UPDATE `creature_movement` SET `script_id` = 0 WHERE `id` = 79796 AND `point` IN (7, 10, 13, 16, 22, 27);
 UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry` = 3520;
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (352001, 3520, 0, 1, 0, 100, 1, 0, 0, 180000, 180000, 352001, 0, 0, 'Ol Emma - Say OOC');
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (352002, 3520, 0, 1, 0, 100, 1, 6000, 6000, 180000, 180000, 352002, 0, 0, 'Ol Emma - Say OOC');
