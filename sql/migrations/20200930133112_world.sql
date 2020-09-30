fa7c6e97a2dd17571107f251343580c0b505b55d
ratkosrb <ratkomladic2@abv.bg>
2020-09-30 17:02:31 +0300

    Remove minimum build from spell that exist in 1.2.

--- sql/migrations/20200930133112_world.sql
+++ sql/migrations/20200930133112_world.sql
@@ -36,50 +36,50 @@ UPDATE `spell_proc_event` SET `Cooldown`=20000 WHERE `entry`=26341;
 UPDATE `spell_proc_event` SET `Cooldown`=9 WHERE `entry`=29074;
 
 INSERT INTO `spell_proc_event` (`entry`, `Cooldown`, `build_min`) VALUES 
-(7849, 5000, 4222), -- Absorption
-(14796, 8000, 4222), -- Burning Tar
+(7849, 5000, 0), -- Absorption
+(14796, 8000, 0), -- Burning Tar
 (31255, 5000, 5875), -- Deadly Swiftness
-(19194, 3000, 4222), -- Double Attack
-(19818, 3000, 4222), -- Double Attack
-(18943, 3000, 4222), -- Double Attack
-(19817, 3000, 4222), -- Double Attack
-(15852, 10000, 4222), -- Dragonbreath Chili
-(14869, 8000, 4222), -- Drunken Stupor
-(14870, 8000, 4222), -- Drunken Stupor
+(19194, 3000, 0), -- Double Attack
+(19818, 3000, 0), -- Double Attack
+(18943, 3000, 0), -- Double Attack
+(19817, 3000, 0), -- Double Attack
+(15852, 10000, 0), -- Dragonbreath Chili
+(14869, 8000, 0), -- Drunken Stupor
+(14870, 8000, 0), -- Drunken Stupor
 (29220, 10000, 5464), -- Fiery Festival Brew
-(15573, 4000, 4222), -- Fire Blast
-(16792, 2000, 4222), -- Furious Anger
-(15600, 2000, 4222), -- Hand of Justice
-(13767, 6000, 4222), -- Hate to Zero
-(18983, 30000, 4222), -- Heal on Kill
-(15876, 9000, 4222), -- Ice Blast
-(15733, 3000, 4222), -- Immolate
-(15506, 4000, 4222), -- Immolate
-(15641, 5000, 4222), -- Ironfoe
-(7446, 5000, 4222), -- Lesser Absorption
-(19449, 3000, 4222), -- Magma Spit
-(13879, 1000, 4222), -- Magma Splash
+(15573, 4000, 0), -- Fire Blast
+(16792, 2000, 0), -- Furious Anger
+(15600, 2000, 0), -- Hand of Justice
+(13767, 6000, 0), -- Hate to Zero
+(18983, 30000, 0), -- Heal on Kill
+(15876, 9000, 0), -- Ice Blast
+(15733, 3000, 0), -- Immolate
+(15506, 4000, 0), -- Immolate
+(15641, 5000, 0), -- Ironfoe
+(7446, 5000, 0), -- Lesser Absorption
+(19449, 3000, 0), -- Magma Spit
+(13879, 1000, 0), -- Magma Splash
 (23378, 3000, 4375), -- Magma Splash
-(5202, 3000, 4222), -- MHTest01
-(19362, 3000, 4222), -- MHTest03
-(7445, 5000, 4222), -- Minor Absorption
+(5202, 3000, 0), -- MHTest01
+(19362, 3000, 0), -- MHTest03
+(7445, 5000, 0), -- Minor Absorption
 (27539, 10000, 5302), -- Obsidian Armor
 (24256, 240000, 4695), -- Primal Blessing Trigger DND
-(21853, 5000, 4222), -- Reactive Fade
+(21853, 5000, 0), -- Reactive Fade
 (27561, 20000, 5302), -- Silence Trigger
-(14178, 8000, 4222), -- Sticky Tar
-(12281, 200, 4222), -- Sword Specialization (Rank 1)
-(12812, 200, 4222), -- Sword Specialization
-(12815, 200, 4222), -- Sword Specialization
-(12813, 200, 4222), -- Sword Specialization
-(12814, 200, 4222), -- Sword Specialization
-(13960, 200, 4222), -- Sword Specialization (Rank 1)
-(13961, 200, 4222), -- Sword Specialization
-(13964, 200, 4222), -- Sword Specialization
-(13962, 200, 4222), -- Sword Specialization
-(13963, 200, 4222), -- Sword Specialization
-(18189, 1000, 4222), -- TEST Arcane Concentration
-(3582, 10000, 4222); -- Torch Burst
+(14178, 8000, 0), -- Sticky Tar
+(12281, 200, 0), -- Sword Specialization (Rank 1)
+(12812, 200, 0), -- Sword Specialization
+(12815, 200, 0), -- Sword Specialization
+(12813, 200, 0), -- Sword Specialization
+(12814, 200, 0), -- Sword Specialization
+(13960, 200, 0), -- Sword Specialization (Rank 1)
+(13961, 200, 0), -- Sword Specialization
+(13964, 200, 0), -- Sword Specialization
+(13962, 200, 0), -- Sword Specialization
+(13963, 200, 0), -- Sword Specialization
+(18189, 1000, 0), -- TEST Arcane Concentration
+(3582, 10000, 0); -- Torch Burst
 
 
 -- End of migration.
