a38280b649558dd9153fcec9bc1ac374a03b2efc
NickTyrer <nickyty8@gmail.com>
2020-10-10 15:11:57 +0100

    initial

---
+++ sql/migrations/20201010150923_world.sql
@@ -0,0 +1,20 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20201010150923');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20201010150923');
+-- Add your query below.
+
+-- set correct spellcost for shield slam and mortal strike rank 2
+UPDATE `npc_trainer` SET `spellcost` = 2000 WHERE `spell` IN (21555, 23926);
+
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
