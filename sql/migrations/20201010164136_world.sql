9e51455bfb3ec254f33251f98f39f2033db40fd0
NickTyrer <nickyty8@gmail.com>
2020-10-12 17:16:43 +0100

    Update 20201010164136_world.sql

--- sql/migrations/20201010164136_world.sql
+++ sql/migrations/20201010164136_world.sql
@@ -19,18 +19,18 @@ INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_ty
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
 (1436202, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9374, 0, 0, 0, 0, 0, 0, 0, 0, 'Thornling - Say Text'),
 (1436202, 0, 15, 22788, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Thornling - Cast Spell');
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1436202, 14362, 0, 0, 0, 100, 0, 2000, 2000, 0, 0, 1436202, 0, 0, 'Thornling - Text Emote and Cast Grow on Generic Timer');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1436202, 14362, 0, 1, 0, 100, 0, 2000, 2000, 0, 0, 1436202, 0, 0, 'Thornling - Text Emote and Cast Grow on Generic Timer');
 
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
 (1436203, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9375, 0, 0, 0, 0, 0, 0, 0, 0, 'Thornling - Say Text'),
 (1436203, 0, 15, 22788, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Thornling - Cast Spell');
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1436203, 14362, 0, 0, 0, 100, 0, 4000, 4000, 0, 0, 1436203, 0, 0, 'Thornling - Text Emote and Cast Grow on Generic Timer');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1436203, 14362, 0, 1, 0, 100, 0, 4000, 4000, 0, 0, 1436203, 0, 0, 'Thornling - Text Emote and Cast Grow on Generic Timer');
 
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
 (1436204, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9376, 0, 0, 0, 0, 0, 0, 0, 0, 'Thornling - Say Text'),
 (1436204, 0, 15, 22788, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Thornling - Cast Spell'),
 (1436204, 0, 15, 22781, 1, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Thornling - Cast Spell');
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1436204, 14362, 0, 0, 0, 100, 0, 6000, 6000, 0, 0, 1436204, 0, 0, 'Thornling - Text Emote, Cast Grow and Cast Thornling on Generic Timer');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1436204, 14362, 0, 1, 0, 100, 0, 6000, 6000, 0, 0, 1436204, 0, 0, 'Thornling - Text Emote, Cast Grow and Cast Thornling on Generic Timer');
 
 
 -- End of migration.
