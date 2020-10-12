6009e287ef8ddfaea01b46e54696850c0f093ad1
NickTyrer <nickyty8@gmail.com>
2020-10-12 15:33:44 +0100

    Update 20200729201638_world.sql

--- sql/migrations/20200729201638_world.sql
+++ sql/migrations/20200729201638_world.sql
@@ -1974,7 +1974,7 @@ INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `pos
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4496101, 0, 22, 634, 1, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Mindless Zombie - Set Faction');
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4496101, 0, 26, 0, 0, 0, 0, 28711, 0, 9, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Mindless Zombie - Start Attack');
 
---creature 41905 waypoints
+-- creature 41905 waypoints
 UPDATE `creature` SET `movement_type` = 2 WHERE `guid` = 41905;
 INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
 (41905, 1, 1969.439941, 1592.829956, 82.408302, 100.000000, 0, 0.000000, 0),
@@ -1984,7 +1984,7 @@ INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `pos
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4190501, 0, 22, 634, 1, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Wretched Zombie - Set Faction');
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4190501, 0, 26, 0, 0, 0, 0, 28711, 0, 9, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Wretched Zombie - Start Attack');
 
---creature 44965 waypoints
+-- creature 44965 waypoints
 UPDATE `creature` SET `movement_type` = 2 WHERE `guid` = 44965;
 INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
 (44965, 1, 1939.000000, 1579.939941, 81.942596, 100.000000, 0, 0.000000, 0),
@@ -1993,7 +1993,7 @@ INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `pos
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4496501, 0, 22, 634, 1, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Wretched Zombie - Set Faction');
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4496501, 0, 26, 0, 0, 0, 0, 28711, 0, 9, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Wretched Zombie - Start Attack');
 
---creature 44965 waypoints
+-- creature 44965 waypoints
 UPDATE `creature` SET `movement_type` = 2 WHERE `guid` = 44958;
 INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
 (44958, 1, 1905.900024, 1561.900024, 88.127296, 100.000000, 0, 0.000000, 0),
@@ -2056,7 +2056,7 @@ INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `posit
 (13158, 16, 2509.300049, -897.817017, 56.699600, 100.000000, 0, 0.000000, 0),
 (13158, 17, 2512.050049, -906.869995, 56.323502, 100.000000, 0, 0.000000, 0);
 
---creature 37880 waypoints
+-- creature 37880 waypoints
 UPDATE `creature` SET `movement_type` = 2 WHERE `guid` = 37880;
 INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `wander_distance`, `script_id`) VALUES
 (37880, 1, 1901.810059, 901.916016, 29.363600, 100.000000, 0, 0.000000, 0),
