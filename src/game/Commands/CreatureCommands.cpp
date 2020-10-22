f9e55a50edd52a466c6ddf40f798b714f1abec31
ratkosrb <ratkomladic2@abv.bg>
2020-10-22 14:48:12 +0300

    Stop visual waypoints from falling to the ground.

--- src/game/Commands/CreatureCommands.cpp
+++ src/game/Commands/CreatureCommands.cpp
@@ -1462,6 +1462,7 @@ inline Creature* Helper_CreateWaypointFor(Creature* wpOwner, WaypointPathOrigin 
     wpCreature->SetSummonPoint(pos);
     wpCreature->SetUInt32Value(UNIT_FIELD_LEVEL, wpId+1);
     wpCreature->SetActiveObjectState(true);
+    wpCreature->AddUnitMovementFlag(MOVEFLAG_FIXED_Z);
 
     wpCreature->Summon(TEMPSUMMON_TIMED_DESPAWN, 5 * MINUTE * IN_MILLISECONDS); // Also initializes the AI and MMGen
     return wpCreature;
