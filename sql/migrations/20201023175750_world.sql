9b119d570d3586ba90a1f170c16c8e984f597309
NickTyrer <nickyty8@gmail.com>
2020-10-24 10:03:55 +0100

    Update 20201023175750_world.sql

--- sql/migrations/20201023175750_world.sql
+++ sql/migrations/20201023175750_world.sql
@@ -40,7 +40,7 @@ INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalo
 
 -- add emotes to helena atwood
 DELETE FROM `creature_movement` WHERE `id` = 41844;
-UPDATE `creature` SET `movement_type` = 0 WHERE `guid` = 41844;
+UPDATE `creature` SET `movement_type` = 0, `wander_distance` = 0 WHERE `guid` = 41844;
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (566901, 5669, 0, 1, 0, 100, 1, 9000, 9000, 9000, 9000, 566901, 0, 0, 'Helena Atwood - Emote');
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (566901, 0, 15, 9008, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Helena Atwood - Emote');
 
