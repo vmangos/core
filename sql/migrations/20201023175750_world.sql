2329fe3f049e78ca747efe8a3a17b8b3e208cf23
NickTyrer <nickyty8@gmail.com>
2020-11-02 16:49:59 +0000

    Update 20201023175750_world.sql

--- sql/migrations/20201023175750_world.sql
+++ sql/migrations/20201023175750_world.sql
@@ -35,8 +35,8 @@ INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (569702, 9, 35, 1, 0, 0, 0, 38425, 0, 9, 2, 0, 0, 0, 0, 0, 0, 0, 4.93928, 0, 'Theresa - Set Orientation (Gerard Abernathy)');
 
 -- add emote to james van brunt
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (459601, 4596, 0, 1, 0, 100, 0, 0, 0, 0, 0, 459601, 0, 0, 'James Van Brunt - Emote');
-INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (459601, 0, 1, 233, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'James Van Brunt - Emote');
+INSERT INTO `creature_addon` (`guid`, `emote`) VALUES
+(41843, 233);
 
 -- add emotes to helena atwood
 DELETE FROM `creature_movement` WHERE `id` = 41844;
