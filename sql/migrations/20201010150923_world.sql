c4dabdaa603c1648581a3216852c9d65d8cc506c
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2020-11-02 12:50:54 +0200

    Remove not needed update.

--- sql/migrations/20201010150923_world.sql
+++ sql/migrations/20201010150923_world.sql
@@ -12,9 +12,6 @@ INSERT INTO `migrations` VALUES ('20201010150923');
 -- set correct spellcost for shield slam and mortal strike rank 2
 UPDATE `npc_trainer` SET `spellcost` = 2000 WHERE `spell` IN (21555, 23926);
 
--- set correct run_speed for defias dockworker
-UPDATE `creature_template` SET `speed_run` = 0.857143 WHERE `entry` = 6927;
-
 -- set correct values for Argent Quartermaster Hasana gossip menu option
 UPDATE `gossip_menu_option` SET `action_menu_id` = -1, `action_script_id` = 3421, `condition_id` = 478 WHERE `menu_id` = 3421;
 
