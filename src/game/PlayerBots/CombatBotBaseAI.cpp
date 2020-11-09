f36fc1e3a433b5efe43cb381a11be50684c761c6
ratkosrb <ratkomladic2@abv.bg>
2020-11-09 23:44:56 +0200

    Allow low level mage bots to use Polymorph.

--- src/game/PlayerBots/CombatBotBaseAI.cpp
+++ src/game/PlayerBots/CombatBotBaseAI.cpp
@@ -805,7 +805,7 @@ void CombatBotBaseAI::PopulateSpellData()
                         m_spells.mage.pBlink->Id < pSpellEntry->Id)
                         m_spells.mage.pBlink = pSpellEntry;
                 }
-                else if (pSpellEntry->Id == (12826)) // Sheep
+                else if (pSpellEntry->SpellName[0] == "Polymorph") // Sheep
                 {
                     if (!pPolymorphSheep ||
                         pPolymorphSheep->Id < pSpellEntry->Id)
