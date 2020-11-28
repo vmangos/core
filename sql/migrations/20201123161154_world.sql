314db853d8dde78e244fa7f552cf0e6b8e618de8
NickTyrer <nickyty8@gmail.com>
2020-11-28 18:27:16 +0000

    Update 20201123161154_world.sql

--- sql/migrations/20201123161154_world.sql
+++ sql/migrations/20201123161154_world.sql
@@ -352,6 +352,10 @@ INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (350102, 0, 1, 1, 7, 274, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Horde Guard - Emote');
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (350102, 3501, 350102, 1, 0, 100, 1, 0, 0, 6000, 6000, 350102, 0, 0, 'Horde Guard - Emote OOC');
 
+-- add emote to Misha Tor kren
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (319302, 3193, 0, 1, 0, 100, 1, 0, 0, 180000, 180000, 319302, 0, 0, 'Misha Tor\'kren - Emote OOC');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (319302, 0, 1, 18, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Misha Tor\'kren - Emote');
+
 
 -- End of migration.
 END IF;
