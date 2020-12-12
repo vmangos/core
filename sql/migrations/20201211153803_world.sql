f41ff6c4cd69aff2260e20e4e18c183a5f3ba804
NickTyrer <nickyty8@gmail.com>
2020-12-12 10:41:33 +0000

    Update 20201211153803_world.sql

--- sql/migrations/20201211153803_world.sql
+++ sql/migrations/20201211153803_world.sql
@@ -9,6 +9,7 @@ INSERT INTO `migrations` VALUES ('20201211153803');
 -- Add your query below.
 
 
+-- stonetalon mountains
 -- executioners sword - veenix
 UPDATE `npc_vendor` SET `maxcount` = 2 WHERE `entry` = 4086 AND `item` = 4818;
 
@@ -36,6 +37,37 @@ DELETE FROM `npc_vendor` WHERE `entry` = 4082 AND `item` IN (2947, 3111);
 
 -- fine longbow - borand
 UPDATE `npc_vendor` SET `maxcount` = 2 WHERE `entry` = 9549 AND `item` = 11304;
+
+
+-- thunder bluff
+-- missing items shadi mistrunner
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES
+(8363, 6260),
+(8363, 6529),
+(8363, 2324),
+(8363, 785);
+
+-- imbued vial - shadi mistrunner
+DELETE FROM `npc_vendor` WHERE `entry` = 8363 AND `item` = 18256;
+
+-- rune thread - shadi mistrunner
+DELETE FROM `npc_vendor` WHERE `entry` = 8363 AND `item` = 14341;
+
+-- strange dust - nata dawnstrider
+UPDATE `npc_vendor` SET `maxcount` = 4 WHERE `entry` = 3012 AND `item` = 10940;
+
+-- fine shortbow - kuna thunderhorn
+UPDATE `npc_vendor` SET `maxcount` = 2, `incrtime` = 7200 WHERE `entry` = 3015 AND `item` = 11303;
+
+-- sturdy recurve - kuna thunderhorn
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 7200 WHERE `entry` = 3015 AND `item` = 11306;
+
+-- massive longbow - kuna thunderhorn
+UPDATE `npc_vendor` SET `maxcount` = 1, `incrtime` = 10800 WHERE `entry` = 3015 AND `item` = 11307;
+
+-- aquadynamic fish attractor - sewa mistrunner
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`) VALUES
+(3029, 6533, 2, 7200);
 
 
 -- End of migration.
