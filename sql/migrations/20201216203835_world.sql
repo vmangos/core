6b7ee3e7e3a76c2ccb8d85401547d0b86f7c28b8
NickTyrer <nickyty8@gmail.com>
2020-12-18 16:41:24 +0000

    Update 20201216203835_world.sql

--- sql/migrations/20201216203835_world.sql
+++ sql/migrations/20201216203835_world.sql
@@ -168,6 +168,8 @@ UPDATE `creature_template` SET `health_min` = 3293, `health_max` = 3293, `speed_
 UPDATE `creature_template` SET `npc_flags` = 4, `faction` = 35, `health_min` = 2769, `health_max` = 2769, `speed_walk` = 1, `base_attack_time` = 2000, `ranged_attack_time` = 2000 WHERE `entry` = 11278;
 UPDATE `creature` SET `position_x` = 1144.51, `position_y` = -2510.08, `position_z` = 62.1604, `orientation` = 4.03171 WHERE `guid` = 300332;
 
+REPLACE INTO `creature_addon` (`guid`, `auras`) VALUES
+(300332, '10848 17622');
 
 -- End of migration.
 END IF;
