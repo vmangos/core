7e912ad6afe66c4df222b5dad1d207459909c212
NickTyrer <nickyty8@gmail.com>
2020-12-20 09:41:33 +0000

    initial

---
+++ sql/migrations/20201220093949_world.sql
@@ -0,0 +1,23 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20201220093949');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20201220093949');
+-- Add your query below.
+
+
+UPDATE `quest_template` SET `Required_Races` = 8 WHERE `entry` = 6344;
+UPDATE `quest_template` SET `Required_Races` = 68 WHERE `entry` = 6387;
+UPDATE `quest_template` SET `Required_Races` = 16 WHERE `entry` = 6321;
+UPDATE `quest_template` SET `Required_Races` = 1 WHERE `entry` = 6181;
+
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
