3727c41249bcccecc093c30457c8cae9f21972b0
NickTyrer <nickyty8@gmail.com>
2020-12-23 08:59:28 +0000

    Update 20201222170619_world.sql

--- sql/migrations/20201222170619_world.sql
+++ sql/migrations/20201222170619_world.sql
@@ -15,6 +15,9 @@ UPDATE `creature_template` SET `npc_flags` = 18 WHERE `entry` IN (8128, 3175, 35
 -- correct flags for herbalism trainers
 UPDATE `creature_template` SET `npc_flags` = 18 WHERE `entry` IN (4204, 12025, 3965, 3185, 2390, 2114);
 
+-- correct flags for skinning trainers
+UPDATE `creature_template` SET `npc_flags` = 18 WHERE `entry` IN (12030, 8144, 7089, 7088, 7087, 6387, 6295, 6292, 6290, 6288, 6287);
+
 
 -- End of migration.
 END IF;
