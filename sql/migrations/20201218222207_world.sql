c349c87ddadf4c30915729f0aff160af23a9740e
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2020-12-31 01:53:34 +0200

    Just set to 1.

--- sql/migrations/20201218222207_world.sql
+++ sql/migrations/20201218222207_world.sql
@@ -11,13 +11,13 @@ INSERT INTO `migrations` VALUES ('20201218222207');
 -- Set QUEST_METHOD_DISABLED flag for following quests
 
 -- 171 Four-legged Menaces
-UPDATE `quest_template` SET `Method`=`Method` | 1 WHERE `entry`=171 AND `patch`=0;
+UPDATE `quest_template` SET `Method`=1 WHERE `entry`=171 AND `patch`=0;
 
 -- 172 Ambushed In The Forest
-UPDATE `quest_template` SET `Method`=`Method` | 1 WHERE `entry`=172 AND `patch`=0;
+UPDATE `quest_template` SET `Method`=1 WHERE `entry`=172 AND `patch`=0;
 
 -- 236 Awaiting Word
-UPDATE `quest_template` SET `Method`=`Method` | 1 WHERE `entry`=236 AND `patch`=0;
+UPDATE `quest_template` SET `Method`=1 WHERE `entry`=236 AND `patch`=0;
 
 -- End of migration.
 END IF;
