5d87c42ad2ae8b1fd33ee3258235f1118d354cef
Trevor West <61628974+thenewblock@users.noreply.github.com>
2021-01-17 16:15:28 -0500

    Fixed quest requirement for Scarab Shells pre 1.3

---
+++ sql/migrations/20210117211419_world.sql
@@ -0,0 +1,18 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20210117211419');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20210117211419');
+-- Add your query below.
+-- Change requirement for Scarab Shells pre 1.3
+REPLACE INTO `quest_template` VALUES (2865, 0, 2, 978, 40, 0, 45, 81, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 2864, 0, 0, 0, 0, 0, 0, 'Scarab Shells', 'The scarabs of Tanaris have very hard shells!  Hard enough to use as a building material for lots of things.  So many things!$B$BIn fact, those shells are so useful... the scarabs were hunted all to near extinction!$B$BI know where there are more scarabs, and if you promise to bring me their shells then I\'ll tell you where they are.  Promise?$B$BOk, the scarabs have a colony in Zul\'Farrak.  I guess the trolls don\'t hunt them for their shells.$B$BBut you can!  Go to Zul\'Farrak and get me uncracked shells!', 'Bring 9 Uncracked Scarab Shells to Tran\'rek in Gadgetzan.', 'Oh, great!  You got them!$B$BThanks, $N.  You\'re a real lifesaver!', 'Do you have the shells?  My cousin in Booty Bay is waiting for a load of them, and he\'s getting impatient!', '', '', '', '', '', 9238, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 369, 0, 0, 0, 0, 100, 0, 0, 0, 0, 3900, 6500, 2340, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO `quest_template` VALUES (2865, 1, 2, 978, 40, 0, 45, 81, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 2864, 0, 0, 0, 0, 0, 0, 'Scarab Shells', 'The scarabs of Tanaris have very hard shells!  Hard enough to use as a building material for lots of things.  So many things!$B$BIn fact, those shells are so useful... the scarabs were hunted all to near extinction!$B$BI know where there are more scarabs, and if you promise to bring me their shells then I\'ll tell you where they are.  Promise?$B$BOk, the scarabs have a colony in Zul\'Farrak.  I guess the trolls don\'t hunt them for their shells.$B$BBut you can!  Go to Zul\'Farrak and get me uncracked shells!', 'Bring 5 Uncracked Scarab Shells to Tran\'rek in Gadgetzan.', 'Oh, great!  You got them!$B$BThanks, $N.  You\'re a real lifesaver!', 'Do you have the shells?  My cousin in Booty Bay is waiting for a load of them, and he\'s getting impatient!', '', '', '', '', '', 9238, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 369, 0, 0, 0, 0, 100, 0, 0, 0, 0, 3900, 6500, 2340, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
