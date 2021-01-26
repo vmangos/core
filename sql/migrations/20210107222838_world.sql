b29ac4ef6e9ef06a68517b04531271a29a6c7414
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2021-01-26 22:13:29 +0200

    Update comments.

--- sql/migrations/20210107222838_world.sql
+++ sql/migrations/20210107222838_world.sql
@@ -10,8 +10,8 @@ INSERT INTO `migrations` VALUES ('20210107222838');
 
 -- Add start script to quest The Rethban Gauntlet
 DELETE FROM `quest_start_scripts` WHERE `id`=1699;
-INSERT INTO `quest_start_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1699, 0, 15, 8554, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Player is drinking Barleybrew');
-INSERT INTO `quest_start_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1699, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2261, 0, 0, 0, 0, 0, 0, 0, 0, 'Yorus talk');
+INSERT INTO `quest_start_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1699, 0, 15, 8554, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'The Rethban Gauntlet: Player - Cast Spell Drinking Barleybrew Scalder');
+INSERT INTO `quest_start_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1699, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2261, 0, 0, 0, 0, 0, 0, 0, 0, 'The Rethban Gauntlet: Yorus - Say Text');
 
 UPDATE `quest_template` SET `StartScript`='1699' WHERE  `entry`=1699;
 
