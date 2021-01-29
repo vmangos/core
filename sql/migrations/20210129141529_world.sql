624cd78015ad3446afc465e392c8a38239bd1500
NickTyrer <nickyty8@gmail.com>
2021-01-29 14:16:29 +0000

    initial

---
+++ sql/migrations/20210129141529_world.sql
@@ -0,0 +1,40 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20210129141529');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20210129141529');
+-- Add your query below.
+
+
+-- elwynn
+DELETE FROM `gossip_menu_option` WHERE `menu_id` = 1291 AND `id` = 5;
+
+-- dun morogh
+DELETE FROM `gossip_menu_option` WHERE `menu_id` = 1297 AND `id` = 4;
+
+-- teldrassil
+DELETE FROM `gossip_menu_option` WHERE `menu_id` = 1293 AND `id` = 4;
+
+-- mulgore
+DELETE FROM `gossip_menu_option` WHERE `menu_id` = 1294 AND `id` = 4;
+
+-- tirisfal
+DELETE FROM `gossip_menu_option` WHERE `menu_id` = 1296 AND `id` = 5;
+INSERT INTO `gossip_menu_option`(`menu_id`, `id`, `option_text`, `option_broadcast_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`) VALUES
+(1296, 5, 'What can I do at an inn?', 4308, 1, 1, 1221);
+
+-- durotar
+DELETE FROM `gossip_menu_option` WHERE `menu_id` = 1290 AND `id` = 5;
+INSERT INTO `gossip_menu_option`(`menu_id`, `id`, `option_text`, `option_broadcast_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`) VALUES
+(1290, 5, 'What can I do at an inn?', 4308, 1, 1, 1221);
+
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
