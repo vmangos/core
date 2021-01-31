794fcee3a33e9fb113f7dc3f999d539b05b082d5
NickTyrer <nickyty8@gmail.com>
2021-01-31 15:12:37 +0000

    Update 20210129141529_world.sql

--- sql/migrations/20210129141529_world.sql
+++ sql/migrations/20210129141529_world.sql
@@ -31,6 +31,9 @@ DELETE FROM `gossip_menu_option` WHERE `menu_id` = 1290 AND `id` = 5;
 INSERT INTO `gossip_menu_option`(`menu_id`, `id`, `option_text`, `option_broadcast_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`) VALUES
 (1290, 5, 'What can I do at an inn?', 4308, 1, 1, 1221);
 
+-- fix erelas ambersky broken gossip menu option
+UPDATE `gossip_menu_option` SET `action_menu_id` = 1481 WHERE `menu_id` = 1482 AND `id` = 0;
+
 
 -- End of migration.
 END IF;
