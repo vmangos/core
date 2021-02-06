22c354ce2acdce9f86bfdff2e5219755bb655637
NickTyrer <nickyty8@gmail.com>
2021-02-06 15:15:16 +0000

    Update 20210129141529_world.sql

--- sql/migrations/20210129141529_world.sql
+++ sql/migrations/20210129141529_world.sql
@@ -34,6 +34,9 @@ INSERT INTO `gossip_menu_option`(`menu_id`, `id`, `option_text`, `option_broadca
 -- fix erelas ambersky broken gossip menu option
 UPDATE `gossip_menu_option` SET `action_menu_id` = 1481 WHERE `menu_id` = 1482 AND `id` = 0;
 
+-- fix quest typo for trial of the lake
+UPDATE `quest_template` SET `OfferRewardText` = 'Well now. It looks like yet another $r has successfully completed the Trial of the Lake! Well done, my young friend - well done.$B$BAs I've said, my name is Tajarri. It is my duty and my privilege to serve as a warden for Moonglade, and specifically this shrine. Keeper Remulos guides the Cenarion Circle in preservation of nature and balance, and his shrine serves as a lasting tribute to such. By using the bauble here, it serves as a nod to the importance of this place within the Circle.' WHERE `entry` IN (28, 29);
+
 
 -- End of migration.
 END IF;
