bf3ae8621c88a3142023a70669eabca7b5fe8d63
ratkosrb <ratkomladic2@abv.bg>
2021-02-09 05:32:19 +0200

    Fix dark portal migration.

--- sql/migrations/20201002171825_world.sql
+++ sql/migrations/20201002171825_world.sql
@@ -11,25 +11,25 @@ INSERT INTO `migrations` VALUES ('20201002171825');
 
 -- The Dark Portal spawns
 -- Spawn Felguard Elite
-INSERT INTO `creature` VALUES (203275, 8717, 0, 0, 0, 0, 0, 0, -11835.7, -3253.5, -25.9082, 5.2709, 900, 900, 0, 100, 0, 0, 0, 0, 0, 10);
-INSERT INTO `creature` VALUES (203278, 8717, 0, 0, 0, 0, 0, 0, -11817.6, -3237.61, -29.7055, 5.8294, 900, 900, 0, 100, 0, 0, 0, 0, 0, 10);
-INSERT INTO `creature` VALUES (203291, 8717, 0, 0, 0, 0, 0, 0, -11843.4, -3229.34, -26.4251, 0, 900, 900, 0, 100, 0, 0, 0, 0, 0, 10);
-INSERT INTO `creature` VALUES (203279, 8717, 0, 0, 0, 0, 0, 0, -11803.8, -3217.06, -30.651, 6.03884, 900, 900, 0, 100, 0, 0, 0, 0, 0, 10);
-INSERT INTO `creature` VALUES (203282, 8717, 0, 0, 0, 0, 0, 0, -11802.3, -3192.65, -29.7192, 0.0698132, 900, 900, 0, 100, 0, 0, 0, 0, 0, 10);
-INSERT INTO `creature` VALUES (203298, 8717, 0, 0, 0, 0, 0, 0, -11805.5, -3170.9, -30.4301, 0.436332, 900, 900, 0, 100, 0, 0, 0, 0, 0, 10);
-INSERT INTO `creature` VALUES (203303, 8717, 0, 0, 0, 0, 0, 0, -11815.9, -3148.64, -27.1289, 0.541052, 900, 900, 0, 100, 0, 0, 0, 0, 0, 10);
-INSERT INTO `creature` VALUES (203307, 8717, 0, 0, 0, 0, 0, 0, -11832.4, -3133.45, -26.8395, 5.23599, 900, 900, 0, 100, 0, 0, 0, 0, 0, 10);
-INSERT INTO `creature` VALUES (203306, 8717, 0, 0, 0, 0, 0, 0, -11853.6, -3158.95, -27.7374, 0.0523599, 900, 900, 0, 100, 0, 0, 0, 0, 0, 10);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203275, 8717, -11835.7, -3253.5, -25.9082, 5.2709, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203278, 8717, -11817.6, -3237.61, -29.7055, 5.8294, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203291, 8717, -11843.4, -3229.34, -26.4251, 0, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203279, 8717, -11803.8, -3217.06, -30.651, 6.03884, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203282, 8717, -11802.3, -3192.65, -29.7192, 0.0698132, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203298, 8717, -11805.5, -3170.9, -30.4301, 0.436332, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203303, 8717, -11815.9, -3148.64, -27.1289, 0.541052, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203307, 8717, -11832.4, -3133.45, -26.8395, 5.23599, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203306, 8717, -11853.6, -3158.95, -27.7374, 0.0523599, 0, 0);
 
 -- Spawn Doomguard Commander
-INSERT INTO `creature` VALUES (203289, 12396, 0, 0, 0, 0, 0, 0, -11850.3, -3250.76, -27.4681, 0.523599, 900, 900, 0, 100, 100, 0, 0, 0, 0, 10);
-INSERT INTO `creature` VALUES (203290, 12396, 0, 0, 0, 0, 0, 0, -11858.5, -3236.31, -24.6962, 0.279253, 900, 900, 0, 100, 100, 0, 0, 0, 0, 10);
-INSERT INTO `creature` VALUES (203308, 12396, 0, 0, 0, 0, 0, 0, -11873.2, -3165.44, -27.055, 5.91667, 900, 900, 0, 100, 100, 0, 0, 0, 0, 10);
-INSERT INTO `creature` VALUES (203309, 12396, 0, 0, 0, 0, 0, 0, -11863.4, -3146.96, -25.6828, 5.55015, 900, 900, 0, 100, 100, 0, 0, 0, 0, 10);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203289, 12396, -11850.3, -3250.76, -27.4681, 0.523599, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203290, 12396, -11858.5, -3236.31, -24.6962, 0.279253, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203308, 12396, -11873.2, -3165.44, -27.055, 5.91667, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203309, 12396, -11863.4, -3146.96, -25.6828, 5.55015, 0, 0);
 
 -- Spawn Manahound
-INSERT INTO `creature` VALUES (203281, 8718, 0, 0, 0, 0, 0, 0, -11824.3, -3215.66, -30.9589, 5.23638, 900, 900, 40, 100, 0, 1, 0, 0, 0, 10);
-INSERT INTO `creature` VALUES (203304, 8718, 0, 0, 0, 0, 0, 0, -11827.2, -3154.9, -29.7552, 4.30938, 900, 900, 40, 100, 0, 1, 0, 0, 0, 10);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203281, 8718, -11824.3, -3215.66, -30.9589, 5.23638, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `position_x`, `position_y`, `position_z`, `orientation`, `wander_distance`, `movement_type`) VALUES (203304, 8718, -11827.2, -3154.9, -29.7552, 4.30938, 0, 0);
 
 
 -- End of migration.
