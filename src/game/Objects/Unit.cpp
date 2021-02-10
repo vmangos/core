1d60bf63fff55a93e6fd4b4b94e48ee42da44a26
schell244 <schell244@web.de>
2021-02-10 14:43:25 +0100

    revent isvictim change

--- src/game/Objects/Unit.cpp
+++ src/game/Objects/Unit.cpp
@@ -8049,7 +8049,7 @@ void Unit::ProcDamageAndSpellFor(bool isVictim, Unit* pTarget, uint32 procFlag, 
         if (IsPlayer())
         {
             // On melee based hit/miss/resist need update skill (for victim and attacker)
-            if (!isVictim && procExtra & (PROC_EX_NORMAL_HIT | PROC_EX_CRITICAL_HIT | PROC_EX_MISS | PROC_EX_DODGE | PROC_EX_PARRY | PROC_EX_BLOCK | PROC_EX_RESIST))
+            if (procExtra & (PROC_EX_NORMAL_HIT | PROC_EX_CRITICAL_HIT | PROC_EX_MISS | PROC_EX_DODGE | PROC_EX_PARRY | PROC_EX_BLOCK | PROC_EX_RESIST))
                 ((Player*)this)->UpdateCombatSkills(pTarget, attType, isVictim);
 
             // Update defence if player is victim and parry/dodge/block
