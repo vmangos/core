e6605cc684bf9fcae1a726dcc852dcac392adb80
NickTyrer <nickyty8@gmail.com>
2021-02-10 18:07:20 +0000

    Update 20210207112151_world.sql

--- sql/migrations/20210207112151_world.sql
+++ sql/migrations/20210207112151_world.sql
@@ -147,7 +147,7 @@ INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1590809, 15908, 220558, 1, 0, 100, 1, 10000, 15000, 10000, 15000, 1590809, 0, 0, 'Orgrimmar Reveler - Cast Small Red Rocket OOC');
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1590809, 0, 15, 26286, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Orgrimmar Reveler - Cast Small Red Rocket');
 
-- add trick - critter template
+-- add trick - critter template
 INSERT INTO `creature_template` VALUES
 (15219, 0, 6296, 5556, 10992, 11009, 0, 0, 0, 0, 1, 1, 1, 1, 4, 0, 'Trick - Critter', '', 0, 1, 1, 42, 42, 0, 0, 15, 188, 0, 1, 0, 20, 0, 0, 0, 0, 2, 2, 0, 9, 1, 2000, 2000, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, , 0, 0, '', 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0, '');
 
