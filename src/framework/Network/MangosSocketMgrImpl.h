0cee2309912686c5f5b6326986e387d30c9085cd
ratkosrb <ratkomladic2@abv.bg>
2021-02-14 11:44:37 +0200

    Try to fix gcc build.

--- src/framework/Network/MangosSocketMgrImpl.h
+++ src/framework/Network/MangosSocketMgrImpl.h
@@ -30,8 +30,8 @@ public:
     MangosSocketAcceptor(void) { }
     virtual ~MangosSocketAcceptor(void)
     {
-        if (reactor())
-            reactor()->cancel_timer(this, 1);
+        if (this->reactor())
+            this->reactor()->cancel_timer(this, 1);
     }
 
 protected:
@@ -39,8 +39,8 @@ protected:
     virtual int handle_timeout(const ACE_Time_Value &current_time, const void *act = 0)
     {
         sLog.outBasic("Resuming acceptor");
-        reactor()->cancel_timer(this, 1);
-        return reactor()->register_handler(this, ACE_Event_Handler::ACCEPT_MASK);
+        this->reactor()->cancel_timer(this, 1);
+        return this->reactor()->register_handler(this, ACE_Event_Handler::ACCEPT_MASK);
     }
 
     virtual int handle_accept_error(void)
@@ -49,8 +49,8 @@ protected:
         if (errno == ENFILE || errno == EMFILE)
         {
             sLog.outError("Out of file descriptors, suspending incoming connections for 10 seconds");
-            reactor()->remove_handler(this, ACE_Event_Handler::ACCEPT_MASK | ACE_Event_Handler::DONT_CALL);
-            reactor()->schedule_timer(this, NULL, ACE_Time_Value(10));
+            this->reactor()->remove_handler(this, ACE_Event_Handler::ACCEPT_MASK | ACE_Event_Handler::DONT_CALL);
+            this->reactor()->schedule_timer(this, NULL, ACE_Time_Value(10));
         }
 #endif
         return 0;
