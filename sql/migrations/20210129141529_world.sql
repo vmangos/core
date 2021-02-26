805e65c00f7883c0ff69282c02adf675a03f31ee
NickTyrer <nickyty8@gmail.com>
2021-02-26 15:00:56 +0000

    Update 20210129141529_world.sql

--- sql/migrations/20210129141529_world.sql
+++ sql/migrations/20210129141529_world.sql
@@ -43,10 +43,8 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `positi
 (1554, 2038, 1, 10107.08, 1206.241, 1311.55, 4.468043, 300, 300, 100, 100, 10),
 (1551, 2038, 1, 10127.8, 1202.00, 1323.30, 3.01113, 300, 300, 100, 100, 10),
 (1552, 2038, 1, 10127.3, 1125.23, 1338.11, 3.58055, 300, 300, 100, 100, 10);
-
 INSERT INTO `pool_template` (`entry`, `max_limit`, `description`, `patch_max`) VALUES
 (108, 1, 'Lord Melenas (2038)', 10);
-
 INSERT INTO `pool_creature` (`guid`, `pool_entry`, `description`) VALUES
 (1554, 108, 'Lord Melenas #1'),
 (1551, 108, 'Lord Melenas #2'),
@@ -56,10 +54,8 @@ INSERT INTO `pool_creature` (`guid`, `pool_entry`, `description`) VALUES
 INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `wander_distance`, `health_percent`, `mana_percent`, `movement_type`, `patch_max`) VALUES
 (12597, 7319, 1, 10950.3, 1397.79, 1311.63, 2.32941, 300, 300, 5, 100, 100, 1, 10),
 (73001, 7319, 1, 10980, 1731.3, 1303.1, 5.14113, 300, 300, 5, 100, 100, 1, 10);
-
 INSERT INTO `pool_template` (`entry`, `max_limit`, `description`, `patch_max`) VALUES
 (109, 1, 'Lady Sathra (7319)', 10);
-
 INSERT INTO `pool_creature` (`guid`, `pool_entry`, `description`) VALUES
 (12596, 109, 'Lady Sathra #1'),
 (12597, 109, 'Lady Sathra #2'),
@@ -78,7 +74,6 @@ DELETE FROM `quest_end_scripts` WHERE `id`=1191;
 INSERT INTO `quest_end_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1191, 0, 4, 147, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Zamek - Modify Flags');
 INSERT INTO `quest_end_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1191, 0, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Zamek - Run');
 INSERT INTO `quest_end_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1191, 1, 60, 2, 0, 0, 0, 0, 0, 0, 0, 0, 4709, 0, 0, 0, 0, 0, 0, 0, 'Zamek - Start Waypoints');
-
 -- Add waypoints and scripts
 INSERT INTO `creature_movement_template` (`entry`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `waittime`, `script_id`) VALUES
 -- Zamek 4709
@@ -112,14 +107,12 @@ INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (472002, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1542, 0, 0, 0, 0, 0, 0, 0, 0, 'Rizzle Brassbolts - Talk');
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (472002, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1543, 0, 0, 0, 0, 0, 0, 0, 0, 'Rizzle Brassbolts - Talk');
 INSERT INTO `creature_movement_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (472003, 0, 35, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3.38594, 0, 'Rizzle Brassbolts - Orientation');
-
 -- in case if object despawns before player accept next one
 UPDATE `quest_template` SET `RequiredCondition` = 20227 WHERE `entry` = 1191;
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`) VALUES 
 (20225, 9, 1190, 0),
 (20226, 19, 1194, 0),
 (20227, -2, 20226, 20225);
-
 -- Objects
 -- Rizzle's Guarded Plans 179888
 UPDATE `gameobject_template` SET `flags` = 0 WHERE entry = 179888;
@@ -142,9 +135,7 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `positi
 (160361, 8389, 1, 1748.87, -5863.46, -90.2665, 2.83088, 300, 300, 0, 0),
 (160362, 8394, 1, 1746.45, -5861.29, -91.4165, 3.20841, 300, 300, 0, 0),
 (160363, 8478, 1, 1916.44, -5733.37, 10.1936, 4.10397, 300, 300, 0, 0);
-
 UPDATE `creature_template` SET `inhabit_type` = 1 WHERE `entry` IN (8387, 8388, 8389, 8394);
-
 INSERT INTO `creature_addon` (`guid`, `stand_state`, `sheath_state`, `auras`) VALUES
 (160363, 8, 1, '');
 
@@ -159,6 +150,9 @@ DELETE FROM `creature_loot_template` WHERE `item` = 4483 AND `entry` = 2760;
 DELETE FROM `creature` WHERE `guid` IN (90271,90014,90375,90216,89906,90434,90341);
 DELETE FROM `creature_addon` WHERE `guid` IN (90271,90014,90375,90216,89906,90434,90341);
 UPDATE `creature` SET `position_x` = -10829.554688, `position_y` = 733.747437, `position_z` = 34.381603, `orientation` = 3.798669 WHERE `guid` = 90362;
+
+-- Troll Tribal Necklace should always drop as quest 2881 is repeatable
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = 20 WHERE `item` = 9259;
 
 
 -- End of migration.
