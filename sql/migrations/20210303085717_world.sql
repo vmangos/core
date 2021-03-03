92524141a3abc743cd5b32ce6ed8367a368cb622
NickTyrer <nickyty8@gmail.com>
2021-03-03 09:00:33 +0000

    initial

---
+++ sql/migrations/20210303085717_world.sql
@@ -0,0 +1,22 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20210303085717');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20210303085717');
+-- Add your query below.
+
+
+-- remove duplicate serpentbloom gameobject
+DELETE FROM `gameobject` WHERE `id` = 19535;
+DELETE FROM `gameobject_template` WHERE `entry` = 19535;
+
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
