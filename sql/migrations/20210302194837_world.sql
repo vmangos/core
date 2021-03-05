1d57f42cae8989a21e1be66f32bf0e8000502136
NickTyrer <nickyty8@gmail.com>
2021-03-05 16:57:33 +0000

    Update 20210302194837_world.sql

--- sql/migrations/20210302194837_world.sql
+++ sql/migrations/20210302194837_world.sql
@@ -10,6 +10,7 @@ INSERT INTO `migrations` VALUES ('20210302194837');
 
 
 -- add Justine Demalier script
+UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry` = 12481;
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1248101, 12481, 1248104, 10, 0, 25, 1, 1, 3, 0, 0, 1248101, 0, 0, 'Justine Demalier - Talk - Unit In LOS OOC');
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1248101, 0, 39, 1248101, 0, 0, 0, 0, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 'Justine Demalier - Start Script');
 INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1248101, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7900, 0, 0, 0, 0, 0, 0, 0, 0, 'Justine Demalier - Talk');
