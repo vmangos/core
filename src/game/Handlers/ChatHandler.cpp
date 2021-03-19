c729450c209c27488ceaf555b2ca87a3a92abb95
GrenderG <grenderg@gmail.com>
2021-03-19 03:43:09 +0100

    Add missing parenthesis.

--- src/game/Handlers/ChatHandler.cpp
+++ src/game/Handlers/ChatHandler.cpp
@@ -177,7 +177,7 @@ void WorldSession::HandleMessagechatOpcode(WorldPacket& recv_data)
         {
             // Send message in universal language if crossfaction chat is enabled and player is using default faction
             // languages.
-            if (sWorld.getConfig(CONFIG_BOOL_ALLOW_TWO_SIDE_INTERACTION_CHAT) && lang == LANG_COMMON || lang == LANG_ORCISH)
+            if (sWorld.getConfig(CONFIG_BOOL_ALLOW_TWO_SIDE_INTERACTION_CHAT) && (lang == LANG_COMMON || lang == LANG_ORCISH))
                 lang = LANG_UNIVERSAL;
             else
             {
