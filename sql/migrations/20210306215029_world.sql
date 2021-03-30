7ae9da2d9e07adf48cabeb341921ba9442c033b6
ratkosrb <ratkomladic2@abv.bg>
2021-03-30 21:05:11 +0300

    Increase detection range in starting zones slightly.

--- sql/migrations/20210306215029_world.sql
+++ sql/migrations/20210306215029_world.sql
@@ -10,7 +10,7 @@ INSERT INTO `migrations` VALUES ('20210306215029');
 
 
 -- Aggro range for mobs in starting zones is lower than normal.
-UPDATE `creature_template` SET `detection_range`=8 WHERE `entry` IN (6, 38, 69, 80, 103, 257, 299, 704, 705, 706, 707, 708, 724, 808, 946, 1501, 1502, 1504, 1505, 1506, 1507, 1508, 1509, 1512, 1513, 1667, 1688, 1890, 1916, 1917, 1918, 1919, 1984, 1985, 1986, 1988, 1989, 1994, 2031, 2032, 2952, 2953, 2954, 2955, 2961, 2966, 3098, 3101, 3102, 3124, 3183, 3229, 3281, 3300, 8554);
+UPDATE `creature_template` SET `detection_range`=9.5 WHERE `entry` IN (6, 38, 69, 80, 103, 257, 299, 704, 705, 706, 707, 708, 724, 808, 946, 1501, 1502, 1504, 1505, 1506, 1507, 1508, 1509, 1512, 1513, 1667, 1688, 1890, 1916, 1917, 1918, 1919, 1984, 1985, 1986, 1988, 1989, 1994, 2031, 2032, 2952, 2953, 2954, 2955, 2961, 2966, 3098, 3101, 3102, 3124, 3183, 3229, 3281, 3300, 8554);
 
 -- Aggro range for most mobs in classic is around 3-4 yards lower than our current default.
 UPDATE `creature_template` SET `detection_range`=16 WHERE `detection_range`=20 && `rank`=0;
