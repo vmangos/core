61e95aa4bd0e55f9cc0a051eb19735c871efac7c
GrenderG <grenderg@gmail.com>
2021-04-02 21:54:02 +0200

    Formatting.

--- sql/migrations/20210402210056_world.sql
+++ sql/migrations/20210402210056_world.sql
@@ -10,7 +10,7 @@ INSERT INTO `migrations` VALUES ('20210402210056');
 
 -- Thulbek was not spawned in Grom'gol until Patch 1.7.
 -- https://web.archive.org/web/20041115164537/http://wow.allakhazam.com/db/mob.html?wmob=5814
-UPDATE `creature` set `patch_min` = 5 WHERE `id` = 5814;
+UPDATE `creature` SET `patch_min` = 5 WHERE `id` = 5814;
 UPDATE `creature_template` SET `patch` = 5 WHERE `entry` = 5814 AND `patch` = 0;
 
 -- End of migration.
