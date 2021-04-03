d5a38c8d8e57434055594a8999d7bed2f1521b2e
tonymmm1 <tony@mail.magnatox.com>
2021-04-02 20:00:17 -0500

    Fix mmap directory creation under Windows (#1147)
    * changed ci to branch patch-9
    
    * added headers for creating directory
    
    * add windows exclusion
    
    * revert ci to development branch
    
    * fix tabs

--- contrib/mmap/src/generator.cpp
+++ contrib/mmap/src/generator.cpp
@@ -18,6 +18,14 @@
 
 #include "MMapCommon.h"
 #include "MapBuilder.h"
+#ifdef _WIN32
+#include <Windows.h>
+#include <sys/stat.h>
+#include <direct.h>
+#define mkdir _mkdir
+#else
+#include <sys/stat.h>
+#endif
 
 using namespace MMAP;
 
@@ -42,7 +50,11 @@ bool checkDirectories(bool debugOutput)
     if (getDirContents(dirFiles, "mmaps") == LISTFILE_DIRECTORY_NOT_FOUND)
     {
         /* create mmaps directory */
-        if (mkdir("mmaps",0777) != 0)
+        if (mkdir("mmaps"
+#ifndef _WIN32
+            , 0777
+#endif
+            ) != 0)
         {
             /* return error if operation fails */
             printf("'mmaps' directory does not exist, please create it\n");
