9e56fe983543e93c984052b8561886cfee568aab
woblight <woblight@gmail.com>
2021-04-11 16:54:33 +0200

    Dither AURA_SCHOOL_ASSORB at cast rather than on assorb

--- src/game/Objects/Unit.cpp
+++ src/game/Objects/Unit.cpp
@@ -1871,8 +1871,9 @@ void Unit::CalculateDamageAbsorbAndResist(WorldObject* pCaster, SpellSchoolMask 
         if (!(mod->m_miscvalue & schoolMask))
             continue;
 
+        assert(mod->m_amount - floor(mod->m_amount) == 0);
         // Max Amount can be absorbed by this aura
-        int32  currentAbsorb = dither(mod->m_amount);
+        int32  currentAbsorb = mod->m_amount;
 
         // Found empty aura (impossible but..)
         if (currentAbsorb <= 0)
