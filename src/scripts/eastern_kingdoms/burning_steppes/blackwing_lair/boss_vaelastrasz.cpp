26b7dc422e785995b43089069fe22ad3fef8b58f
GrenderG <grenderg@gmail.com>
2021-04-21 18:00:45 +0200

    Vaelastraz shouldn't despawn after 1 hour in Patch 1.8 or greater. (#1129)
    * Vaelastraz shouldn't despawn after 1 hour in Patch 1.8 or greater.
    
    * Fixed content patch to check.

--- src/scripts/eastern_kingdoms/burning_steppes/blackwing_lair/boss_vaelastrasz.cpp
+++ src/scripts/eastern_kingdoms/burning_steppes/blackwing_lair/boss_vaelastrasz.cpp
@@ -206,8 +206,12 @@ struct boss_vaelAI : public ScriptedAI
         if (!m_bEngaged)
         {
             m_bEngaged = true;
-            m_creature->SetRespawnDelay(43200); // 12h 43200
-            m_creature->ForcedDespawn(3600000); // 1h 3600000
+            // From 1.8: There is no longer a one-hour time restriction on the Vaelastraz the Corrupt encounter in Blackwing Lair.
+            if (sWorld.GetWowPatch() < WOW_PATCH_108)
+            {
+                m_creature->SetRespawnDelay(43200); // 12h 43200
+                m_creature->ForcedDespawn(3600000); // 1h 3600000
+            }
         }
     }
 
