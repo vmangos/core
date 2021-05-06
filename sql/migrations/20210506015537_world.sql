332668ec1cee07f044e002d1d86e9fa9f773f484
GrenderG <grenderg@gmail.com>
2021-05-06 03:27:29 +0200

    Setting Method as 1 (disabled) should be enough.

--- sql/migrations/20210506015537_world.sql
+++ sql/migrations/20210506015537_world.sql
@@ -9,9 +9,8 @@ INSERT INTO `migrations` VALUES ('20210506015537');
 -- Add your query below.
 
 -- https://web.archive.org/web/20060524160851/http://wow.allakhazam.com/db/quest.html?wquest=787
-UPDATE `quest_template` SET `OfferRewardText` = 'Another one of Eitrigg''s recruits, hm?$B$BA sorry state of affairs we find ourselves in if this is the best the Horde can produce. No matter. By the time we think you''re ready to leave the Valley, you''ll be a proud warrior of the Horde.', `RewXP` = 15, `Method` = 1 WHERE `entry` = 787;
 -- This quest was only available from Eitrigg during beta days when he was spawned in Valley of Trials instead of Orgrimmar.
-DELETE FROM `creature_questrelation` WHERE `id` = 3144 AND `quest` = 787;
+UPDATE `quest_template` SET `OfferRewardText` = 'Another one of Eitrigg''s recruits, hm?$B$BA sorry state of affairs we find ourselves in if this is the best the Horde can produce. No matter. By the time we think you''re ready to leave the Valley, you''ll be a proud warrior of the Horde.', `RewXP` = 15, `Method` = 1 WHERE `entry` = 787;
 
 -- End of migration.
 END IF;
