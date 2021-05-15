c335e84eae2a6a5db604e498486e8ff1442b4478
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2021-05-15 17:45:49 +0300

    Assign chat type.

--- sql/migrations/20210501105521_world.sql
+++ sql/migrations/20210501105521_world.sql
@@ -8,9 +8,13 @@ IF v=0 THEN
 INSERT INTO `migrations` VALUES ('20210501105521');
 -- Add your query below.
 
+
+-- Remove custom version of STV fishing tournament texts.
 DELETE FROM `script_texts` WHERE `entry`= -1900100; 
 DELETE FROM `script_texts` WHERE `entry`= -1900101; 
 DELETE FROM `script_texts` WHERE `entry`= -1900102;
+UPDATE `broadcast_text` SET `chat_type`=6 WHERE `entry` IN (10608, 10609, 10610);
+
 
 -- End of migration.
 END IF;
