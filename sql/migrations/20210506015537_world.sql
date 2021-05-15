2ec5ae915c59d4e1ee16d214ce39e13b6c7099df
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2021-05-15 18:00:54 +0300

    Remove XP change.

--- sql/migrations/20210506015537_world.sql
+++ sql/migrations/20210506015537_world.sql
@@ -10,7 +10,7 @@ INSERT INTO `migrations` VALUES ('20210506015537');
 
 -- https://web.archive.org/web/20060524160851/http://wow.allakhazam.com/db/quest.html?wquest=787
 -- This quest was only available from Eitrigg during beta days when he was spawned in Valley of Trials instead of Orgrimmar.
-UPDATE `quest_template` SET `OfferRewardText` = 'Another one of Eitrigg''s recruits, hm?$B$BA sorry state of affairs we find ourselves in if this is the best the Horde can produce. No matter. By the time we think you''re ready to leave the Valley, you''ll be a proud warrior of the Horde.', `RewXP` = 15, `Method` = 1 WHERE `entry` = 787;
+UPDATE `quest_template` SET `OfferRewardText` = 'Another one of Eitrigg''s recruits, hm?$B$BA sorry state of affairs we find ourselves in if this is the best the Horde can produce. No matter. By the time we think you''re ready to leave the Valley, you''ll be a proud warrior of the Horde.', `Method` = 1 WHERE `entry` = 787;
 
 -- End of migration.
 END IF;
