66c56c5eb89da75d35131bf52312b9fb93f3cd0e
GrenderG <grenderg@gmail.com>
2021-05-16 19:07:24 +0200

    Removed Theramore TBC spawns.

---
+++ sql/migrations/20210516190702_world.sql
@@ -0,0 +1,20 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20210516190702');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20210516190702');
+-- Add your query below.
+
+-- Theramore Deserters that were added in TBC.
+DELETE FROM `creature` WHERE `guid` IN (30741, 30769, 30770, 30742, 30772, 30773, 30771, 30774, 30776);
+DELETE FROM `creature_addon` WHERE `guid` = 30741;
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
