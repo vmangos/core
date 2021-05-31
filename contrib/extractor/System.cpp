25d5d27ab34c62644a024c64c13936a6a0763097
ratkosrb <ratkomladic2@abv.bg>
2021-05-31 09:27:52 +0300

    Increate map version.

--- contrib/extractor/System.cpp
+++ contrib/extractor/System.cpp
@@ -257,7 +257,7 @@ void ReadLiquidTypeTableDBC()
 
 // Map file format data
 static char const* MAP_MAGIC         = "MAPS";
-static char const* MAP_VERSION_MAGIC = "z1.4";
+static char const* MAP_VERSION_MAGIC = "z1.5";
 static char const* MAP_AREA_MAGIC    = "AREA";
 static char const* MAP_HEIGHT_MAGIC  = "MHGT";
 static char const* MAP_LIQUID_MAGIC  = "MLIQ";
--- contrib/vmap_extractor/vmapextract/vmapexport.cpp
+++ contrib/vmap_extractor/vmapextract/vmapexport.cpp
@@ -76,7 +76,7 @@ bool preciseVectorData = false;
 
 //static const char * szWorkDirMaps = ".\\Maps";
 const char* szWorkDirWmo = "./Buildings";
-const char* szRawVMAPMagic = "VMAP005";
+const char* szRawVMAPMagic = "VMAP006";
 
 // Local testing functions
 
