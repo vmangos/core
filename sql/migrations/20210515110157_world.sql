c74b50ca636b79d575175f27672a5f673bf404fa
NickTyrer <nickyty8@gmail.com>
2021-06-11 16:26:44 +0100

    Update 20210515110157_world.sql

--- sql/migrations/20210515110157_world.sql
+++ sql/migrations/20210515110157_world.sql
@@ -765,6 +765,23 @@ INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalo
 -- Quest 6962 Is Not Repeatable
 UPDATE `quest_template` SET `SpecialFlags` = 0 WHERE `entry` = 6962;
 
+-- Falling underground - Fixes (credit cmangos)
+UPDATE creature SET position_x = -6690.331543, position_y = -2988.697021, position_z = 248.367706, wander_distance = 7, movement_type = 1 WHERE guid = 6910;
+UPDATE creature SET position_x = -6629.925293, position_y = -3554.510498, position_z = 254.083786, wander_distance = 7, movement_type = 1 WHERE guid = 6926;
+UPDATE creature SET position_x = 51.213818, position_y = 1539.047974, position_z = 107.961784, wander_distance = 7, movement_type = 1 WHERE guid = 27980;
+UPDATE creature SET position_x = 393.519775, position_y = -623.946899, position_z = 162.409653, wander_distance = 5, movement_type = 1 WHERE guid = 17345;
+UPDATE creature SET position_x = -3649.268, position_y = -727.9708, position_z = 10.7234, orientation = 5.907869, wander_distance = 0, movement_type = 0 WHERE guid = 9530;
+
+-- Elder Shadowmaw Panther 1713 (credit cmangos)
+-- swimming anim fixed
+DELETE FROM creature_addon WHERE guid IN (SELECT guid FROM creature WHERE id = 1713);
+UPDATE creature_template_addon SET bytes1 = 0 WHERE entry = 1713;
+
+-- Ridge Stalker 2731 (credit cmangos)
+-- swimming anim fixed
+DELETE FROM creature_addon WHERE guid IN (SELECT guid FROM creature WHERE id = 2731);
+UPDATE creature_template_addon SET bytes1 = 0, auras = NULL WHERE entry = 2731;
+
 
 -- End of migration.
 END IF;
