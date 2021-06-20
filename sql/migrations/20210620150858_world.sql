ae6d3104f9d524cca99f404d2a4f251a87bf5052
NickTyrer <nickyty8@gmail.com>
2021-06-20 15:20:44 +0100

    Update 20210620150858_world.sql

--- sql/migrations/20210620150858_world.sql
+++ sql/migrations/20210620150858_world.sql
@@ -9,7 +9,7 @@ INSERT INTO `migrations` VALUES ('20210620150858');
 -- Add your query below.
 
 
-UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry` = 15892;
+UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry` IN (15892, 15891);
 UPDATE `gameobject_template` SET `faction` = 114 WHERE `entry` = 180872;
 UPDATE `gameobject` SET `spawntimesecsmin` = 5, `spawntimesecsmax` = 5 WHERE id IN (180763, 180764, 180871, 180873 , 180872);
 
@@ -19,6 +19,13 @@ INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`
 INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1589202, 0, 81, 0, 5, 0, 0, 180764, 50000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 501, 'Lunar Festival Emissary - Despawn Gobject');
 INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1589203, 0, 81, 0, 5, 0, 0, 180871, 50000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 'Lunar Festival Emissary - Despawn Gobject');
 INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1589204, 0, 81, 0, 5, 0, 0, 180872, 50000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 503, 'Lunar Festival Emissary - Despawn Gobject');
+
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1589101, 15891, 0, 1, 0, 100, 1, 10000, 20000, 10000, 20000, 1589101, 0, 0, 'Lunar Festival Herald - Start Script - OOC');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1589101, 0, 39, 1589101, 1589102, 1589103, 1589104, 0, 0, 0, 0, 25, 25, 25, 25, 0, 0, 0, 0, 0, 'Lunar Festival Herald - Start Script - OOC');
+INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1589101, 0, 81, 0, 5, 0, 0, 180763, 50000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 500, 'Lunar Festival Herald - Despawn Gobject');
+INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1589102, 0, 81, 0, 5, 0, 0, 180764, 50000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 501, 'Lunar Festival Herald - Despawn Gobject');
+INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1589103, 0, 81, 0, 5, 0, 0, 180871, 50000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 'Lunar Festival Herald - Despawn Gobject');
+INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1589104, 0, 81, 0, 5, 0, 0, 180872, 50000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 503, 'Lunar Festival Herald - Despawn Gobject');
 
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (500, 21, 180763, 50000, 0, 0, 0);
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (501, 21, 180764, 50000, 0, 0, 0);
