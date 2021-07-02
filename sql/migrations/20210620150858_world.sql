c4ebd5b8bc65d1d3f7191e9b157c04fda213835d
NickTyrer <nickyty8@gmail.com>
2021-07-02 08:27:15 +0100

    Update 20210620150858_world.sql

--- sql/migrations/20210620150858_world.sql
+++ sql/migrations/20210620150858_world.sql
@@ -10,7 +10,7 @@ INSERT INTO `migrations` VALUES ('20210620150858');
 
 
 UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry` IN (15892, 15891);
-UPDATE `gameobject_template` SET `faction` = 114 WHERE `entry` = 180872;
+UPDATE `gameobject_template` SET `faction` = 114 WHERE `entry` IN (180871, 180872, 180763, 180873, 180870, 180764);
 UPDATE `gameobject` SET `spawntimesecsmin` = 5, `spawntimesecsmax` = 5 WHERE id IN (180763, 180764, 180871, 180873 , 180872);
 
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1589201, 15892, 0, 1, 0, 100, 1, 10000, 20000, 10000, 20000, 1589201, 0, 0, 'Lunar Festival Emissary - Start Script - OOC');
