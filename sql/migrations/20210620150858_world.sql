e70384c63c2d7dfaf07293e928895fb9139907fa
NickTyrer <nickyty8@gmail.com>
2021-07-08 19:07:55 +0100

    Update 20210620150858_world.sql

--- sql/migrations/20210620150858_world.sql
+++ sql/migrations/20210620150858_world.sql
@@ -32,7 +32,14 @@ INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (509, -1, 506, 501, 0, 0, 0);
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (517, -1, 506, 502, 0, 0, 0);
 
-
+-- Renato Gallina AI
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (143203, 1432, 532, 1, 0, 100, 1, 0, 10000, 5000, 10000, 143203, 0, 0, 'Renato Gallina - Despawn Gameobject (180763) - OOC');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (143204, 1432, 533, 1, 0, 100, 1, 0, 10000, 5000, 10000, 143204, 0, 0, 'Renato Gallina - Despawn Gameobject (180764) - OOC');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (143203, 0, 81, 0, 16, 0, 0, 180763, 5000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Renato Gallina - Despawn Gameobject (180763)');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (143204, 0, 81, 0, 16, 0, 0, 180764, 0, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Renato Gallina - Despawn Gameobject (180764)');
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (519, 52, 79688, 0, 0, 0, 0);
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (532, -1, 519, 501, 0, 0, 0);
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (533, -1, 519, 502, 0, 0, 0);
 
 
 
