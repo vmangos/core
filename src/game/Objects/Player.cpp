16fca0aeb1a0177800b8ad1178cd060f1f0b9be4
ratkosrb <ratkomladic2@abv.bg>
2021-07-13 09:31:10 +0300

    Fix weapon switch global cooldown.

--- src/game/Objects/Player.cpp
+++ src/game/Objects/Player.cpp
@@ -10556,7 +10556,7 @@ Item* Player::EquipItem(uint16 pos, Item* pItem, bool update)
 #else
                     m_weaponChangeTimer = (GetClass() == CLASS_ROGUE) ? 1000 : spellProto->StartRecoveryTime;
 #endif
-                    AddGCD(*spellProto, true);
+                    AddGCD(*spellProto, 0, true);
                 }
             }
 #endif
