3172c4c4ca24818d994f2fb26e3d27758029985c
NickTyrer <nickyty8@gmail.com>
2021-07-18 07:40:04 +0100

    Update 20210624194440_world.sql

--- sql/migrations/20210624194440_world.sql
+++ sql/migrations/20210624194440_world.sql
@@ -333,7 +333,7 @@ DELETE FROM `creature_loot_template` WHERE `entry` = 14983;
 
 -- Add Correct Completion Text to Quests
 UPDATE `quest_template` SET `OfferRewardText` = 'Hm... $N. You are still new to your path, but I sense the possibility for greatness in you.$B$BYou were born with gifts, $N. See that they do not go to waste.' WHERE `entry` = 1507;
-UPDATE `mangos`.`quest_template` SET `OfferRewardText`='Incredible! Improbable! Simply amazing! You\'ve got talent, $N. Either that or you\'re the luckiest $gman:woman; alive!$b$bHere\'s your cut of the action. I\'m sure you would make better use of this stuff than I could.$b$b' WHERE  `entry`=2381 AND `patch`=0;
+UPDATE `quest_template` SET `OfferRewardText` = 'Incredible! Improbable! Simply amazing! You\'ve got talent, $N. Either that or you\'re the luckiest $gman:woman; alive!$b$bHere\'s your cut of the action. I\'m sure you would make better use of this stuff than I could.$b$b' WHERE `entry`=2381;
 
 
 
