d9399818fea35c50de050d8b26ce9d01d0124b34
NickTyrer <nickyty8@gmail.com>
2021-08-03 20:50:19 +0100

    Update 20210624194440_world.sql

--- sql/migrations/20210624194440_world.sql
+++ sql/migrations/20210624194440_world.sql
@@ -400,6 +400,10 @@ INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalo
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1242804, 12428, 0, 8, 0, 100, 1, 1243, -1, 0, 0, 1242804, 0, 0, 'Deathguard Kel - Remove Buff on Buffed');
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1242804, 0, 14, 1243, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Deathguard Kel - Remove Aura');
 
+-- Rename Darkmoon Faire Events
+UPDATE `game_event` SET `description`='Darkmoon Faire Building (Elwynn)' WHERE  `entry`=23;
+UPDATE `game_event` SET `description`='Darkmoon Faire Building (Mulgore)' WHERE  `entry`=24;
+
 
 -- End of migration.
 END IF;
