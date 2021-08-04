434ec53730415a322a306338e1fe9402765da6e2
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2021-08-04 19:51:59 +0300

    Assign c++ script.

--- sql/migrations/20210620150858_world.sql
+++ sql/migrations/20210620150858_world.sql
@@ -8,46 +8,6 @@ IF v=0 THEN
 INSERT INTO `migrations` VALUES ('20210620150858');
 -- Add your query below.
 
-/*
-UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry` IN (15892);
--- Lunar Festival Emissary AI
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1589201, 15892, 503, 1, 0, 100, 1, 0, 10000, 5000, 10000, 1589201, 0, 0, 'Lunar Festival Emissary - Despawn Gameobject (180763) - OOC');
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1589202, 15892, 504, 1, 0, 100, 1, 0, 10000, 5000, 10000, 1589202, 0, 0, 'Lunar Festival Emissary - Despawn Gameobject (180764) - OOC');
-INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1589201, 0, 81, 0, 0, 0, 0, 180763, 5000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Lunar Festival Emissary - Despawn Gameobject (180763)');
-INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1589202, 0, 81, 0, 0, 0, 0, 180764, 5000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Lunar Festival Emissary - Despawn Gameobject (180764)');
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (500, 52, 91623, 91634, 91622, 94928, 0);
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (501, 21, 180763, 5000, 0, 0, 2);
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (502, 21, 180764, 5000, 0, 0, 2);
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (503, -1, 500, 501, 0, 0, 0);
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (504, -1, 500, 502, 0, 0, 0);
-
--- Darnassus Sentinel AI
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (426203, 4262, 509, 1, 0, 100, 1, 0, 10000, 5000, 10000, 426203, 0, 0, 'Darnassus Sentinel - Despawn Gameobject (180763) - OOC');
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (426204, 4262, 517, 1, 0, 100, 1, 0, 10000, 5000, 10000, 426204, 0, 0, 'Darnassus Sentinel - Despawn Gameobject (180764) - OOC');
-INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (426203, 0, 81, 0, 0, 0, 0, 180763, 5000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Darnassus Sentinel - Despawn Gameobject (180763)');
-INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (426204, 0, 81, 0, 0, 0, 0, 180764, 5000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Darnassus Sentinel - Despawn Gameobject (180764)');
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (506, 52, 46852, 0, 0, 0, 0);
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (509, -1, 506, 501, 0, 0, 0);
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (517, -1, 506, 502, 0, 0, 0);
-
--- Renato Gallina AI
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (143203, 1432, 532, 1, 0, 100, 1, 0, 10000, 5000, 10000, 143204, 0, 0, 'Renato Gallina - Despawn Gameobject (180763) - OOC');
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (143204, 1432, 533, 1, 0, 100, 1, 0, 10000, 5000, 10000, 143205, 0, 0, 'Renato Gallina - Despawn Gameobject (180764) - OOC');
-INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (143204, 0, 81, 0, 0, 0, 0, 180763, 5000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Renato Gallina - Despawn Gameobject (180763)');
-INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (143205, 0, 81, 0, 0, 0, 0, 180764, 5000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Renato Gallina - Despawn Gameobject (180764)');
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (519, 52, 79688, 0, 0, 0, 0);
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (532, -1, 519, 501, 0, 0, 0);
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (533, -1, 519, 502, 0, 0, 0);
-
--- Lomac Gearstrip AI
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (408101, 4081, 535, 1, 0, 100, 1, 0, 10000, 5000, 10000, 408101, 0, 0, 'Lomac Gearstrip - Despawn Gameobject (180763) - OOC');
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (408102, 4081, 536, 1, 0, 100, 1, 0, 10000, 5000, 10000, 408102, 0, 0, 'Lomac Gearstrip - Despawn Gameobject (180764) - OOC');
-INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (408101, 0, 81, 0, 0, 0, 0, 180763, 5000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Lomac Gearstrip - Despawn Gameobject (180763)');
-INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (408102, 0, 81, 0, 0, 0, 0, 180764, 5000, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Lomac Gearstrip - Despawn Gameobject (180764)');
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (534, 52, 40, 0, 0, 0, 0);
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (535, -1, 534, 501, 0, 0, 0);
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (536, -1, 534, 502, 0, 0, 0);
-*/
 
 -- Respawn Firecrackers (Entry 180763 and 180764)
 SET @LUNAR_FESTIVAL_OGUID = 22202;
@@ -780,6 +740,9 @@ INSERT INTO `pool_gameobject` (`guid`, `pool_entry`, `description`, `patch_max`)
 (@LUNAR_FESTIVAL_OGUID+284, @LUNAR_FESTIVAL_POOL_TEMPLATE+141, 'Firecrackers', 10),
 (@LUNAR_FESTIVAL_OGUID+285, @LUNAR_FESTIVAL_POOL_TEMPLATE+142, 'Firecrackers', 10),
 (@LUNAR_FESTIVAL_OGUID+286, @LUNAR_FESTIVAL_POOL_TEMPLATE+143, 'Firecrackers', 10);
+
+-- Assign script to despawn firecrackers.
+UPDATE `gameobject_template` SET `script_name`='go_lunar_festival_firecracker' WHERE `entry` IN (180763, 180764);
 
 
 -- End of migration.
