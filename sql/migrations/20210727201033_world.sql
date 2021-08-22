d003e1c6c1337b853782775728d352e445adbb44
Oroxzy <crusadermeile@gmail.com>
2021-08-22 16:38:48 +0200

    Update 20210727201033_world.sql

--- sql/migrations/20210727201033_world.sql
+++ sql/migrations/20210727201033_world.sql
@@ -22,15 +22,18 @@ DELETE FROM `game_event_gameobject` WHERE `event` = @LUNAR_FESTIVAL_EVENT AND NO
 DELETE FROM `game_event_creature` WHERE `event` = @LUNAR_FESTIVAL_EVENT;
 
 -- Fix flags.
-UPDATE `creature_template` SET `unit_flags`='512', `flags_extra`=131072 WHERE `entry` IN (15891,15892);
-UPDATE `creature_template` SET `unit_flags`='0' WHERE `entry` IN (15895,15907,15908,15906,15905,15719,15694);
+UPDATE `creature_template` SET `unit_flags`=512, `flags_extra`=131072 WHERE `entry` IN (15891,15892);
+UPDATE `creature_template` SET `unit_flags`=0 WHERE `entry` IN (15895,15907,15908,15906,15905,15719,15694);
+
+-- Omen.
+UPDATE `creature_template` SET `dmg_min`=1210, `dmg_max`=2183, `dmg_multiplier`=1, `unit_flags`=2048 WHERE `entry`=15467;
 
 -- Revelers don't cast "Small Red Rocket".
 DELETE FROM `creature_ai_events` WHERE `id` IN (1590709,1590609,1569409,1571909,1590509,1590809);
 DELETE FROM `creature_ai_scripts` WHERE `id` IN (1590709,1590609,1569409,1571909,1590509,1590809);
 
 -- Revelers cast 26347 but we use Small Red Rocket.
-UPDATE `creature_ai_scripts` SET `datalong`='26286', `datalong2`='0' WHERE `id` IN (1569403,1571903,1590503,1590603,1590703,1590803,1572303);
+UPDATE `creature_ai_scripts` SET `datalong`=26286, `datalong2`=0 WHERE `id` IN (1569403,1571903,1590503,1590603,1590703,1590803,1572303);
 
 -- Dance added in creature_addon.
 DELETE FROM `creature_ai_events` WHERE `id` IN (1569407,1571907,1590507,1590607,1590707,1590807,1572307);
@@ -65,12 +68,12 @@ INSERT INTO `spell_target_position` (`id`, `target_map`, `target_position_x`, `t
 (26456, 0, 1642.41, 239.9, 62.59, 3.01, 5086, 5875);
 
 -- Lunar Festival Sentinel spelllist.
-UPDATE `creature_template` SET `unit_flags`='512', `dmg_min`='328', `dmg_max`='821', `spell_list_id`='159610' WHERE `entry`=15961;
+UPDATE `creature_template` SET `unit_flags`=512, `dmg_min`=328, `dmg_max`=821, `spell_list_id`=159610 WHERE `entry`=15961;
 REPLACE INTO `creature_spells` (`entry`, `name`, `spellId_1`, `probability_1`, `castTarget_1`, `targetParam1_1`, `targetParam2_1`, `castFlags_1`, `delayInitialMin_1`, `delayInitialMax_1`, `delayRepeatMin_1`, `delayRepeatMax_1`, `scriptId_1`, `spellId_2`, `probability_2`, `castTarget_2`, `targetParam1_2`, `targetParam2_2`, `castFlags_2`, `delayInitialMin_2`, `delayInitialMax_2`, `delayRepeatMin_2`, `delayRepeatMax_2`, `scriptId_2`, `spellId_3`, `probability_3`, `castTarget_3`, `targetParam1_3`, `targetParam2_3`, `castFlags_3`, `delayInitialMin_3`, `delayInitialMax_3`, `delayRepeatMin_3`, `delayRepeatMax_3`, `scriptId_3`, `spellId_4`, `probability_4`, `castTarget_4`, `targetParam1_4`, `targetParam2_4`, `castFlags_4`, `delayInitialMin_4`, `delayInitialMax_4`, `delayRepeatMin_4`, `delayRepeatMax_4`, `scriptId_4`, `spellId_5`, `probability_5`, `castTarget_5`, `targetParam1_5`, `targetParam2_5`, `castFlags_5`, `delayInitialMin_5`, `delayInitialMax_5`, `delayRepeatMin_5`, `delayRepeatMax_5`, `scriptId_5`, `spellId_6`, `probability_6`, `castTarget_6`, `targetParam1_6`, `targetParam2_6`, `castFlags_6`, `delayInitialMin_6`, `delayInitialMax_6`, `delayRepeatMin_6`, `delayRepeatMax_6`, `scriptId_6`, `spellId_7`, `probability_7`, `castTarget_7`, `targetParam1_7`, `targetParam2_7`, `castFlags_7`, `delayInitialMin_7`, `delayInitialMax_7`, `delayRepeatMin_7`, `delayRepeatMax_7`, `scriptId_7`, `spellId_8`, `probability_8`, `castTarget_8`, `targetParam1_8`, `targetParam2_8`, `castFlags_8`, `delayInitialMin_8`, `delayInitialMax_8`, `delayRepeatMin_8`, `delayRepeatMax_8`, `scriptId_8`) VALUES
 (159610, 'Lunar Festival - Lunar Festival Sentinel', 15618, 100, 1, 0, 0, 322, 5, 10, 15, 30, 0, 18328, 100, 1, 0, 0, 34, 5, 15, 15, 30, 0, 19643, 100, 1, 0, 0, 66, 0, 2, 10, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 -- Correct mainhand for Lunar Festival Sentinel.
-UPDATE `creature_equip_template` SET `equipentry1`='14882' WHERE  `entry`=15961 AND `patch`=6;
+UPDATE `creature_equip_template` SET `equipentry1`=14882 WHERE  `entry`=15961 AND `patch`=6;
 
 UPDATE `creature_template` SET `ai_name`='EventAI' WHERE `entry` IN (15891,15892);
 
@@ -2069,7 +2072,7 @@ INSERT INTO `game_event_creature_data` (`guid`, `entry_id`, `spell_start`, `even
 (@LUNAR_FESTIVAL_CGUID+125, 0, 26383, 7);
 
 -- Cast Rocket Red OOC min is 30 seconds in sniffs.
-UPDATE `creature_ai_events` SET `event_param1`='30000', `event_param3`='30000' WHERE `id` IN (1569403,1571903,1572303,1590503,1590603,1590703,1590803);
+UPDATE `creature_ai_events` SET `event_param1`=30000, `event_param3`=30000 WHERE `id` IN (1569403,1571903,1572303,1590503,1590603,1590703,1590803);
 
 
 -- End of migration.
