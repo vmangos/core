725a4f928665a2972407e7c9752c1e9f4195085c
NickTyrer <nickyty8@gmail.com>
2021-08-27 22:03:48 +0100

    Update 20210810191400_world.sql

--- sql/migrations/20210810191400_world.sql
+++ sql/migrations/20210810191400_world.sql
@@ -657,6 +657,42 @@ UPDATE `npc_vendor` SET `itemflags` = 0 WHERE `itemflags` != 0;
 -- Remove Duplicate Spawns
 DELETE FROM `creature` WHERE `guid` IN (91936, 92482, 92471);
 
+-- Add Battle Under Blackwood Lake Script
+-- Remove Duplicate Spawns
+DELETE FROM `creature` WHERE `guid` IN (
+92828,
+92827,
+92824,
+92831,
+92823,
+92826,
+92248,
+92244,
+92249,
+92247,
+92246,
+92245,
+92829
+);
+
+
+-- Spectral Betrayer AI
+UPDATE `creature_template` SET `ai_name`='EventAI' WHERE `entry` IN (11289, 11288);
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1128801, 11288, 0, 0, 0, 100, 1, 5000, 15000, 15000, 25000, 1128801, 0, 0, 'Spectral Betrayer - Talk - In Combat');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1128801, 0, 39, 1128801, 1128802, 0, 0, 0, 0, 0, 0, 50, 50, 0, 0, 0, 0, 0, 0, 0, 'Spectral Betrayer - Start Script');
+INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1128801, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6834, 6832, 6827, 6829, 0, 0, 0, 0, 0, 'Spectral Betrayer - Talk');
+INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1128802, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6831, 6833, 6830, 6828, 0, 0, 0, 0, 0, 'Spectral Betrayer - Talk');
+
+-- Spectral Defender AI
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1128901, 11289, 0, 1, 0, 100, 1, 5000, 5000, 5000, 5000, 1128901, 0, 0, 'Spectral Defender - Summon Spectral Betrayer - OOC');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1128901, 0, 10, 11288, 0, 0, 0, 0, 0, 0, 0, 0, 1128901, 0, 5, 0, 0, 0, 0, 0, 'Spectral Defender - Summon Creature');
+INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1128901, 0, 26, 0, 0, 0, 0, 11289, 20, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Spectral Betrayer - Start Attack');
+
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1128902, 11289, 0, 0, 0, 100, 1, 5000, 15000, 15000, 25000, 1128902, 0, 0, 'Spectral Defender - Talk - In Combat');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1128902, 0, 39, 1128902, 1128903, 0, 0, 0, 0, 0, 0, 50, 50, 0, 0, 0, 0, 0, 0, 0, 'Spectral Defender - Start Script');
+INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1128902, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6840, 6836, 6841, 6837, 0, 0, 0, 0, 0, 'Scarlet Defender - Talk');
+INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1128903, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6838, 6835, 6842, 6839, 0, 0, 0, 0, 0, 'Spectral Defender - Talk');
+
 
 -- End of migration.
 END IF;
