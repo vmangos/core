e77fc14548b09f7165a4ef82cf66d492149eb0a0
놀린 <nolin.nolin.nolin.nolin@gmail.com>
2021-09-11 09:56:52 +0200

    Remove obsolete query

--- src/game/Commands/CharacterCommands.cpp
+++ src/game/Commands/CharacterCommands.cpp
@@ -87,9 +87,6 @@ bool ChatHandler::HandleModifyXpRateCommand(char* args)
     }
 
     pPlayer->SetPersonalXpRate(xp);
-    
-    // Save it to the DB
-    CharacterDatabase.PExecute("UPDATE `characters` SET `xp_rate` = '%f' WHERE `guid` = '%u'", pPlayer->GetPersonalXpRate(), pPlayer->GetGUIDLow());
 
     PSendSysMessage(LANG_XP_RATE_SET, (pPlayer != m_session->GetPlayer() ? (std::string(pPlayer->GetName()) + std::string("'s")).c_str() : "your"), xp);
     return true;
