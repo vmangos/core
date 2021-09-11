30f0b04e92530f549bee1ed36aefa9fee33d6fcd
NickTyrer <nickyty8@gmail.com>
2021-09-11 09:04:45 +0100

    Update 20210810191400_world.sql

--- sql/migrations/20210810191400_world.sql
+++ sql/migrations/20210810191400_world.sql
@@ -854,7 +854,7 @@ INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (558, -1, 74, 556, 0, 0, 0);
 UPDATE `npc_vendor` SET `condition_id` = 557 WHERE `entry` = 10856 AND `item` IN (18169, 18170, 18171, 18172, 18173);
 UPDATE `npc_vendor` SET `condition_id` = 558 WHERE `entry` = 10856 AND `item` = 18182;
-UPDATE `gossip_menu_option` SET `condition_id` = 0 WHERE `menu_id` = 3421 AND `id` = 1;
+UPDATE `gossip_menu_option` SET `condition_id` = 0, `action_script_id` = 0 WHERE `menu_id` = 3421 AND `id` = 1;
 
 
 -- End of migration.
