45ddbdec28dccc19a346d68d60e6c9442827e04e
NickTyrer <nickyty8@gmail.com>
2021-09-26 23:23:57 +0100

    Update 20210926224902_world.sql

--- sql/migrations/20210926224902_world.sql
+++ sql/migrations/20210926224902_world.sql
@@ -29,20 +29,8 @@ UPDATE `quest_template` SET `OfferRewardText`='You\'ve proven yourself today, $n
 -- INCORRECT
 UPDATE `gameobject_template` SET `data12` = 181290 WHERE `entry` = 181347;
 UPDATE `gameobject_template` SET `patch` = 9 WHERE `entry` = 181346
-UPDATE `quest_template` SET `SpecialFlags` = 0, `PrevQuestId` = 0 WHERE `entry` = 9386;
-UPDATE `quest_template` SET `Details` = 'We are not the only ones who celebrate this holiday, $n. All creatures, all cultures, are touched by the flame. Even in the darkest places, the fires are being lit.$b$bIf you wish to see them for yourself, travel into the depths. Seek out the bonfires burning within Stratholme, Scholomance, among the ogres of Dire Maul, and the halls of Blackrock Spire itself; I will await your return.' WHERE `entry` = 9386;
-UPDATE `quest_template` SET `Objectives` = 'Find and touch the bonfires located within Blackrock Spire, Dire Maul, Scholomance, and Stratholme, then return to the Festival Flamekeeper.' WHERE `entry` = 9386;
-UPDATE `quest_template` SET `RequestItemsText` = '' WHERE `entry` = 9319;
 UPDATE `quest_template` SET `OfferRewardText` = 'Your essence sings with the energy of the flames you found, $n. The fire you encountered is potent, and with the right knowledge, its power can be harnessed...$b$b<The Flamekeeper mutters an incantation in a strange, arcane tongue. then pulls out a glowing bottle.>$b$bAh! Here we are. May this light your path, no matter where you tread.' WHERE `entry` = 9319;
-UPDATE `quest_template` SET `OfferRewardText` = 'Your essence sings with the energy of the flames you found, $n. The fire you encountered is potent, and with the right knowledge, its power can be harnessed...$b$b<The Flamekeeper mutters an incantation in a strange, arcane tongue. then pulls out a glowing bottle.>$b$bAh! Here we are. May this light your path, no matter where you tread.' WHERE  `entry`=9386;
-UPDATE `quest_template` SET `RewItemId1` = 23083 WHERE `entry` = 9386;
-UPDATE `quest_template` SET `RewItemId2` = 23247 WHERE `entry` = 9386;
-UPDATE `quest_template` SET `RewItemCount1` = 1 WHERE `entry` = 9386;
-UPDATE `quest_template` SET `RewItemCount2` = 10 WHERE `entry` = 9386;
-UPDATE `quest_template` SET `RewXP` = 4950 WHERE `entry` = 9386;
-UPDATE `quest_template` SET `RewMoneyMaxLevel` = 2970 WHERE `entry` = 9386;
-UPDATE `quest_template` SET `DetailsEmote1` = 1, `DetailsEmote2`='1' WHERE `entry`=9386;
-UPDATE `quest_template` SET `CompleteEmote` = 1 WHERE `entry` = 9319;
+UPDATE `quest_template` SET `RequiredRaces` = 255 WHERE `entry` IN (9386, 9319);
 
 
 -- End of migration.
