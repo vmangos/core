4f8f593e211b6695c0a4b4cdad461af735f20fe1
NickTyrer <nickyty8@gmail.com>
2021-09-26 22:51:27 +0100

    Update 20210926224902_world.sql

--- sql/migrations/20210926224902_world.sql
+++ sql/migrations/20210926224902_world.sql
@@ -9,6 +9,24 @@ INSERT INTO `migrations` VALUES ('20210926224902');
 -- Add your query below.
 
 
+-- Update Some Midsummer Fire Festival Quest Chains
+-- Alliance
+UPDATE `creature_questrelation` SET `id` = 16817 WHERE `id` = 16818 AND `quest` = 9339;
+UPDATE `creature_involvedrelation` SET `id` = 16817 WHERE `id` = 16818 AND `quest` = 9339;
+UPDATE `quest_template` SET `RequiredRaces` = 77 WHERE `entry` = 9339 AND `patch` = 9;
+UPDATE `quest_template` SET `Details`='' WHERE `entry` = 9339 AND `patch` = 9;
+UPDATE `quest_template` SET `Objectives`='' WHERE `entry` = 9339 AND `patch` = 9;
+UPDATE `quest_template` SET `OfferRewardText`='You\'ve proven yourself today, $n.$b$bWhile I have little else to give you, I do have this crown; a fitting gift for a conqueror. May it light your way.' WHERE `entry` = 9339 AND `patch`=9;
+
+-- Horde
+UPDATE `creature_questrelation` SET `id` = 16818 WHERE `id` = 16817 AND `quest` = 9365;
+UPDATE `creature_involvedrelation` SET `id` = 16818 WHERE `id` = 16817 AND `quest` = 9365;
+UPDATE `quest_template` SET `RequiredRaces`= 178 WHERE `entry` = 9365 AND `patch` = 9;
+UPDATE `quest_template` SET `Details`='' WHERE `entry` = 9365 AND `patch`=9;
+UPDATE `quest_template` SET `Objectives`='' WHERE `entry` = 9365 AND `patch`=9;
+UPDATE `quest_template` SET `OfferRewardText`='You\'ve proven yourself today, $n.$b$bWhile I have little else to give you, I do have this crown; a fitting gift for a conqueror. May it light your way.' WHERE `entry` = 9365 AND `patch`=9;
+
+
 UPDATE `gameobject_template` SET `data12` = 181290 WHERE `entry` = 181347;
 UPDATE `gameobject_template` SET `patch` = 9 WHERE `entry` = 181346
 UPDATE `quest_template` SET `SpecialFlags` = 0, `PrevQuestId` = 0 WHERE `entry` = 9386;
