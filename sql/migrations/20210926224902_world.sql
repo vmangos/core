e71b50bd4dbf5c9e0a4a76c7e3b45fee0c7b216f
NickTyrer <nickyty8@gmail.com>
2021-09-27 21:13:59 +0100

    Update 20210926224902_world.sql

--- sql/migrations/20210926224902_world.sql
+++ sql/migrations/20210926224902_world.sql
@@ -39,7 +39,7 @@ UPDATE `quest_template` SET `OfferRewardText` = 'Your essence sings with the ene
 UPDATE `quest_template` SET `RequiredRaces` = 255 WHERE `entry` IN (9386, 9319);
 UPDATE `quest_template` SET `RequestItemsText` = 'Have you found your way through the dark?' WHERE `entry` = 9386;
 
-- Correct Scale For Cinder Elemental
+-- Correct Scale For Cinder Elemental
 UPDATE `creature_template` SET `display_scale1` = 0 WHERE `entry` = 17003;
 
 
