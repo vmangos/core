cea461c0fa7c2c3d1f956b26606b9ee796d2e7f8
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2021-09-27 13:04:08 +0300

    Fix order of objectives for quest A Light in Dark Places

--- sql/migrations/20210926224902_world.sql
+++ sql/migrations/20210926224902_world.sql
@@ -26,6 +26,12 @@ UPDATE `quest_template` SET `Details`='' WHERE `entry` = 9365 AND `patch`=9;
 UPDATE `quest_template` SET `Objectives`='' WHERE `entry` = 9365 AND `patch`=9;
 UPDATE `quest_template` SET `OfferRewardText`='You\'ve proven yourself today, $n.$b$bWhile I have little else to give you, I do have this crown; a fitting gift for a conqueror. May it light your way.' WHERE `entry` = 9365 AND `patch`=9;
 
+-- Fix order of objectives for quest A Light in Dark Places.
+UPDATE `quest_template` SET `ObjectiveText1`='Flame of Dire Maul', `ReqCreatureOrGOId1`=-181346 WHERE `entry`=9319;
+UPDATE `quest_template` SET `ObjectiveText2`='Flame of Blackrock Spire', `ReqCreatureOrGOId2`=-181347 WHERE `entry`=9319;
+UPDATE `quest_template` SET `ObjectiveText3`='Flame of Stratholme', `ReqCreatureOrGOId3`=-181348 WHERE `entry`=9319;
+UPDATE `quest_template` SET `ObjectiveText4`='Flame of the Scholomance', `ReqCreatureOrGOId4`=-181349 WHERE `entry`=9319;
+
 -- INCORRECT
 UPDATE `gameobject_template` SET `data12` = 181290 WHERE `entry` = 181347;
 UPDATE `gameobject_template` SET `patch` = 9 WHERE `entry` = 181346
