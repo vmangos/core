d7bed8992f7614bfaddb56a98cc8dbcd892412e7
NickTyrer <nickyty8@gmail.com>
2021-09-29 16:57:18 +0100

    Update 20210810191400_world.sql

--- sql/migrations/20210810191400_world.sql
+++ sql/migrations/20210810191400_world.sql
@@ -1078,7 +1078,6 @@ INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `pos
 INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `health_percent`, `mana_percent`, `patch_min`, `patch_max`) VALUES (87023, 3509, 0, -8839.57, 666.54, 97.95, 0.48, 300, 300, 100, 0, 0, 10);
 
 -- Remove Old Waypoints and Scripts
-DELETE FROM `creature_movement` WHERE `id` IN 
 DELETE FROM `creature_movement` WHERE `id`  IN (87092, 87091, 87090, 87089, 87082, 87088, 87023, 84028);
 DELETE FROM `creature_movement_scripts` WHERE `id` IN (
 8708224, 
