66ab14c4e1b7489f415aa5ffbf84d45a66b1e489
NickTyrer <nickyty8@gmail.com>
2021-10-06 17:00:56 +0100

    Update 20210810191400_world.sql

--- sql/migrations/20210810191400_world.sql
+++ sql/migrations/20210810191400_world.sql
@@ -1870,12 +1870,12 @@ INSERT INTO `gossip_menu` (`entry`, `text_id`) VALUES
 (1842,2493),
 (1843,2494);
 INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_broadcast_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `box_broadcast_text`, `condition_id`) VALUES
-(1842, 0, 0, 'I present you with proof of my deeds, Cyrus.', 4754, 1, 1, 0, 0, 0, 0, 0, '', 0, 214),
+(1842, 0, 0, 'I present you with proof of my deeds, Cyrus.', 4754, 1, 1, -1, 0, 4022, 0, 0, '', 0, 214),
 (1842, 1, 0, 'I do not possess any proof, Cyrus.', 4755, 1, 1, 1843, 0, 0, 0, 0, '', 0, 216),
 (1843, 0, 0, 'I am ready!', 4759, 1, 1, -1, 0, 4023, 0, 0, '', 0, 217);
 
 -- Unlock Quest 4022 if Quest 3481 is Complete and you have Item 10575
-UPDATE `quest_template` SET `RequiredCondition` = 210, `PrevQuestId` = 0, `NextQuestId` = 0 WHERE `entry`=4022;
+UPDATE `quest_template` SET `RequiredCondition` = 210, `PrevQuestId` = 0, `NextQuestId` = 0, `SpecialFlags` = 2 WHERE `entry`=4022;
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (208, 8, 3481, 0, 0, 0, 0);
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (209, 23, 10575, 1, 0, 0, 0);
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (210, -1, 208, 209, 0, 0, 0);
@@ -1898,6 +1898,9 @@ INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3
 -- Move Script to Summon Drake to Gossip Option
 DELETE FROM `quest_start_scripts` WHERE `id`=4023;
 INSERT INTO `gossip_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4023, 5, 10, 9461, 60000, 0, 0, 0, 0, 0, 0, 4, 0, -1, 1, -7672.74, -2973.34, 132.52, 1.86749, 0, 'Summon Frenzied Black Drake');
+
+-- Add Script to Complete Quest
+INSERT INTO `gossip_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (4022, 0, 7, 4022, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Cyrus Therepentous - Complete Quest');
 
 
 -- End of migration.
