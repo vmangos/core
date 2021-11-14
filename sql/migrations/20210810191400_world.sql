c0622466a3249d0b49d0afd9ad5d5a04c24b5a95
NickTyrer <nickyty8@gmail.com>
2021-11-14 09:38:56 +0000

    Update 20210810191400_world.sql

--- sql/migrations/20210810191400_world.sql
+++ sql/migrations/20210810191400_world.sql
@@ -2952,6 +2952,19 @@ UPDATE `quest_template` SET `PrevQuestId` = 0 WHERE  `entry` = 691;
 UPDATE `quest_template` SET `RequiredCondition` = 790 WHERE `entry` = 690;
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (790, 22, 691, 0, 0, 0, 0);
 
+-- -----------------------------------------------------------------------------------------
+
+-- Add Missing Text to Gilnid
+DELETE FROM `creature_ai_scripts` WHERE `id`=176301;
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (176301, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1147, 1146, 1145, 0, 0, 0, 0, 0, 0, 'Gilnid - Say Text');
+
+-- -----------------------------------------------------------------------------------------
+
+-- Arikara should be Tameable and Cast Spell on Spawn (Credit Cmangos)
+UPDATE `creature_template` SET `unit_class` = 1, `ai_name` = 'EventAI' WHERE `entry` = 10882;
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (1088201, 10882, 0, 11, 0, 100, 1, 0, 0, 0, 0, 1088201, 0, 0, 'Arikara - Cast Spell On Spawn');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (1088201, 0, 15, 10389, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Arikara - Cast Spell');
+
 
 -- End of migration.
 END IF;
