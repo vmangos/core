612e7712e22f89219f23340f97276b714a50778c
ratkosrb <ratkomladic2@abv.bg>
2021-12-01 07:22:30 +0200

    Add completion script for quest Forbidden Knowledge.

--- sql/migrations/20211201050010_world.sql
+++ sql/migrations/20211201050010_world.sql
@@ -11,9 +11,10 @@ INSERT INTO `migrations` VALUES ('20211201050010');
 
 -- Completion script for quest Solution to Doom.
 DELETE FROM `quest_end_scripts` WHERE `id`=709;
-INSERT INTO `quest_end_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
+INSERT INTO `quest_end_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES 
 (709, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 914, 0, 0, 0, 0, 0, 0, 0, 0, 'Solution to Doom: Theldurin the Lost - Say Text'),
-(709, 0, 1, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Solution to Doom: Theldurin the Lost - Emote OneShotKneel'),
+(709, 0, 28, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Solution to Doom: Theldurin the Lost - Set Stand State to Kneel'),
+(709, 2, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Solution to Doom: Theldurin the Lost - Set Stand State to Stand'),
 (709, 3, 1, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Solution to Doom: Theldurin the Lost - Emote OneShotCheer'),
 (709, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 915, 0, 0, 0, 0, 0, 0, 0, 0, 'Solution to Doom: Theldurin the Lost - Say Text');
 UPDATE `quest_template` SET `CompleteScript`=709 WHERE `entry`=709;
