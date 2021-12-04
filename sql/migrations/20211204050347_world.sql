8409022f10328fbffb55c209ae88ea9ed744b4aa
ratkosrb <ratkomladic2@abv.bg>
2021-12-04 07:11:11 +0200

    Assign completion script to other Return to the Bulwark quests.

--- sql/migrations/20211204050347_world.sql
+++ sql/migrations/20211204050347_world.sql
@@ -13,7 +13,7 @@ INSERT INTO `migrations` VALUES ('20211204050347');
 DELETE FROM `quest_end_scripts` WHERE `id`=5230;
 INSERT INTO `quest_end_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
 (5230, 0, 15, 23947, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Return to the Bulwark: Shadow Priestess Vandis - Cast Spell Power Word: Fortitude');
-UPDATE `quest_template` SET `CompleteScript`=5230 WHERE `entry`=5230;
+UPDATE `quest_template` SET `CompleteScript`=5230 WHERE `entry` IN (5230, 5232, 5234, 5236);
 
 
 -- End of migration.
