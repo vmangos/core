b62abbf533b3551ca9d54e4d35570bc43ab8ecb8
NickTyrer <nickyty8@gmail.com>
2021-12-14 17:57:40 +0000

    Update 20211212152302_world.sql

--- sql/migrations/20211212152302_world.sql
+++ sql/migrations/20211212152302_world.sql
@@ -13,18 +13,21 @@ INSERT INTO `migrations` VALUES ('20211212152302');
 DELETE FROM `creature` WHERE `guid` IN (SELECT `guid` FROM `game_event_creature` WHERE `event` = 153);
 DELETE FROM `game_event` WHERE `entry` = 153;
 DELETE FROM `game_event_creature` WHERE `event` = 153;
+DELETE FROM `creature_groups` WHERE `leader_guid` = 301764;
+DELETE FROM `creature_movement_scripts` WHERE `id` = 14;
+DELETE FROM `creature_movement` WHERE  `id` IN (301764, 301765, 301761, 301763, 301762);
 
 -- Misc
 UPDATE `creature_template` SET `movement_type` = '0' WHERE `entry` = 2434;
 UPDATE `creature` SET `position_x` = -576.589, `position_y` = -555.617, `position_z` = 33.4733 WHERE `id` = 2435;
 UPDATE `broadcast_text` SET `chat_type` = 2 WHERE `entry` = 623;
 
--- Events list for Farmer Kent
+-- Events List for Farmer Kent
 UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry`=2436;
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (243601, 2436, 0, 4, 0, 100, 0, 0, 0, 0, 0, 243601, 0, 0, 'Farmer Kent - Talk on Aggro');
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES (243601, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 628, 627, 0, 0, 0, 0, 0, 0, 0, 'Farmer Kent - Talk');
 
--- Events list for Southshore Crier
+-- Events List for Southshore Crier
 DELETE FROM `creature_ai_events` WHERE `creature_id`=2435;
 DELETE FROM `creature_ai_scripts` WHERE `id` IN (243502, 243503, 243504);
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES
