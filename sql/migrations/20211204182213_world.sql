7004cd8c47be16ddf0ebb8152f46bfde4e397934
NickTyrer <nickyty8@gmail.com>
2021-12-19 15:17:39 +0000

    Update 20211204182213_world.sql

--- sql/migrations/20211204182213_world.sql
+++ sql/migrations/20211204182213_world.sql
@@ -9,11 +9,8 @@ INSERT INTO `migrations` VALUES ('20211204182213');
 -- Add your query below.
 
 
--- Completion script for quest The Stave of Equinex.
-INSERT INTO `quest_end_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
-(2879, 1, 76, 146082, 22, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2976.61, 2743.18, 52.4701, 0.680677, 0, 'The Stave of Equinex - Equinex Monolith: Summon GameObject Equinex Monolith Lights 1'),
-(2879, 1, 76, 146083, 23, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2977.17, 2743.14, 52.4016, 6.21337, 0, 'The Stave of Equinex - Equinex Monolith: Summon GameObject Equinex Monolith Lights 2');
-UPDATE `quest_template` SET `CompleteScript` = 2879 WHERE `entry` = 2879;
+-- Remove Seep Immunity From Sunken Temple Dragonkin
+UPDATE `creature_template` SET `mechanic_immune_mask` = 0 WHERE `entry` IN (5283, 5277, 8319, 5280);
 
 
 -- End of migration.
