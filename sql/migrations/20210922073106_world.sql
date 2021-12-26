25edad5e3a9b1aca4d5a58b6b16590bc4060fc4d
ratkosrb <ratkomladic2@abv.bg>
2021-12-26 15:06:37 +0200

    Fix migration error.

--- sql/migrations/20210922073106_world.sql
+++ sql/migrations/20210922073106_world.sql
@@ -13,7 +13,7 @@ INSERT INTO `migrations` VALUES ('20210922073106');
 DELETE FROM `script_texts` WHERE `entry` IN (-1047000, -1047001, -1047002, -1047003, -1047004, -1047005, -1047006, -1047007, -1047008, -1047009, -1047010, -1047011, -1047012);
 
 -- Assign emote.
-UPDATE `broadcast_text` SET `emote_id`=25 WHERE `entry`=1483;
+UPDATE `broadcast_text` SET `emote_id1`=25 WHERE `entry`=1483;
 
 
 -- End of migration.
