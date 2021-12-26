c1ee5980bea51d89a82dd2b8fea37350d6054673
NickTyrer <nickyty8@gmail.com>
2021-12-26 09:08:11 +0000

    Update 20211212152302_world.sql

--- sql/migrations/20211212152302_world.sql
+++ sql/migrations/20211212152302_world.sql
@@ -291,9 +291,6 @@ INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`
 (243525, 0, 20, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 'Shadowy Assassin - Set Movement Type'),
 (243526, 0, 20, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 'Shadowy Assassin - Set Movement Type');
 
-
-/*
--- TBC - DO ASSASSINS DO A CHECK ON EVENT END AND TALK IF MAGISTARTE IS ALIVE OR DEAD?
 -- Magistrate Henry Maleb Alive
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (2434, 20, 2276, 300, 0, 0, 0);
 INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
@@ -329,7 +326,6 @@ INSERT INTO `generic_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`
 (243524, 2040, 0, 0, 0, 0, 0, 0, 0, 0, 0, 630, 631, 632, 0, 0, 0, 0, 0, 2435, 'Shadowy Assassin - Talk'),
 (243525, 2040, 0, 0, 0, 0, 0, 0, 0, 0, 0, 630, 631, 632, 0, 0, 0, 0, 0, 2435, 'Shadowy Assassin - Talk'),
 (243526, 2040, 0, 0, 0, 0, 0, 0, 0, 0, 0, 630, 631, 632, 0, 0, 0, 0, 0, 2435, 'Shadowy Assassin - Talk');
-*/
 
 
 -- End of migration.
