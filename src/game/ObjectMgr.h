4cd5ca80997a18de296129531158ae3cfb76898f
schell244 <Schell244@web.de>
2022-03-17 05:15:28 +0100

    Fix gossip condition for Riggle Bassbait (#1481)

--- src/game/ObjectMgr.h
+++ src/game/ObjectMgr.h
@@ -492,6 +492,7 @@ enum PermVariables
     VAR_TOURNAMENT  = 30021,    // last quest completion time
     VAR_TOURN_GOES  = 30022,    // tournament was started already
     VAR_TOURN_OVER  = 30023,    // tournament is over
+    VAR_TOURN_WINNER = 30056,   // for gosssip menu condition
 
     // War Effort shared contributions
     VAR_WE_ALLIANCE_COPPER          = 30024,
