2c2609d37bc976e91b9d7fd3c33aa3177c061614
schell244 <>
2022-03-28 22:53:32 +0200

    convert script to EventAI

--- sql/migrations/20220322181309_world.sql
+++ sql/migrations/20220322181309_world.sql
@@ -8,8 +8,21 @@ IF v=0 THEN
 INSERT INTO `migrations` VALUES ('20220322181309');
 -- Add your query below.
 
--- Assign script to Jang
-UPDATE `creature_template` SET `script_name`='npc_jang' WHERE `entry`=15078;
+-- New condition: fishing event has no winner
+INSERT INTO `conditions` VALUES (7716, 11, 30056, 0, 0, 0, 0);
+
+-- Events list for Jang
+DELETE FROM `creature_ai_events` WHERE `creature_id`=15078;
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES
+(1507801, 15078, 0, 1, 0, 100, 1, 1000, 1000, 1000, 1000, 1507801, 0, 0, 'Jang - Add Quest Flag'),
+(1507802, 15078, 0, 1, 0, 100, 1, 1000, 1000, 1000, 1000, 1507802, 0, 0, 'Jang - Remove Quest Flag');
+
+DELETE FROM `creature_ai_scripts` WHERE `id` IN (1507801, 1507802);
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
+(1507801, 0, 0, 4, 147, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7715, 'Jang - Add Quest Flag'),
+(1507802, 0, 0, 4, 147, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7716, 'Jang - Remove Quest Flag');
+
+UPDATE `creature_template` SET `ai_name`='EventAI' WHERE `entry`=15078;
 
 -- Master Angler
 UPDATE `locales_quest` SET `OfferRewardText_loc3`='Beeilt Euch und reicht die Fische ein, wenn Ihr den Titel des Meisteranglers erwerben wollt! Ich stehe hier schon seit STUNDEN ohne etwas zu essen! Gebt mir die Viecher schon her!' WHERE `entry`=8193;
