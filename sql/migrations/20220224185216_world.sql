cef6c5864968b407c73c71247bdc2169c1cef73b
NickTyrer <nickyty8@gmail.com>
2022-04-17 17:07:58 +0100

    Update 20220224185216_world.sql

--- sql/migrations/20220224185216_world.sql
+++ sql/migrations/20220224185216_world.sql
@@ -3371,6 +3371,11 @@ DELETE FROM `npc_trainer` WHERE `entry` = 2818;
 INSERT INTO `npc_trainer` (`entry`, `spell`, `spellcost`, `reqskill`, `reqskillvalue`, `reqlevel`, `build_min`, `build_max`) VALUES
 (2818, 4095, 450, 185, 175, 25, 0, 5875);
 
+-- ----------------------------------------------------------------------
+
+-- The Text for Laird's Response is Missing Linebreaks (credit TrinityCore)
+UPDATE `page_text` SET `text` = 'Nessa,$B$BIt is good to hear from you.$B$BThe collection of bones and scales  you sent me was incredible.  Never have I seen fish of that size off the mainland.  the fishers of Rut'theran must pull in easily a ton of fish each day!$B$BI envy the size of their bounty, but I am a small bit troubled as well.  Could our new world tree make the fish grow to such a size?  If that is so, what other effects might it have?$B$BQuestions to ponder,$B-Laird' WHERE `entry` = 2513;
+
 
 -- End of migration.
 END IF;
