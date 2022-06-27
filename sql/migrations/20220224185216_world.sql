f522c853c03df5926593f3798ff7e7f66f2bf2a9
NickTyrer <nickyty8@gmail.com>
2022-06-27 16:09:07 +0100

    Update 20220224185216_world.sql

--- sql/migrations/20220224185216_world.sql
+++ sql/migrations/20220224185216_world.sql
@@ -5933,7 +5933,7 @@ DELETE FROM `quest_end_scripts` WHERE `id`=5803;
 INSERT INTO `quest_end_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
 (5803, 0, 0, 4, 147, 7, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Araj\'s Scarab - Alchemist Arbington: npc\\_flag questgiver+gossip+vendor removed'),
 (5803, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7278, 0, 0, 0, 0, 0, 0, 0, 0, 'Araj\'s Scarab - Alchemist Arbington: Say text 1'),
-(5803, 3, 0, 15, 9744, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Araj\'s Scarab - Alchemist Arbington: Cast spell \"Psychometry\"'),
+(5803, 3, 0, 15, 9744, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Araj\'s Scarab - Alchemist Arbington: Cast spell'),
 (5803, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7279, 0, 0, 0, 0, 0, 0, 0, 0, 'Araj\'s Scarab - Alchemist Arbington: Say text 2'),
 (5803, 10, 0, 4, 147, 7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Araj\'s Scarab - Alchemist Arbington: npc\\_flag questgiver+gossip+vendor added');
 
@@ -5941,7 +5941,7 @@ DELETE FROM `quest_end_scripts` WHERE `id`=5804;
 INSERT INTO `quest_end_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
 (5804, 0, 0, 4, 147, 7, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Araj\'s Scarab - Apothecary Dithers: npc\\_flag questgiver+gossip+vendor removed'),
 (5804, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7278, 0, 0, 0, 0, 0, 0, 0, 0, 'Araj\'s Scarab - Apothecary Dithers: Say text 1'),
-(5804, 3, 0, 15, 9744, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Araj\'s Scarab - Apothecary Dithers: Cast spell \"Psychometry\"'),
+(5804, 3, 0, 15, 9744, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Araj\'s Scarab - Apothecary Dithers: Cast spell"'),
 (5804, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7279, 0, 0, 0, 0, 0, 0, 0, 0, 'Araj\'s Scarab - Apothecary Dithers: Say text 2'),
 (5804, 10, 0, 4, 147, 7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Araj\'s Scarab - Apothecary Dithers: npc\\_flag questgiver+gossip+vendor added');
 
