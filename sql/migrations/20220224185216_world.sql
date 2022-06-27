87762f68161ca70de448f6daa5fbfc2e577407e2
NickTyrer <nickyty8@gmail.com>
2022-06-27 16:59:06 +0100

    Update 20220224185216_world.sql

--- sql/migrations/20220224185216_world.sql
+++ sql/migrations/20220224185216_world.sql
@@ -5214,13 +5214,13 @@ DELETE FROM `reference_loot_template` WHERE `entry` IN (2043, 1011) AND `item` =
 
 -- Add Text To Creature
 UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry` = 6516;
--- 223: Source's Guid Is 24081
-INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (223, 52, 24081, 0, 0, 0, 0);
+-- 224: Source's Guid Is 24081
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (224, 52, 24081, 0, 0, 0, 0);
 
 -- Events list for Un'Goro Thunderer
 DELETE FROM `creature_ai_events` WHERE `creature_id`=6516;
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES
-(651601, 6516, 223, 1, 0, 100, 1, 0, 0, 60000, 180000, 651601, 0, 0, 'Un\'Goro Thunderer - Talk - OOC');
+(651601, 6516, 224, 1, 0, 100, 1, 0, 0, 60000, 180000, 651601, 0, 0, 'Un\'Goro Thunderer - Talk - OOC');
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
 (651601, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5045, 5046, 0, 0, 0, 0, 0, 0, 0, 'Un\'Goro Thunderer - Talk');
 
