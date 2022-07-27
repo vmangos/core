ccbc5a0cbd815354839ff6f7d987f20ad415fc29
NickTyrer <nickyty8@gmail.com>
2022-07-27 19:47:52 +0100

    Update 20220319115102_world.sql

--- sql/migrations/20220319115102_world.sql
+++ sql/migrations/20220319115102_world.sql
@@ -301,6 +301,26 @@ DELETE FROM `pool_template` WHERE `entry` = 1607;
 -- Set Correct Creature ID
 UPDATE `creature` SET `id` = 8566 WHERE `guid` = 300647;
 
+-- ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Events list for Techbot
+DELETE FROM `creature_ai_events` WHERE `creature_id`=6231;
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES
+(623101, 6231, 0, 0, 0, 100, 3, 1000, 1000, 11000, 11000, 623101, 623102, 623103, 'Techbot - Talk - In Combat');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
+(623101, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2426, 2427, 2428, 2429, 0, 0, 0, 0, 0, 'Techbot - Talk');
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
+(623102, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2430, 2431, 2433, 2434, 0, 0, 0, 0, 0, 'Techbot - Talk');
+DELETE FROM `creature_ai_scripts` WHERE `id`=623103;
+INSERT INTO `creature_ai_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
+(623103, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2435, 2436, 2437, 2438, 0, 0, 0, 0, 0, 'Techbot - Talk');
+
+-- Add Missing Spell 10860 
+REPLACE INTO `creature_spells` (`entry`, `name`, `spellId_1`, `probability_1`, `castTarget_1`, `targetParam1_1`, `targetParam2_1`, `castFlags_1`, `delayInitialMin_1`, `delayInitialMax_1`, `delayRepeatMin_1`, `delayRepeatMax_1`, `scriptId_1`, `spellId_2`, `probability_2`, `castTarget_2`, `targetParam1_2`, `targetParam2_2`, `castFlags_2`, `delayInitialMin_2`, `delayInitialMax_2`, `delayRepeatMin_2`, `delayRepeatMax_2`, `scriptId_2`, `spellId_3`, `probability_3`, `castTarget_3`, `targetParam1_3`, `targetParam2_3`, `castFlags_3`, `delayInitialMin_3`, `delayInitialMax_3`, `delayRepeatMin_3`, `delayRepeatMax_3`, `scriptId_3`, `spellId_4`, `probability_4`, `castTarget_4`, `targetParam1_4`, `targetParam2_4`, `castFlags_4`, `delayInitialMin_4`, `delayInitialMax_4`, `delayRepeatMin_4`, `delayRepeatMax_4`, `scriptId_4`, `spellId_5`, `probability_5`, `castTarget_5`, `targetParam1_5`, `targetParam2_5`, `castFlags_5`, `delayInitialMin_5`, `delayInitialMax_5`, `delayRepeatMin_5`, `delayRepeatMax_5`, `scriptId_5`, `spellId_6`, `probability_6`, `castTarget_6`, `targetParam1_6`, `targetParam2_6`, `castFlags_6`, `delayInitialMin_6`, `delayInitialMax_6`, `delayRepeatMin_6`, `delayRepeatMax_6`, `scriptId_6`, `spellId_7`, `probability_7`, `castTarget_7`, `targetParam1_7`, `targetParam2_7`, `castFlags_7`, `delayInitialMin_7`, `delayInitialMax_7`, `delayRepeatMin_7`, `delayRepeatMax_7`, `scriptId_7`, `spellId_8`, `probability_8`, `castTarget_8`, `targetParam1_8`, `targetParam2_8`, `castFlags_8`, `delayInitialMin_8`, `delayInitialMax_8`, `delayRepeatMin_8`, `delayRepeatMax_8`, `scriptId_8`) VALUES (62310, 'Gnomeregan - Techbot', 10852, 100, 0, 0, 0, 0, 5, 5, 19, 19, 0, 10855, 100, 1, 0, 0, 0, 4, 8, 15, 25, 0, 10858, 100, 1, 0, 0, 0, 10, 10, 22, 22, 0, 10860, 100, 0, 0, 0, 0, 20, 30, 30, 60, 0, 0, 100, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 1, 0, 0, 0, 0, 0, 0, 0, 0);
+
+-- ------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 
 -- End of migration.
 END IF;
