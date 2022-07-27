1b53196a23fe1f790c5d79fc29dd373de7986444
NickTyrer <nickyty8@gmail.com>
2022-07-27 19:52:47 +0100

    Update 20220319115102_world.sql

--- sql/migrations/20220319115102_world.sql
+++ sql/migrations/20220319115102_world.sql
@@ -303,7 +303,7 @@ UPDATE `creature` SET `id` = 8566 WHERE `guid` = 300647;
 
 -- ------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- Events list for Techbot
+-- Events list for Techbot (credit cmangos) 
 DELETE FROM `creature_ai_events` WHERE `creature_id`=6231;
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES
 (623101, 6231, 0, 0, 0, 100, 3, 1000, 1000, 11000, 11000, 623101, 623102, 623103, 'Techbot - Talk - In Combat');
@@ -317,6 +317,11 @@ INSERT INTO `creature_ai_scripts` (`id`, `delay`, `priority`, `command`, `datalo
 
 -- Add Missing Spell 10860 
 REPLACE INTO `creature_spells` (`entry`, `name`, `spellId_1`, `probability_1`, `castTarget_1`, `targetParam1_1`, `targetParam2_1`, `castFlags_1`, `delayInitialMin_1`, `delayInitialMax_1`, `delayRepeatMin_1`, `delayRepeatMax_1`, `scriptId_1`, `spellId_2`, `probability_2`, `castTarget_2`, `targetParam1_2`, `targetParam2_2`, `castFlags_2`, `delayInitialMin_2`, `delayInitialMax_2`, `delayRepeatMin_2`, `delayRepeatMax_2`, `scriptId_2`, `spellId_3`, `probability_3`, `castTarget_3`, `targetParam1_3`, `targetParam2_3`, `castFlags_3`, `delayInitialMin_3`, `delayInitialMax_3`, `delayRepeatMin_3`, `delayRepeatMax_3`, `scriptId_3`, `spellId_4`, `probability_4`, `castTarget_4`, `targetParam1_4`, `targetParam2_4`, `castFlags_4`, `delayInitialMin_4`, `delayInitialMax_4`, `delayRepeatMin_4`, `delayRepeatMax_4`, `scriptId_4`, `spellId_5`, `probability_5`, `castTarget_5`, `targetParam1_5`, `targetParam2_5`, `castFlags_5`, `delayInitialMin_5`, `delayInitialMax_5`, `delayRepeatMin_5`, `delayRepeatMax_5`, `scriptId_5`, `spellId_6`, `probability_6`, `castTarget_6`, `targetParam1_6`, `targetParam2_6`, `castFlags_6`, `delayInitialMin_6`, `delayInitialMax_6`, `delayRepeatMin_6`, `delayRepeatMax_6`, `scriptId_6`, `spellId_7`, `probability_7`, `castTarget_7`, `targetParam1_7`, `targetParam2_7`, `castFlags_7`, `delayInitialMin_7`, `delayInitialMax_7`, `delayRepeatMin_7`, `delayRepeatMax_7`, `scriptId_7`, `spellId_8`, `probability_8`, `castTarget_8`, `targetParam1_8`, `targetParam2_8`, `castFlags_8`, `delayInitialMin_8`, `delayInitialMax_8`, `delayRepeatMin_8`, `delayRepeatMax_8`, `scriptId_8`) VALUES (62310, 'Gnomeregan - Techbot', 10852, 100, 0, 0, 0, 0, 5, 5, 19, 19, 0, 10855, 100, 1, 0, 0, 0, 4, 8, 15, 25, 0, 10858, 100, 1, 0, 0, 0, 10, 10, 22, 22, 0, 10860, 100, 0, 0, 0, 0, 20, 30, 30, 60, 0, 0, 100, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 1, 0, 0, 0, 0, 0, 0, 0, 0);
+
+
+UPDATE `creature_template` SET `ai_name`='EventAI' WHERE  `entry`=6231 AND `patch`=0;
+UPDATE `creature` SET `position_x` = -4925.538, `position_y` = 824.91534, `position_z` = 247.46948, `orientation` = 1.731216192245483398, `wander_distance` = 3, `movement_type` = 1 WHERE `id` = 6231;
+DELETE FROM `creature` WHERE `id` = 7732;
 
 -- ------------------------------------------------------------------------------------------------------------------------------------------------------
 
