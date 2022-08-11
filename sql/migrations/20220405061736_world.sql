8d9edea63f9c5fa850f478800735bb10dcd559ee
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2022-08-11 13:05:02 +0300

    new lines

--- sql/migrations/20220405061736_world.sql
+++ sql/migrations/20220405061736_world.sql
@@ -7,7 +7,11 @@ SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20220405061736');
 IF v=0 THEN
 INSERT INTO `migrations` VALUES ('20220405061736');
 -- Add your query below.
+
+
  DELETE FROM `gameobject` WHERE `guid`= 99805;
+ 
+ 
 -- End of migration.
 END IF;
 END??
