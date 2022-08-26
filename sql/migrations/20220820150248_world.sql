247d245309bd0c5b9c9da487e063bde4248dc52e
schell244 <>
2022-08-26 21:01:33 +0200

    extract new guids, make them not conflic with other PRs

--- sql/migrations/20220820150248_world.sql
+++ sql/migrations/20220820150248_world.sql
@@ -8,17 +8,22 @@ IF v=0 THEN
 INSERT INTO `migrations` VALUES ('20220820150248');
 -- Add your query below.
 
+-- New bell guids
+SET @BELL1 := 60000;
+SET @BELL2 := 60001;
+SET @BELL3 := 60002;
+
 -- Missing alliance bell spawns
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`, `spawn_flags`, `visibility_mod`, `patch_min`, `patch_max`) VALUES
-(18178,176573,1,-3667.09,-4754,1.80047,2.26893,0,0,0.906307,0.422619,900,900,100,1,0,0,0,10),    -- Theramore Lighthouse
-(18180,176573,0,-8463.69,592.138,94.8484,0.401425,0,0,0.199368,0.979925,900,900,100,1,0,0,0,10), -- Stormwind (Dwarven District)
-(18182,176573,0,-8998.99,852.309,105.596,0.872664,0,0,0.422618,0.906308,900,900,100,1,0,0,0,10); -- Stormwind (Mage Quarter)
+(@BELL1,176573,1,-3667.09,-4754,1.80047,2.26893,0,0,0.906307,0.422619,900,900,100,1,0,0,0,10),    -- Theramore Lighthouse
+(@BELL2,176573,0,-8463.69,592.138,94.8484,0.401425,0,0,0.199368,0.979925,900,900,100,1,0,0,0,10), -- Stormwind (Dwarven District)
+(@BELL3,176573,0,-8998.99,852.309,105.596,0.872664,0,0,0.422618,0.906308,900,900,100,1,0,0,0,10); -- Stormwind (Mage Quarter)
 
 -- Link to hourly bell event
 INSERT INTO `game_event_gameobject` (`guid`, `event`) VALUES
-(18178,78),
-(18180,78),
-(18182,78);
+(@BELL1,78),
+(@BELL2,78),
+(@BELL3,78);
 
 -- End of migration.
 END IF;
