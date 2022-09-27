0d5a47711afd2a143fac3a27c7693ac57db6f74e
FlagFlayer <76889547+FlagFlayer@users.noreply.github.com>
2022-09-27 06:08:01 +0300

    Update and rename 20220925022431 to 20220925022431_world.sql

--- sql/migrations/20220925022431_world.sql
+++ sql/migrations/20220925022431_world.sql
@@ -11,6 +11,12 @@ INSERT INTO `migrations` VALUES ('20220925022431');
 -- Set Mulgore, Red Cloud Mesa GY for Brambleblade Ravine
 INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `build_min`) VALUES (34, 358, 67, 0);
 
+-- Condition to return false if quest 769 is complete
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (769, 8, 769, 0, 0, 0, 1);
+
+-- Quest 769 (Kodo Hide Bag) requires a Leatherworking skill of 10 instead of 1, should not be available if it has already been completed.
+UPDATE `quest_template` SET `RequiredSkillValue` = 10, `RequiredCondition` = 769 WHERE `entry` = 3447;
+
 -- End of migration.
 END IF;
 END??
