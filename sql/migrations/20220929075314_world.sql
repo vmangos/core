f10b2bffc1f0b65162d5aa2388e67c4e2b786ee8
FlagFlayer <76889547+FlagFlayer@users.noreply.github.com>
2022-09-29 11:04:42 +0300

    Create 20220929075314_world.sql

---
+++ sql/migrations/20220929075314_world.sql
@@ -0,0 +1,19 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20220925022431');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20220925022431');
+-- Add your query below.
+
+-- Quest 823 (Report to Orgnil) is a follow-up to Quest 794 (Burning Blade Medallion)
+UPDATE `quest_template` SET `PrevQuestId` = 794 WHERE `entry` = 823;
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
