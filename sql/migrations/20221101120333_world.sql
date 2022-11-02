0e7161b6692e8b5bace4864396efc2c4a73bac73
FlagFlayer <76889547+FlagFlayer@users.noreply.github.com>
2022-11-02 13:13:44 +0200

    Update 20221101120333_world.sql

--- sql/migrations/20221101120333_world.sql
+++ sql/migrations/20221101120333_world.sql
@@ -17,6 +17,24 @@ UPDATE `quest_template` SET `RequiredSkillValue` = 40 WHERE `entry` = 866;
 -- 1618 (Gearing Redridge) should require a Blacksmithing skill of 60 (currently requires 70)
 UPDATE `quest_template` SET `RequiredSkillValue` = 60 WHERE `entry` = 1618;
 
+-- Condition to return true if 2855 (Wild Leather Shoulders (horde side)) is complete
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (20000, 8, 2855, 0, 0, 0, 0);
+
+-- Condition to return true if 2856 (Wild Leather Vest (horde side)) is complete
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (20001, 8, 2856, 0, 0, 0, 0);
+
+-- Condition to return true if 2857 (Wild Leather Helmet (horde side)) is complete
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (20002, 8, 2857, 0, 0, 0, 0);
+
+-- Condition to return true if the previous 3 conditions are true
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (20003, -1, 20000, 20001, 20002, 0, 0);
+
+-- Set quest requirement for 2858 (Wild Leather Boots (horde side)) to previous condition
+UPDATE `quest_template` SET `RequiredSkillValue` = 200, `PrevQuestId` = 0, `NextQuestId` = 0, `RequiredCondition` = 20003 WHERE `entry` = 2858;
+
+-- Set quest requirement for 2859 (Wild Leather Leggings (horde side)) to previous condition
+UPDATE `quest_template` SET `RequiredSkillValue` = 200, `PrevQuestId` = 0, `NextQuestId` = 0, `RequiredCondition` = 20003 WHERE `entry` = 2859;
+
 -- End of migration.
 END IF;
 END??
