3179d6955e7f410159d4f1f05054658d36efcc72
gamemechanicwow <stesve@gmail.com>
2022-11-07 20:00:34 +0100

    Update 20221106005233_world.sql

--- sql/migrations/20221106005233_world.sql
+++ sql/migrations/20221106005233_world.sql
@@ -38,7 +38,7 @@ DELETE FROM `creature_ai_scripts` WHERE `id`=272901;
 
 -- Events list for Prairie Wolf
 DELETE FROM `creature_ai_events` WHERE `creature_id`=2958;
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (295801, 2958, 0, 9, 0, 100, 1, 0, 5, 45000, 45000, 295802, 0, 0, 'Prairie Wolf - Cast Threatening Growl');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (295801, 2958, 0, 9, 0, 100, 1, 0, 5, 45000, 45000, 295801, 0, 0, 'Prairie Wolf - Cast Threatening Growl');
 
 DELETE FROM `creature_ai_scripts` WHERE `id`=295801;
 DELETE FROM `creature_ai_scripts` WHERE `id`=295802;
@@ -51,7 +51,7 @@ DELETE FROM `creature_ai_scripts` WHERE `id`=295901;
 
 -- Events list for Prairie Wolf Alpha
 DELETE FROM `creature_ai_events` WHERE `creature_id`=2960;
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (296001, 2960, 0, 9, 0, 100, 1, 0, 5, 45000, 45000, 296002, 0, 0, 'Prairie Wolf Alpha - Cast Threatening Growl');
+INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES (296001, 2960, 0, 9, 0, 100, 1, 0, 5, 45000, 45000, 296001, 0, 0, 'Prairie Wolf Alpha - Cast Threatening Growl');
 
 DELETE FROM `creature_ai_scripts` WHERE `id`=296001;
 DELETE FROM `creature_ai_scripts` WHERE `id`=296002;
