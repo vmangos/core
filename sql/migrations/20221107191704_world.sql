0cd2a5ae051d8d63ef9a09faf2b9753b4e8f13b0
FlagFlayer <76889547+FlagFlayer@users.noreply.github.com>
2022-11-07 21:19:51 +0200

    Create 20221107191704_world.sql

---
+++ sql/migrations/20221107191704_world.sql
@@ -0,0 +1,16 @@
+DROP PROCEDURE IF EXISTS add_migration;
+delimiter ??
+CREATE PROCEDURE `add_migration`()
+BEGIN
+DECLARE v INT DEFAULT 1;
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20221107191704');
+IF v=0 THEN
+INSERT INTO `migrations` VALUES ('20221107191704');
+-- Add your query below.
+
+-- End of migration.
+END IF;
+END??
+delimiter ; 
+CALL add_migration();
+DROP PROCEDURE IF EXISTS add_migration;
