bea8587b682fdc9732f835c80e63f5bf0eeef901
FlagFlayer <76889547+FlagFlayer@users.noreply.github.com>
2022-11-21 20:05:05 +0200

    Update 20221107191704_world.sql

--- sql/migrations/20221107191704_world.sql
+++ sql/migrations/20221107191704_world.sql
@@ -17,12 +17,20 @@ INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3
 (10999, 17, 20219, 1, 0, 0, 0), -- Condition to return true if the player has NOT learnt Gnomish Engineering
 (11000, 17, 20222, 1, 0, 0, 0), -- Condition to return true if the player has NOT learnt Goblin Engineering
 (11001, -1, 10999, 11000, 0, 0, 0) -- Condition to return true if the player has NOT learnt Gnomish Engineering AND Goblin Engineering
-(11002, -1, 11001, 10998, 10997, 0, 0) -- Condition to return true if the player doesn't have an Engineering specialisation, completed one of the Engineering specialisation quests, and has a skill of 200 Engineering
+-- Note: Condition 4018 corresponds to a condition checking if the current patch is 1.10 or higher
+(11002, -1, 11001, 10998, 10997, 4018, 0) -- Condition to return true if the player doesn't have an Engineering specialisation, completed one of the Engineering specialisation quests, and has a skill of 200 Engineering, and the current patch is 1.10 or later
 
 INSERT INTO `npc_text` (`ID`, `BroadcastTextID0`, `Probability0`, `BroadcastTextID1`, `Probability1`, `BroadcastTextID2`, `Probability2`, `BroadcastTextID3`, `Probability3`, `BroadcastTextID4`, `Probability4`, `BroadcastTextID5`, `Probability5`, `BroadcastTextID6`, `Probability6`, `BroadcastTextID7`, `Probability7`) VALUES 
 (21000, 11880, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
 (21001, 11882, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(21001, 11884, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+(21002, 11884, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(21003, 11875, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+
+INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES
+(1469, 21000, 0, 11002), -- Gossip for Nixx Sprocketspring
+(1468, 21001, 0, 11002), -- Gossip for Tinkmaster Overspark
+(1467, 21002, 0, 11002), -- Gossip for Oglethorpe Obnoticus
+(7058, 8321, 0, 0), - Gossip for Book "Soothslaying for Dummies" (Engineering)
 
 -- https://www.wowhead.com/classic/item=11825/pet-bombling#comments:id=2650836
 
