69f12118fc8151fc885dc99f85dfa6bad91c0cf7
Gamemechanic <stesve@gmail.com>
2023-01-24 20:33:01 +0100

    rip-redridgeboss

--- src/game/Handlers/MiscHandler.cpp
+++ src/game/Handlers/MiscHandler.cpp
@@ -313,7 +313,7 @@ void WorldSession::HandleLogoutRequestOpcode(WorldPacket& /*recv_data*/)
 
     uint8 reason = 0;
 
-    if (GetPlayer()->IsInCombat())
+    if (GetPlayer()->IsInCombat() || GetPlayer()->IsFeigningDeath())
         reason = 1;
     else if (GetPlayer()->m_movementInfo.HasMovementFlag(MovementFlags(MOVEFLAG_JUMPING | MOVEFLAG_FALLINGFAR)))
         reason = 3;                      // is jumping or falling
