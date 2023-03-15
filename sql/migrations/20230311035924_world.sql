26849334de0a58960d926b8455e6f38a302c8c11
ratkosrb <ratkomladic2@abv.bg>
2023-03-15 05:51:28 +0200

    Remove Warlock's Doomguard from immune to enslave list.
    Not sure why it was seen being immune to the spell in a sniff.

--- sql/migrations/20230311035924_world.sql
+++ sql/migrations/20230311035924_world.sql
@@ -10,7 +10,7 @@ INSERT INTO `migrations` VALUES ('20230311035924');
 
 
 -- Add enslave immunity to several demons.
-UPDATE `creature_template` SET `mechanic_immune_mask` = (`mechanic_immune_mask` | 1) WHERE `entry` IN (7461, 7462, 7463, 7665, 7666, 7667, 7728, 8318, 8717, 8718, 9516, 10201, 10813, 11490, 11492, 11859, 12236, 12396, 14101, 14354, 15467);
+UPDATE `creature_template` SET `mechanic_immune_mask` = (`mechanic_immune_mask` | 1) WHERE `entry` IN (7461, 7462, 7463, 7665, 7666, 7667, 7728, 8318, 8717, 8718, 9516, 10201, 10813, 11490, 11492, 12236, 12396, 14101, 14354, 15467);
 
 
 -- End of migration.
