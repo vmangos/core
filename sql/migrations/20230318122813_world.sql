eec1d0cd412012342823f208b9ffbf5b4c33c0aa
schell244 <>
2023-03-19 21:03:13 +0100

    correct polly summon position, handle spell 9976 in db

--- sql/migrations/20230318122813_world.sql
+++ sql/migrations/20230318122813_world.sql
@@ -15,25 +15,34 @@ UPDATE `creature_template` SET `ai_name` = 'EventAI' WHERE `entry`=7168;
 -- Polly (pre E.C.A.C)
 DELETE FROM `creature_ai_events` WHERE `creature_id`=7167;
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES 
-(716701, 7167, 0, 1, 0, 100, 1, 73000, 73000, 60000, 60000, 716701, 0, 0, 'Polly - OOC - Cast Stealth'),
-(716702, 7167, 0, 4, 0, 100, 0, 0, 0, 0, 0, 716702, 0, 0, 'Polly - On Aggro - Say Aggro');
+(716701, 7167, 0, 11, 0, 100, 0, 0, 0, 0, 0, 716701, 0, 0, 'Polly - On Just Spawned - Say Spawned'),
+(716702, 7167, 0, 8, 0, 100, 0, 9976, -1, 0, 0, 716702, 0, 0, 'Polly - On Spellhit - Set Home Position'),
+(716703, 7167, 0, 8, 0, 100, 0, 9976, -1, 0, 0, 716703, 0, 0, 'Polly - On Spellhit - Set Faction 35'),
+(716704, 7167, 0, 8, 0, 100, 0, 9976, -1, 0, 0, 716704, 0, 0, 'Polly - On Spellhit - Say Cracker'),
+(716705, 7167, 0, 8, 0, 100, 0, 9976, -1, 0, 0, 716705, 0, 0, 'Polly - On Spellhit - Cast Spell Summon Polly Jr.'),
+(716706, 7167, 0, 8, 0, 100, 0, 9976, -1, 0, 0, 716706, 0, 0, 'Polly - On Spellhit - Despawn');
 
-DELETE FROM `creature_ai_scripts` WHERE `id` IN (716701, 716702);
+DELETE FROM `creature_ai_scripts` WHERE `id` IN (716701, 716702, 716703, 716704, 716705, 716706);
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
-(716701, 0, 0, 15, 8822, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Polly - OOC - Cast Stealth'),
-(716702, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3164, 3170, 0, 0, 0, 0, 0, 0, 0, 'Polly - On Aggro - Say Aggro');
+(716701, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3164, 3170, 0, 0, 0, 0, 0, 0, 0, 'Polly - On Just Spawned - Say Spawned'),
+(716702, 0, 0, 34, 1, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Polly - On Spellhit - Set Home Position'),
+(716703, 0, 0, 22, 35, 1, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Polly - On Spellhit - Set Faction 35'),
+(716704, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3167, 0, 0, 0, 0, 0, 0, 0, 0, 'Polly - On Spellhit - Say Cracker'),
+(716705, 2000, 0, 15, 9998, 2, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Polly - On Spellhit - Cast Spell Summon Polly Jr.'),
+(716706, 2500, 0, 18, 2500, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Polly - On Spellhit - Despawn');
 
 -- Polly (after E.C.A.C)
 DELETE FROM `creature_ai_events` WHERE `creature_id`=7168;
 INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES
-(716801, 7168, 0, 4, 0, 100, 0, 0, 0, 0, 0, 716801, 0, 0, 'Polly - On Aggro - Say Cracker'),
-(716802, 7168, 0, 4, 0, 100, 0, 0, 0, 0, 0, 716802, 0, 0, 'Polly - On Aggro - Say Squawk');
+(716801, 7168, 0, 11, 0, 100, 0, 0, 0, 0, 0, 716801, 0, 0, 'Polly - On Just Spawned - Say Spawned');
 
 
 DELETE FROM `creature_ai_scripts` WHERE `id` IN (716801);
 INSERT INTO `creature_ai_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
-(716801, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3165, 0, 0, 0, 0, 0, 0, 0, 0, 'Polly - On Aggro - Say Cracker'),
-(716802, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3167, 0, 0, 0, 0, 0, 0, 0, 0, 'Polly - On Aggro - Say Squawk');
+(716801, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3165, 0, 0, 0, 0, 0, 0, 0, 0, 'Polly - On Just Spawned - Say Spawned');
+
+-- Script event (object=123462) - Summon 7167
+UPDATE `event_scripts` SET `x`=-1463.40, `y`=-3926.96, `z`=0.24, `o`=4.88 WHERE `id`=2153;
 
 -- End of migration.
 END IF;
