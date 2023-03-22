dd6b58961d5f7dcb8a1ca3521c024451dd282c93
schell244 <Schell244@web.de>
2023-03-22 22:51:09 +0100

    Implement light house sound (hourly bell event) (#1846)

--- src/scripts/world/go_scripts.cpp
+++ src/scripts/world/go_scripts.cpp
@@ -113,7 +113,7 @@ enum BellHourlySoundFX
     BELLTOLLALLIANCE   = 6594, // Stormwind
     BELLTOLLNIGHTELF   = 6674, // Darnassus
     BELLTOLLDWARFGNOME = 7234, // Ironforge
-    LIGHTHOUSEFOGHORN  = 7197  // Lighthouses (TODO: not implememted yet)
+    LIGHTHOUSEFOGHORN  = 7197  // Lighthouses
 };
 
 enum BellHourlySoundZones
@@ -126,14 +126,21 @@ enum BellHourlySoundZones
     IRONFORGE_ZONE           = 1537,
     TELDRASSIL_ZONE          = 141,
     DARNASSUS_ZONE           = 1657,
-    ASHENVALE_ZONE           = 331,
+    ASHENVALE_ZONE           = 331
+};
+
+enum LightHouseAreas
+{
+    AREA_THERAMORE           = 513,
+    AREA_ALCAZ_ISLAND        = 2079,
+    AREA_WESTFALL_LIGHTHOUSE = 115
 };
 
 enum BellHourlyObjects
 {
     // bell gameobjects
     GO_HORDE_BELL = 175885,
-    GO_ALLIANCE_BELL = 176573,
+    GO_ALLIANCE_BELL = 176573 // Also used for lighthouse spawns
 };
 
 enum BellHourlyMisc
@@ -147,8 +154,8 @@ struct go_bells : public GameObjectAI
 {
     go_bells(GameObject* go) : GameObjectAI(go), _soundId(0), once(true)
     {
-        uint32 zoneId = me->GetZoneId();
 
+        uint32 zoneId = me->GetZoneId();
         switch (me->GetEntry())
         {
             case GO_HORDE_BELL:
@@ -169,6 +176,12 @@ struct go_bells : public GameObjectAI
             }
             case GO_ALLIANCE_BELL:
             {
+                if (isLightHouseObject())
+                {
+                    _soundId = LIGHTHOUSEFOGHORN;
+                    return;
+                }
+
                 switch (zoneId)
                 {
                     case IRONFORGE_ZONE:
@@ -189,6 +202,25 @@ struct go_bells : public GameObjectAI
         default:
             sLog.Out(LOG_BASIC, LOG_LVL_ERROR, "go_bells() called with invalid object, ID: %u", me->GetEntry());
         }
+    }
+
+    bool isLightHouseObject()
+    {
+        switch (me->GetAreaId())
+        {
+            case AREA_THERAMORE:
+            {
+                // Area is not precise enough here, since there also is an alliance bell spawn
+                // in the same area. Check if location is close to lighthouse:
+                return me->GetDistance(-3667.0f, -4754.0f, 1.8f) < 1;
+            }
+            case AREA_ALCAZ_ISLAND:
+            case AREA_WESTFALL_LIGHTHOUSE:
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     void UpdateAI(uint32 const diff) override
@@ -215,8 +247,11 @@ struct go_bells : public GameObjectAI
                     uint8 _rings = (timeinfo->tm_hour) % 12;
                     _rings = (_rings == 0) ? 12 : _rings; // 00:00 and 12:00
 
-                    // Dwarf hourly horn should only play a single time, each time the next hour begins.
-                    if (_soundId == BELLTOLLDWARFGNOME)
+                    // Only play once: Dwarf horn and Lighthouse
+                    // On official servers, this sound is played once very two minutes.
+                    // This would require a additional event with a 2 minute timer.
+                    // For now, play the correct sound at least once every hour.
+                    if (_soundId == BELLTOLLDWARFGNOME || _soundId == LIGHTHOUSEFOGHORN)
                     {
                         _rings = 1;
                     }
