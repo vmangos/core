9890c44af8f0e8974638fc5727aa4fe238c412d1
schell244 <>
2023-03-26 14:09:28 +0200

    link already existing spell_list_id

--- sql/migrations/20230324094209_world.sql
+++ sql/migrations/20230324094209_world.sql
@@ -8,17 +8,8 @@ IF v=0 THEN
 INSERT INTO `migrations` VALUES ('20230324094209');
 -- Add your query below.
 
--- Events list for Pyrewood Elder
-DELETE FROM `creature_ai_events` WHERE `creature_id`=1895;
-INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES
-(189501, 1895, 0, 2, 0, 100, 0, 15, 0, 0, 0, 189501, 0, 0, 'Pyrewood Elder - Flee at 15% HP'),
-(189502, 1895, 0, 14, 0, 100, 1, 400, 40, 15300, 22900, 189502, 0, 0, 'Pyrewood Elder - Cast Lesser Heal on Friendly Missing HP');
-
-DELETE FROM `creature_ai_scripts` WHERE `id` IN (189501, 189502);
-INSERT INTO `creature_ai_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
-(189501, 0, 0, 47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Pyrewood Elder - Flee'),
-(189502, 0, 0, 15, 2053, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Pyrewood Elder - Cast Lesser Heal');
-
+-- Entry at creature_spells was already existing, just spell_list_id was not linked
+UPDATE `creature_template` SET `spell_list_id`= 18950 WHERE `entry`=1895;
 
 -- End of migration.
 END IF;
