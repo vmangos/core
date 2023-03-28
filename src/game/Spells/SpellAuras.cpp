4fbd705f42a382333ce6994ab4ad5425b819ad9e
ratkosrb <ratkomladic2@abv.bg>
2023-03-28 13:32:16 +0300

    Fix quiver exploit with wands.

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -5247,8 +5247,12 @@ void Aura::HandleRangedAmmoHaste(bool apply, bool /*Real*/)
 
     // Quivers should not increase attack speed for ranged weapons which do not require any ammo.
     Item* ranged_weapon = GetTarget()->ToPlayer()->GetItemByPos(INVENTORY_SLOT_BAG_0, EQUIPMENT_SLOT_RANGED);
-    if (!ranged_weapon || ranged_weapon->GetProto()->AmmoType == 0)
+    if ((!ranged_weapon || ranged_weapon->GetProto()->AmmoType == 0) && apply)
+    {
+        // Revert m_applied assigned in Aura::ApplyModidier
+        m_applied = !apply;
         return;
+    }
 
     if (apply)
     {
