a3e4cd3960251fcd157d97e36364b40c9e44cd93
ratkosrb <ratkomladic2@abv.bg>
2023-04-21 09:28:17 +0300

    Fix whispers command.

--- src/game/Commands/CharacterCommands.cpp
+++ src/game/Commands/CharacterCommands.cpp
@@ -1240,13 +1240,13 @@ bool ChatHandler::HandleWhispersCommand(char* args)
     // whisper on
     if (value)
     {
-        GetSession()->GetMasterPlayer()->SetAcceptWhispers(true);
+        GetSession()->GetPlayer()->SetAcceptWhispers(true);
         SendSysMessage(LANG_COMMAND_WHISPERON);
     }
     // whisper off
     else
     {
-        GetSession()->GetMasterPlayer()->SetAcceptWhispers(false);
+        GetSession()->GetPlayer()->SetAcceptWhispers(false);
         GetSession()->GetMasterPlayer()->ClearAllowedWhisperers();
         SendSysMessage(LANG_COMMAND_WHISPEROFF);
     }
