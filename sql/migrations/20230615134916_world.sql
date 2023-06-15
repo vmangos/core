5b5a2b6a3a24b025c186da61dc7af809a875311c
MantisLord <MantisLord@users.noreply.github.com>
2023-06-15 17:01:35 -0400

    fix migration thing

--- sql/migrations/20230615134916_world.sql
+++ sql/migrations/20230615134916_world.sql
@@ -3,9 +3,9 @@ delimiter ??
 CREATE PROCEDURE \`add_migration\`()
 BEGIN
 DECLARE v INT DEFAULT 1;
-SET v = (SELECT COUNT(*) FROM \`migrations\` WHERE \`id\`='$DATE');
+SET v = (SELECT COUNT(*) FROM \`migrations\` WHERE \`id\`='20230615134916');
 IF v=0 THEN
-INSERT INTO \`migrations\` VALUES ('$DATE');
+INSERT INTO \`migrations\` VALUES ('20230615134916');
 -- Add your query below.
 
 -- Archmage Tarsis Kir-Moldir 16381
