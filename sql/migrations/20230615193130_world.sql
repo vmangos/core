b9fa77a4b2f4302591dbb3e137468d0242ed0978
NickTyrer <nickyty8@gmail.com>
2023-06-17 06:47:55 +0100

    Correct Migration Header

--- sql/migrations/20230615193130_world.sql
+++ sql/migrations/20230615193130_world.sql
@@ -1,11 +1,12 @@
+SET NAMES utf8;
 DROP PROCEDURE IF EXISTS add_migration;
 delimiter ??
-CREATE PROCEDURE \`add_migration\`()
+CREATE PROCEDURE `add_migration`()
 BEGIN
 DECLARE v INT DEFAULT 1;
-SET v = (SELECT COUNT(*) FROM \`migrations\` WHERE \`id\`='20230615193130');
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20230615193130');
 IF v=0 THEN
-INSERT INTO \`migrations\` VALUES ('20230615193130');
+INSERT INTO `migrations` VALUES ('20230615193130');
 -- Add your query below.
 
 
