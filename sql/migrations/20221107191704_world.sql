0a510b9cdb95e482d2949a99e040ed50712676d1
FlagFlayer <76889547+FlagFlayer@users.noreply.github.com>
2023-07-02 02:48:50 +0300

    Update 20221107191704_world.sql
    update comment

--- sql/migrations/20221107191704_world.sql
+++ sql/migrations/20221107191704_world.sql
@@ -38,7 +38,7 @@ INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3
 (11019, 17, 10660, 0, 0, 0, 0), -- Condition to return true if the player has learnt Tribal Leatherworking
 (11020, 17, 10658, 0, 0, 0, 0), -- Condition to return true if the player has learnt Elemental Leatherworking
 (11021, 17, 10656, 0, 0, 0, 0), -- Condition to return true if the player has learnt Dragonscale Leatherworking
-(11022, -2, 11019, 11020, 11021, 0, 1), -- Condition to return true if the player has learnt Tribal, Elemental, OR Dragonscale Leatherworking
+(11022, -2, 11019, 11020, 11021, 0, 1), -- Condition to return true if the player has NOT learnt Tribal, Elemental, OR Dragonscale Leatherworking
 (11023, -1, 11009, 11018, 11022, 4018, 0), -- Condition to return true if the player doesn't have a Leatherworking specialisation, completed one of the Leatherworking specialisation quests, and has a skill of 225 Leatherworking, and the current patch is 1.10 or later
 (11034, -1, 11023, 11027, 0, 0, 0), -- Condition for Book "Soothsaying for Dummies" Leatherworking gossip
 (11035, -3, 11027, 0, 0, 0, 0), -- Condition if requirements for both Engineering and Leatherworking gossip is met
