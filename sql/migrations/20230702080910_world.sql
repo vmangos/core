c28664c0f3bdde578d5cd6da7ffed009b0b33b10
NickTyrer <nickyty8@gmail.com>
2023-07-02 12:04:33 +0100

    Update 20230702080910_world.sql

--- sql/migrations/20230702080910_world.sql
+++ sql/migrations/20230702080910_world.sql
@@ -48,6 +48,32 @@ INSERT INTO `pool_gameobject` (`guid`, `pool_entry`, `chance`, `description`, `p
 
 UPDATE `gameobject` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 300 WHERE `id`= 19019;
 
+-- Create new pool to hold Box of Assorted Parts spawns in The Barrens.
+INSERT INTO `pool_template` (`entry`, `max_limit`, `description`, `patch_min`, `patch_max`) VALUES
+(73, 3, 'Box of Assorted Partss in The Barrens', 0, 10);
+-- Add existing spawns to pool.
+INSERT INTO `pool_gameobject` (`guid`, `pool_entry`, `chance`, `description`, `patch_min`, `patch_max`) VALUES
+(13303, 73, 0, 'Box of Assorted Parts', 0, 10),
+(13304, 73, 0, 'Box of Assorted Parts', 0, 10);
+
+-- Missing Box of Assorted Parts spawns in The Barrens.
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `state`, `animprogress`, `patch_min`, `patch_max`) VALUES
+(13547, 19020, 1, -4144.57, -2178.27, 50.4043, 4.7473, 0, 0, -0.694658, 0.71934, 3600, 3600, 1, 100, 0, 10), -- Closest existing guid is 13303 at 47.903759 yards.
+(13548, 19020, 1, -4168.89, -2187.92, 50.2294, 0.174532, 0, 0, 0.0871553, 0.996195, 3600, 3600, 1, 100, 0, 10), -- Closest existing guid is 13303 at 55.239307 yards.
+(13549, 19020, 1, -4187.06, -2192.49, 50.1852, 2.72271, 0, 0, 0.978148, 0.207912, 3600, 3600, 1, 100, 0, 10), -- Closest existing guid is 13303 at 68.267975 yards.
+(13550, 19020, 1, -4163.62, -2197.79, 50.1831, 3.26377, 0, 0, -0.998135, 0.0610518, 3600, 3600, 1, 100, 0, 10), -- Closest existing guid is 13303 at 45.194328 yards.
+(13551, 19020, 1, -4071.55, -2166.92, 50.6379, 3.28124, 0, 0, -0.997563, 0.0697661, 3600, 3600, 1, 100, 0, 10), -- Closest existing guid is 13303 at 77.385201 yards.
+(13552, 19020, 1, -4111.56, -2305.06, 125.464, 4.90438, 0, 0, -0.636078, 0.771625, 3600, 3600, 1, 100, 0, 10); -- Closest existing guid is 13304 at 36.490269 yards.
+INSERT INTO `pool_gameobject` (`guid`, `pool_entry`, `chance`, `description`, `patch_min`, `patch_max`) VALUES
+(13547, 73, 0, 'Box of Assorted Parts', 0, 10), -- Box of Assorted Partss in The Barrens
+(13548, 73, 0, 'Box of Assorted Parts', 0, 10), -- Box of Assorted Partss in The Barrens
+(13549, 73, 0, 'Box of Assorted Parts', 0, 10), -- Box of Assorted Partss in The Barrens
+(13550, 73, 0, 'Box of Assorted Parts', 0, 10), -- Box of Assorted Partss in The Barrens
+(13551, 73, 0, 'Box of Assorted Parts', 0, 10), -- Box of Assorted Partss in The Barrens
+(13552, 73, 0, 'Box of Assorted Parts', 0, 10); -- Box of Assorted Partss in The Barrens
+
+UPDATE `gameobject` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 300 WHERE `id`= 19020;
+
 
 -- End of migration.
 END IF;
