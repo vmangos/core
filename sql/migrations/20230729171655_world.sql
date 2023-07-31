627e47effd0fedbf08fd3604f678da3455a5b5f0
NickTyrer <nickyty8@gmail.com>
2023-07-31 17:59:38 +0100

    Update 20230729171655_world.sql

--- sql/migrations/20230729171655_world.sql
+++ sql/migrations/20230729171655_world.sql
@@ -23,6 +23,12 @@ DELETE FROM `pool_gameobject` WHERE `guid` IN (129);
 DELETE FROM `gameobject` WHERE `guid` IN (25981, 25918, 25962, 25972, 55363);
 DELETE FROM `pool_gameobject` WHERE `guid` IN (25981, 25918, 25962, 25972, 55363);
 
+-- Add Missing Wanted: Hogger Sign
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`, `spawn_flags`, `visibility_mod`, `patch_min`, `patch_max`) VALUES
+(502, 156561, 0, -9750.33, 682.773, 27.071, 3.86591, -0.042439, 0.0258846, -0.934402, 0.352734, 180, 180, 100, 1, 0, 0, 0, 10);
+INSERT INTO `gameobject_questrelation` (`id`, `quest`, `patch_min`, `patch_max`) VALUES
+(156561, 176, 0, 10);
+
 
 -- End of migration.
 END IF;
