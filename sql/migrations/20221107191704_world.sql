e9a83e86b7e25de3df3b1edaeb6efcb8ac0376f2
FlagFlayer <76889547+FlagFlayer@users.noreply.github.com>
2023-08-05 03:49:06 +0200

    Update 20221107191704_world.sql
    Add basic respec gossip menus

--- sql/migrations/20221107191704_world.sql
+++ sql/migrations/20221107191704_world.sql
@@ -125,18 +125,20 @@ UPDATE `gossip_menu_option` SET `condition_id` = 11019 WHERE `menu_id` = 3073 AN
 DELETE FROM `conditions` WHERE `condition_entry` = 1357;
 
 INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES 
-(11040, 30, 46, 2999, 0, 0, 0), -- Condition to check if the player has less than 3000 Armorsmithing reputation
-(11041, 30, 289, 2999, 0, 0, 0), -- Condition to check if the player has less than 3000 Weaponsmithing reputation
+(11040, 30, 46, 5999, 0, 0, 0), -- Condition to check if the player has less than 6000 Armorsmithing reputation
+(11041, 30, 289, 5999, 0, 0, 0), -- Condition to check if the player has less than 6000 Weaponsmithing reputation
 -- Note: Condition 178 corresponds to a condition checking if the player is level 40 or higher
 -- Note: Condition 368 corresponds to a condition checking if the player has a Blacksmithing skill of 200
 (1357, -1, 178, 368, 11040, 11041, 0), -- Condition for first specialisation gossip by Bengus Deepforge (Alliance) and Krathok Moltenfist (Horde)
 (11042, 8, 5283, 0, 0, 0, 0), -- Condition to check if the player has completed "The Art of the Armorsmith" (Alliance)
-(11043, 8, 5284, 0, 0, 0, 0), -- Condition to check if the player has completed "The Art of the Armorsmith" (Alliance)
+(11043, 8, 5284, 0, 0, 0, 0), -- Condition to check if the player has completed "The Way of the Weaponsmith" (Alliance)
 (11044, 8, 5301, 0, 0, 0, 0), -- Condition to check if the player has completed "The Art of the Armorsmith" (Horde)
 (11045, 8, 5302, 0, 0, 0, 0), -- Condition to check if the player has completed "The Way of the Weaponsmith" (Horde)
-(11046, -2, 11042, 11043, 11044, 11045, 0), -- Condition to return true if the player has completed any of the above quests
+(11046, -2, 11042, 11044, 0, 0, 0), -- Condition to check if the player has completed "The Art of the Armorsmith" (Both factions)
+(11047, -2, 11043, 11045, 0, 0, 0), -- Condition to check if the player has completed "The Way of the Weaponsmith" (Both factions)
 -- Note: Condition 1356 corresponds to a condition checking if the player has NOT learnt Armorsmith AND has NOT learnt Weaponsmith
-(11047, -1,178, 368, 11046, 0, 0); -- Condition for first respecialisation gossip by Bengus Deepforge (Alliance) and Krathok Moltenfist (Horde)
+(11048, -1, 178, 368, 11046, 0, 0), -- Condition for respecialisation gossip by Bengus Deepforge (Alliance) and Krathok Moltenfist (Horde) for players who were previously armorsmiths
+(11049, -1, 178, 368, 11047, 0, 0); -- Condition for respecialisation gossip by Bengus Deepforge (Alliance) and Krathok Moltenfist (Horde) for players who were previously weaponsmiths
 
 UPDATE `quest_template` SET `RequiredMinRepFaction` = 46, `RequiredMinRepValue` = 6000,  WHERE `entry` = 5283; -- Add Armorsmithing reputation requirement for "The Art of the Armorsmith" (Alliance)
 UPDATE `quest_template` SET `RequiredMinRepFaction` = 46, `RequiredMinRepValue` = 6000,  WHERE `entry` = 5301; -- Add Armorsmithing reputation requirement for "The Art of the Armorsmith" (Horde)
@@ -148,6 +150,16 @@ UPDATE `gossip_scripts` SET `datalong` = 17452, `comment` = 'Cast Reputation - W
 INSERT INTO `gossip_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES 
 (318203, 0, 0, 15, 9790, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1357, 'Cast Artisan Armorsmith'),
 (318204, 0, 0, 15, 9789, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1357, 'Cast Artisan Weaponsmith');
+
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_broadcast_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `box_broadcast_text`, `condition_id`) VALUES 
+(3182, 2, 0, 'Myolor, I was once an armorsmith and wish to retake the hammer once more! Teach me the way of the armorsmith.', 8892, 1, 3, -1, 0, 318203, 0, 0, '', 0, 11048),
+(3182, 3, 0, 'Myolor, I was once a weaponsmith and wish to retake the hammer once more! Teach me the way of the weaponsmith!', 8893, 1, 3, -1, 0, 318204, 0, 0, '', 0, 11049), -- Interesting to note this the only option to have 2 exclamation marks, an odd inconsistency by Blizzard.
+(3182, 4, 0, 'Myolor, I was once an armorsmith and wish to instead learn the way of the weaponsmith. Will you teach me?', 10895, 1, 3, -1, 0, 318204, 0, 0, '', 0, 11048),
+(3182, 5, 0, 'Myolor, I was once a weaponsmith and wish to instead learn the way of the armorsmith. Will you teach me?', 10896, 1, 3, -1, 0, 318203, 0, 0, '', 0, 11049),
+(3187, 2, 0, 'Krathok, I was once an armorsmith and wish to retake the hammer once more! Teach me the way of the armorsmith.', 8894, 1, 3, -1, 0, 318203, 0, 0, '', 0, 11048),
+(3187, 3, 0, 'Krathok, I was once a weaponsmith and wish to retake the hammer once more! Teach me the way of the weaponsmith.', 8895, 1, 3, -1, 0, 318204, 0, 0, '', 0, 11049),
+(3187, 4, 0, 'Krathok, I was once an armorsmith and wish to instead learn the ways of the weaponsmith. Will you train me?', 10897, 1, 3, -1, 0, 318204, 0, 0, '', 0, 11048),
+(3187, 5, 0, 'Krathok, I was once a weaponsmith and wish to instead learn the ways of the armorsmith. Will you train me?', 10898, 1, 3, -1, 0, 318203, 0, 0, '', 0, 11049);
 
 -- End of migration.
 END IF;
