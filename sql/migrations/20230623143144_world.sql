0f5c86e69c2752b6ffd11aa42da6bd46b3399d3c
NickTyrer <nickyty8@gmail.com>
2023-08-13 15:42:25 +0100

    Update 20230623143144_world.sql

--- sql/migrations/20230623143144_world.sql
+++ sql/migrations/20230623143144_world.sql
@@ -903,6 +903,15 @@ INSERT INTO `creature_movement` (`id`, `point`, `position_x`, `position_y`, `pos
 -- Felix Whindlebolt 8416
 UPDATE `creature` SET `wander_distance` = 7, `movement_type` = 1 WHERE `id` = 8416;
 
+-- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Increase Droprate of Samophlange Manual Page
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -20 WHERE `entry`=3283 AND `item`=11148 AND `groupid`=0 AND `patch_min`=0 AND `patch_max`=10;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance` = -20 WHERE `entry`=3286 AND `item`=11148 AND `groupid`=0 AND `patch_min`=0 AND `patch_max`=10;
+
+-- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Correct Quest Complete Text For Dawn's Gambit (credit cmangos)
+UPDATE `quest_template` SET `OfferRewardText` = 'You did it! Vectus is defeated! And Dawn\\'s Gambit... did it work?$B$BHm... maybe my device wasn\'t the success I had hoped, but I\'m glad you were able to handle things anyway. Well done, $N!$B$BThe Argent Dawn, and the good people of Azeroth, are in your debt.' WHERE  `entry` = 4771;
+
 
 -- End of migration.
 END IF;
