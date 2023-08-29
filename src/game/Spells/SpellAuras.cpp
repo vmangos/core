6cb5b37f33ed684c2ccfdb7e85ea6ba268c688d0
vanilla-reforged <vanilla.reforged@outlook.com>
2023-08-29 19:49:46 +0000

    Curse of Doom redesign

--- src/game/Spells/SpellAuras.cpp
+++ src/game/Spells/SpellAuras.cpp
@@ -6064,7 +6064,7 @@ void Aura::PeriodicTick(SpellEntry const* sProto, AuraType auraType, uint32 data
             cleanDamage.resist = resist;
             pCaster->DealDamage(target, pdamage, &cleanDamage, DOT, spellProto->GetSpellSchoolMask(), spellProto, true, nullptr, GetHolder()->IsReflected());
             // Curse of Doom: If the target dies from this damage, there is a chance that a Doomguard will be summoned.
-            if (spellProto->Id == 603 && !target->IsAlive() && !urand(0, 9))
+            if (spellProto->Id == 603 && !target->IsAlive() && !urand(0, 0)) /* Vanilla Reforged - Curse of Doom redesign */
                 pCaster->CastSpell(pCaster, 18662, true);
             break;
         }
