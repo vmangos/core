d147045535330296e9dca216c0a9f1d35681a073
NickTyrer <nickyty8@gmail.com>
2023-09-15 06:59:44 +0100

    Update 20230623143144_world.sql

--- sql/migrations/20230623143144_world.sql
+++ sql/migrations/20230623143144_world.sql
@@ -215,12 +215,6 @@ UPDATE `creature_template` SET `inhabit_type` = 3 WHERE `type` = 7 AND `inhabit_
 UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance` = 100 WHERE `entry` = 938 AND `item` = 737;
 
 -- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- Corrections to The Captain's Chest Patch Progression
-UPDATE `quest_template` SET `CompleteEmote` = 1 WHERE `entry` = 8551 AND `patch`=7;
-DELETE FROM `quest_template` WHERE `entry` = 614 AND `patch` = 7;
-UPDATE `creature_questrelation` SET `patch_max` = 6 WHERE `id` = 2500 AND `quest` = 614;
-
--- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Remove Incorrect Creatures
 DELETE FROM `creature` WHERE `guid` IN (21707, 21708);
 
