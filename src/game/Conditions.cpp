46de366423855b88bb2810b4f46fbdc7a797c869
ratkosrb <ratkomladic2@abv.bg>
2023-10-07 00:32:43 +0300

    Fix local time condition.

--- src/game/Conditions.cpp
+++ src/game/Conditions.cpp
@@ -602,7 +602,10 @@ bool inline ConditionEntry::Evaluate(WorldObject const* target, Map const* map, 
             struct tm* timeinfo;
             timeinfo = localtime(&rawtime);
 
-            return (timeinfo->tm_hour >= m_value1) && (timeinfo->tm_min >= m_value2) && (timeinfo->tm_hour <= m_value3) && (timeinfo->tm_min <= m_value4);
+            return (timeinfo->tm_hour >= m_value1) &&
+                   ((timeinfo->tm_hour > m_value1) || (timeinfo->tm_min >= m_value2)) &&
+                   (timeinfo->tm_hour <= m_value3) &&
+                   ((timeinfo->tm_hour < m_value3) || (timeinfo->tm_min <= m_value4));
         }
         case CONDITION_DISTANCE_TO_POSITION:
         {
