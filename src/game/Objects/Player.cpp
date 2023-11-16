c3aabb4f2e81368ac26bd02ca96234862cb6e8ff
camerondb <cameron@mollybet.com>
2023-11-16 12:36:43 +0000

    add new formula for crafting skill gain chance

--- src/game/Objects/Player.cpp
+++ src/game/Objects/Player.cpp
@@ -5740,6 +5740,13 @@ inline int SkillGainChance(uint32 SkillValue, uint32 GrayLevel, uint32 GreenLeve
     return sWorld.getConfig(CONFIG_UINT32_SKILL_CHANCE_ORANGE) * 10;
 }
 
+inline int CraftingSkillGainChance(uint32 SkillValue, uint32 GrayLevel, uint32 YellowLevel)
+{
+    // Linear decrease in chance as you go from the YellowLevel -> GrayLevel.
+    // To avoid casting to floats and back to uint32 we multiply the numerator by 1000, to get into a range of 0-1000.
+    return G3D::iClamp(((GrayLevel - SkillValue) * 1000) / (GrayLevel - YellowLevel), 0, 1000);
+}
+
 bool Player::UpdateCraftSkill(uint32 spellid)
 {
     sLog.Out(LOG_BASIC, LOG_LVL_DEBUG, "UpdateCraftSkill spellid %d", spellid);
@@ -5754,9 +5761,8 @@ bool Player::UpdateCraftSkill(uint32 spellid)
 
             uint32 craft_skill_gain = sWorld.getConfig(CONFIG_UINT32_SKILL_GAIN_CRAFTING);
 
-            return UpdateSkillPro(_spell_idx->second->skillId, SkillGainChance(SkillValue,
+            return UpdateSkillPro(_spell_idx->second->skillId, CraftingSkillGainChance(SkillValue,
                                   _spell_idx->second->max_value,
-                                  (_spell_idx->second->max_value + _spell_idx->second->min_value) / 2,
                                   _spell_idx->second->min_value),
                                   craft_skill_gain);
         }
