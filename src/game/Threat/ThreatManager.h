5ece974a3b4ef170640e52ffe9dab3760d041f65
ratkosrb <ratkomladic2@abv.bg>
2023-11-18 10:47:11 +0200

    Fix Grace of Earth trinket.

--- src/game/Threat/ThreatManager.h
+++ src/game/Threat/ThreatManager.h
@@ -43,7 +43,7 @@ class SpellEntry;
 class ThreatCalcHelper
 {
     public:
-        static float CalcThreat(Unit* pHatedUnit, Unit* pHatingUnit, float threat, bool crit, SpellSchoolMask schoolMask, SpellEntry const* threatSpell);
+        static float CalcThreat(Unit* pHatedUnit, float threat, bool crit, SpellSchoolMask schoolMask, SpellEntry const* threatSpell);
 };
 
 //==============================================================
