9d7cf5523f7949c52b8b8b90f82de5c2ba0a5d8e
Gamemechanic <stesve@gmail.com>
2023-12-01 22:05:55 +0100

    Very minor refactor of Creature::HasSpell (#2322)

--- src/game/Objects/Creature.cpp
+++ src/game/Objects/Creature.cpp
@@ -3062,11 +3062,10 @@ bool Creature::IsInEvadeMode() const
 
 bool Creature::HasSpell(uint32 spellId) const
 {
-    uint8 i;
-    for (i = 0; i < CREATURE_MAX_SPELLS; ++i)
+    for (uint8 i = 0; i < CREATURE_MAX_SPELLS; ++i)
         if (spellId == m_spells[i])
-            break;
-    return i < CREATURE_MAX_SPELLS;                         // break before end of iteration of known spells
+            return true;
+    return false;
 }
 
 void Creature::LockOutSpells(SpellSchoolMask schoolMask, uint32 duration)
