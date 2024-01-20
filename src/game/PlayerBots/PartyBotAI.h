8a783344e4e7004a327479aad9e7786ac6376255
Ilya <ilf@bk.ru>
2024-01-20 10:05:14 +0300

    Miss

--- src/game/PlayerBots/PartyBotAI.h
+++ src/game/PlayerBots/PartyBotAI.h
@@ -52,6 +52,7 @@ public:
     bool CanTryToCastSpell(Unit const* pTarget, SpellEntry const* pSpellEntry) const final;
     Player* GetPartyLeader() const;
     bool AttackStart(Unit* pVictim);
+    Unit* SelectMarketAttackTarget(Player* pLeader) const;
     Unit* SelectAttackTarget(Player* pLeader) const;
     Unit* SelectPartyAttackTarget() const;
     Player* SelectResurrectionTarget() const;
