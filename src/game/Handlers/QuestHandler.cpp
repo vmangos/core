ff976f95d4da6e630f28d456d50e7af442a74496
Ilya <ilf@bk.ru>
2024-02-29 13:49:37 +0300

    hardcore no mount quest

--- src/game/Handlers/QuestHandler.cpp
+++ src/game/Handlers/QuestHandler.cpp
@@ -146,6 +146,12 @@ void WorldSession::HandleQuestgiverAcceptQuestOpcode(WorldPacket& recv_data)
             return;
         }
 
+        // hardcore no mount quest
+        if (qInfo->GetQuestId() == 10123)
+        {
+            return;
+        }
+
         if (Player* pPlayer = ObjectAccessor::FindPlayer(_player->GetDividerGuid()))
         {
             pPlayer->SendPushToPartyResponse(_player, QUEST_PARTY_MSG_ACCEPT_QUEST);
