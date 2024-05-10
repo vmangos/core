33f4231e62b26529bd85dd05d920afbb35e16ec9
Daribon <Daribon@users.noreply.github.com>
2024-05-10 21:48:54 +0200

    Update 20240504222324_world.sql

--- sql/migrations/20240504222324_world.sql
+++ sql/migrations/20240504222324_world.sql
@@ -10,8 +10,7 @@ INSERT INTO `migrations` VALUES ('20240504222324');
 
 
 -- Fix Detect Magic to have one debuff per caster
-INSERT INTO `spell_mod` (`Id`, `procChance`, `procFlags`, `procCharges`, `DurationIndex`, `Category`, `CastingTimeIndex`, `StackAmount`, `SpellIconID`, `activeIconID`, `manaCost`, `Attributes`, `AttributesEx`, `AttributesEx2`, `AttributesEx3`, `AttributesEx4`, `Custom`, `InterruptFlags`, `AuraInterruptFlags`, `ChannelInterruptFlags`, `Dispel`, `Stances`, `StancesNot`, `SpellVisual`, `ManaCostPercentage`, `StartRecoveryCategory`, `StartRecoveryTime`, `MaxAffectedTargets`, `MaxTargetLevel`, `DmgClass`, `rangeIndex`, `RecoveryTime`, `CategoryRecoveryTime`, `SpellFamilyName`, `SpellFamilyFlags`, `Mechanic`, `EquippedItemClass`, `Comment`) VALUES 
-(2855, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 4096, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, -1, -1, 'Detect Magic: one debuff per caster');
+UPDATE `spell_template` SET `customFlags`=4096 WHERE `entry`=2855;
 
 
 -- End of migration.
