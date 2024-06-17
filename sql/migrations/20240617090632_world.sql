6399b4af6070d86a5bba741ebd4a80d49c735161
ratkosrb <ratkomladic2@abv.bg>
2024-06-17 12:08:00 +0300

    Fix minimum build in last commit.

--- sql/migrations/20240617090632_world.sql
+++ sql/migrations/20240617090632_world.sql
@@ -10,7 +10,7 @@ INSERT INTO `migrations` VALUES ('20240617090632');
 
 
 -- Fix Widow's Embrace not hitting Grand Widow Faerlina.
-INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`, `conditionId`, `inverseEffectMask`, `build_min`, `build_max`) VALUES (28732, 1, 15953, 0, 0, 0, 5875);
+INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`, `conditionId`, `inverseEffectMask`, `build_min`, `build_max`) VALUES (28732, 1, 15953, 0, 0, 5464, 5875);
 UPDATE `creature_template` SET `immunity_flags`=2 WHERE `entry`=15953;
 
 
