8819ab130e84900538888185f29a2d8164863101
ratkosrb <ratkomladic2@abv.bg>
2024-06-17 13:02:16 +0300

    Assign patch to loot from last commit.

--- sql/migrations/20240617091747_world.sql
+++ sql/migrations/20240617091747_world.sql
@@ -107,17 +107,17 @@ UPDATE `creature_template` SET `skinning_loot_id`=10683 WHERE `entry`=10683;
 
 -- Add skinning loot to Frostwolf.
 -- https://www.wowhead.com/classic/npc=10981/frostwolf#skinning
-INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (10981, 4304, 72, 1, 1, 1, 0, 0, 10);
-INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (10981, 8170, 20, 1, 1, 1, 0, 0, 10);
-INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (10981, 8169, 5, 1, 1, 1, 0, 0, 10);
-INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (10981, 8368, 3, 1, 1, 1, 0, 0, 10);
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (10981, 4304, 72, 1, 1, 1, 0, 3, 10);
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (10981, 8170, 20, 1, 1, 1, 0, 3, 10);
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (10981, 8169, 5, 1, 1, 1, 0, 3, 10);
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (10981, 8368, 3, 1, 1, 1, 0, 3, 10);
 UPDATE `creature_template` SET `skinning_loot_id`=10981 WHERE `entry`=10981;
 
 -- Add skinning loot to Son of Hakkar.
 -- https://www.wowhead.com/classic/npc=11357/son-of-hakkar#skinning
-INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (11357, 8170, 80, 1, 1, 2, 0, 0, 10);
-INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (11357, 4304, 15, 1, 1, 2, 0, 0, 10);
-INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (11357, 8171, 5, 1, 1, 1, 0, 0, 10);
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (11357, 8170, 80, 1, 1, 2, 0, 5, 10);
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (11357, 4304, 15, 1, 1, 2, 0, 5, 10);
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (11357, 8171, 5, 1, 1, 1, 0, 5, 10);
 UPDATE `creature_template` SET `skinning_loot_id`=11357 WHERE `entry`=11357;
 
 -- Add skinning loot to Nefarian.
@@ -135,11 +135,11 @@ UPDATE `creature_template` SET `skinning_loot_id`=12496 WHERE `entry`=12496;
 
 -- Add skinning loot to Frostwolf Bloodhound.
 -- https://www.wowhead.com/classic/npc=14282/frostwolf-bloodhound#skinning
-INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (14282, 8170, 50, 1, 1, 1, 0, 0, 10);
-INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (14282, 4304, 40, 1, 1, 1, 0, 0, 10);
-INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (14282, 8368, 5, 1, 1, 1, 0, 0, 10);
-INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (14282, 8171, 3, 1, 1, 1, 0, 0, 10);
-INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (14282, 8169, 2, 1, 1, 1, 0, 0, 10);
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (14282, 8170, 50, 1, 1, 1, 0, 3, 10);
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (14282, 4304, 40, 1, 1, 1, 0, 3, 10);
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (14282, 8368, 5, 1, 1, 1, 0, 3, 10);
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (14282, 8171, 3, 1, 1, 1, 0, 3, 10);
+INSERT INTO `skinning_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `patch_min`, `patch_max`) VALUES (14282, 8169, 2, 1, 1, 1, 0, 3, 10);
 UPDATE `creature_template` SET `skinning_loot_id`=14282 WHERE `entry`=14282;
 
 
