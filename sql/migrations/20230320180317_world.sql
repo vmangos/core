6ea53deb2d6385ec651b8c93568f9a73e0b9ba5b
ratkosrb <35845488+ratkosrb@users.noreply.github.com>
2024-06-22 01:12:24 +0300

    Update 20230320180317_world.sql

--- sql/migrations/20230320180317_world.sql
+++ sql/migrations/20230320180317_world.sql
@@ -8,11 +8,14 @@ IF v=0 THEN
 INSERT INTO `migrations` VALUES ('20230320180317');
 -- Add your query below.
 
-UPDATE `quest_template` SET `CompleteScript` = 1719 WHERE `entry` = 1719;
 
+-- Add missing completion script for quest The Affray.
 DELETE FROM `quest_end_scripts` WHERE `id`=1719;
 INSERT INTO `quest_end_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
-(1719, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 2354, 0, 0, 0, 0, 0, 0, 0, 0, 'Klannoc Macleod - Zone Yell - Champion of The Affray');
+(1719, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2354, 0, 0, 0, 0, 0, 0, 0, 0, 'The Affray: Klannoc Macleod - Yell Text');
+UPDATE `quest_template` SET `CompleteScript` = 1719 WHERE `entry` = 1719;
+UPDATE `broadcast_text` SET `chat_type`=1, `emote_id1`=22 WHERE `entry`=2354;
+
 
 -- End of migration.
 END IF;
