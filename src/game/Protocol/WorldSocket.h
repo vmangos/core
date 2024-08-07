1d6b03dc54efce60ef8ca2e5fd17e7835680d9d9
0blu <dot0blu@gmail.com>
2024-08-07 19:20:04 +0200

    NativeIO: Remove unused parts of the code

--- src/game/Protocol/WorldSocket.h
+++ src/game/Protocol/WorldSocket.h
@@ -96,11 +96,6 @@ private:
     std::queue<WorldPacket> m_sendQueue;
     std::atomic_flag m_sendQueueIsRunning;
 
-#ifdef _DEBUG
-    //std::deque<uint32> m_opcodeHistoryOut{};
-    //std::deque<uint32> m_opcodeHistoryInc{};
-#endif
-
 public:
     explicit WorldSocket(IO::IoContext* ctx, IO::Networking::SocketDescriptor const& socketDescriptor);
     ~WorldSocket();
@@ -114,9 +109,6 @@ public:
     void SendPacket(WorldPacket packet);
 
     void FinalizeSession() { m_Session = nullptr; }
-
-    //static std::vector<uint32> m_packetCooldowns;
-    //std::map<uint32, TimePoint> m_lastPacket;
 };
 
 #endif // MANGOS_GAME_SERVER_WORLDSOCKET_H
