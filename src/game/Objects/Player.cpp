b096056031a2843a4f1f5a539780028c8dc78020
FlagFlayer <76889547+FlagFlayer@users.noreply.github.com>
2024-09-13 15:25:10 +0300

    Update Player.cpp
    profession specialisations no longer in spell chain, so they do not have a spell rank larger than 1

--- src/game/Objects/Player.cpp
+++ src/game/Objects/Player.cpp
@@ -19762,7 +19762,7 @@ void Player::LearnQuestRewardedSpells(Quest const* quest)
 
     // Prevent learning profession specializations, because unlearning and re-learning a profession doesn't automatically re-add the specialization
     uint32 learned_0 = spellInfo->EffectTriggerSpell[EFFECT_INDEX_0];
-    if (sSpellMgr.GetSpellRank(learned_0) > 1)
+    if (sSpellMgr.IsPrimaryProfessionSpell(learned_0))
         return;
 
     CastSpell(this, spellId, true);
