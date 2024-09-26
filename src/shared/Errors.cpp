21068888e7f47f386506af8cec564e282f7dacff
0blu <dot0blu@gmail.com>
2024-09-26 02:25:03 +0200

    `PrintStacktrace` show error if no debug symbols

--- src/shared/Errors.cpp
+++ src/shared/Errors.cpp
@@ -15,17 +15,48 @@ void MaNGOS::Errors::PrintStacktrace(int skipFrames, int maxFrames)
         std::size_t(maxFrames)
     );
 
+    bool hasStacktraceInfo = false;
     for (size_t i = 0; i < st.frames.size(); ++i)
     {
         cpptrace::stacktrace_frame const& trace = st.frames[i];
-        sLog.Out(LOG_BASIC, LOG_LVL_MINIMAL,
-            "#%u [0x%" PRIXPTR "] %s %s:%u",
-            i,
-            trace.object_address,
-            trace.symbol.c_str(),
-            trace.filename.c_str(),
-            trace.line
-        );
+        if (trace.line.has_value())
+        {
+            sLog.Out(LOG_BASIC, LOG_LVL_MINIMAL,
+                "#%zu [0x%" PRIXPTR "] %s %s:%u",
+                i,
+                trace.object_address,
+                trace.symbol.c_str(),
+                trace.filename.c_str(),
+                trace.line.value()
+            );
+        }
+        else
+        {
+            // without line
+            sLog.Out(LOG_BASIC, LOG_LVL_MINIMAL,
+                "#%zu [0x%" PRIXPTR "] %s %s",
+                i,
+                trace.object_address,
+                trace.symbol.c_str(),
+                trace.filename.c_str()
+            );
+        }
+
+        if (!hasStacktraceInfo && trace.line.has_value() && !trace.symbol.empty())
+        {
+            // we assume there are symbols if at least one frame was parsed successfully
+            hasStacktraceInfo = true;
+        }
+    }
+
+    if (!hasStacktraceInfo)
+    {
+        // without line
+#if PLATFORM == PLATFORM_WINDOWS
+        sLog.Out(LOG_BASIC, LOG_LVL_ERROR, "Missing debug symbols. Place an up-to-date PDB file next to executable and/or build with debug symbols.");
+#else
+        sLog.Out(LOG_BASIC, LOG_LVL_ERROR, "Missing debug symbols. Please build with debug symbols.");
+#endif
     }
 }
 
@@ -40,9 +71,8 @@ void MaNGOS::Errors::PrintStacktraceAndThrow(char const* filename, int line, cha
     MaNGOS::Errors::PrintStacktrace(1, 32);
 
     std::string completeMessage = failedExpression;
-    if (message) {
+    if (message)
         completeMessage += std::string(" Message: ") + message;
-    }
 
     throw std::runtime_error(completeMessage);
 
