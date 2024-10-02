0de6df137f85d8e1a3ece1b25a9fdbc36ae3dcc7
0blu <dot0blu@gmail.com>
2024-10-02 22:39:42 +0200

    Fix asserts in `BarGoLink`

--- src/shared/ProgressBar.cpp
+++ src/shared/ProgressBar.cpp
@@ -41,20 +41,21 @@ BarGoLink::BarGoLink(int row_count)
 
 BarGoLink::BarGoLink(uint32 row_count)
 {
-    init((int)row_count);
+    MANGOS_ASSERT(row_count < std::numeric_limits<int32>::max());
+    init(static_cast<int>(row_count));
 }
 
 BarGoLink::BarGoLink(uint64 row_count)
 {
-    MANGOS_ASSERT(row_count < std::numeric_limits<uint32>::max());
-    init((int)row_count);
+    MANGOS_ASSERT(row_count < std::numeric_limits<int32>::max());
+    init(static_cast<int>(row_count));
 }
 
 #ifdef __APPLE__
 BarGoLink::BarGoLink(size_t row_count)
 {
-    //MANGOS_ASSERT(row_count < (uint64)ACE_INT32_MAX);
-    init((int)row_count);
+    MANGOS_ASSERT(row_count < std::numeric_limits<int32>::max());
+    init(static_cast<int>(row_count));
 }
 #endif
 
--- src/shared/ProgressBar.h
+++ src/shared/ProgressBar.h
@@ -28,10 +28,10 @@ class BarGoLink
 {
     public:                                                 // constructors
         explicit BarGoLink(int row_count);
-        explicit BarGoLink(uint32 row_count);               // row_count < ACE_INT32_MAX
-        explicit BarGoLink(uint64 row_count);               // row_count < ACE_INT32_MAX
+        explicit BarGoLink(uint32 row_count);               // row_count < int32::max
+        explicit BarGoLink(uint64 row_count);               // row_count < int32::max
 #ifdef __APPLE__
-        explicit BarGoLink(size_t row_count);
+        explicit BarGoLink(size_t row_count);               // row_count < int32::max
 #endif
         ~BarGoLink();
 
