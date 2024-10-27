832d40e3c8953dbcfaaff142ca6a80681fec921d
Gamemechanic <stesve@gmail.com>
2024-10-27 02:39:47 +0200

    set static_flags1 according to sniffs

--- sql/migrations/20241026225509_world.sql
+++ sql/migrations/20241026225509_world.sql
@@ -12,17 +12,17 @@ INSERT INTO `migrations` VALUES ('20241026225509');
 -- Un'Goro - some gorrilas is not tameable prior to patch 1.9
 
 -- Un'Goro Thunderer
-UPDATE `creature_template` SET `pet_family`=0 WHERE  `entry`=6516 AND `patch`=0;
+UPDATE `creature_template` SET `pet_family`=0, `static_flags1`=0 WHERE  `entry`=6516 AND `patch`=0;
 
 -- Uhk'loc
-UPDATE `creature_template` SET `pet_family`=0 WHERE  `entry`=6585 AND `patch`=0;
+UPDATE `creature_template` SET `pet_family`=0, `static_flags1`=0 WHERE  `entry`=6585 AND `patch`=0;
 
 -- U'cha
-UPDATE `creature_template` SET `pet_family`=0 WHERE  `entry`=9622 AND `patch`=0;
+UPDATE `creature_template` SET `pet_family`=0, `static_flags1`=0 WHERE  `entry`=9622 AND `patch`=0;
 
 -- Un'Goro Stomper
 INSERT INTO `creature_template` (`entry`, `patch`, `name`, `level_min`, `level_max`, `faction`, `display_id1`, `type`, `pet_family`, `unit_class`, `health_multiplier`, `damage_multiplier`, `damage_variance`, `loot_id`, `skinning_loot_id`, `ai_name`, `movement_type`, `inhabit_type`, `static_flags1`) VALUES (6513, 7, 'Un\'Goro Stomper', 51, 52, 72, 5294, 1, 9, 1, 1.3, 1.5, 0.09, 6513, 6513, 'EventAI', 1, 1, 16);
-UPDATE `creature_template` SET `pet_family`=0 WHERE  `entry`=6513 AND `patch`=0;
+UPDATE `creature_template` SET `pet_family`=0, `static_flags1`=0 WHERE  `entry`=6513 AND `patch`=0;
 
 
 -- Zulgurub Speakers where elites prior to patch 1.11
