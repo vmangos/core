2bdecb63c585680f217668bcb3bc688fb0432655
Stoabrogga <stoabrogga@proton.me>
2024-12-25 00:59:10 +0100

    Fix TerrainInfo::GetWaterOrGroundLevel
    Fix returning z values which are below ground level (can happen in shallow water).

--- src/game/Maps/GridMap.cpp
+++ src/game/Maps/GridMap.cpp
@@ -1122,7 +1122,8 @@ float TerrainInfo::GetWaterOrGroundLevel(float x, float y, float z, float* pGrou
         GridMapLiquidData liquid_status;
 
         GridMapLiquidStatus res = getLiquidStatus(x, y, ground_z, MAP_ALL_LIQUIDS, &liquid_status);
-        return res ? (swim ? liquid_status.level - 2.0f : liquid_status.level) : ground_z;
+        float max_z = res ? (swim ? liquid_status.level - 2.0f : liquid_status.level) : ground_z;
+        return max_z > ground_z ? max_z : ground_z;
     }
 
     return VMAP_INVALID_HEIGHT_VALUE;
