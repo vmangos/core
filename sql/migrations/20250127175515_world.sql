3836100a72a630b5f45b3709fafd3079c6ad9f4f
NickTyrer <nickyty8@gmail.com>
2025-02-02 11:05:03 +0000

    Update 20250127175515_world.sql

--- sql/migrations/20250127175515_world.sql
+++ sql/migrations/20250127175515_world.sql
@@ -12,7 +12,7 @@ INSERT INTO `migrations` VALUES ('20250127175515');
 SET @PTEMPLATE = 8101;
 -- Dustwallow Marsh
 -- Darkmist Cavern
-UPDATE `gameobject` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 900 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (6047, 6051, 6058, 6020, 6052, 6053, 6048, 6046, 6006, 6007, 6001));
+UPDATE `gameobject` SET `spawntimesecsmin` = 600, `spawntimesecsmax` = 2700 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (6047, 6051, 6058, 6020, 6052, 6053, 6048, 6046, 6006, 6007, 6001));
 DELETE FROM `pool_pool` WHERE `pool_id` IN (6047, 6051, 6058, 6020, 6052, 6053, 6048, 6046, 6006, 6007, 6001);
 UPDATE `pool_template` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Dustwallow Marsh - Darkmist Cavern' WHERE `entry` IN (6020);
 UPDATE `pool_gameobject` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Dustwallow Marsh - Darkmist Cavern' WHERE `pool_entry` IN (6020);
@@ -36,7 +36,7 @@ INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `fla
 (6052, @PTEMPLATE+1, 0, 'Dustwallow Marsh - Darkmist Cavern - Silver Vein / Gold Vein / Iron Deposit / Mithril Deposit / Truesilver Deposit', 0);
 
 -- Bloodfen Burrow
-UPDATE `gameobject` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 900 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (6030, 6011, 6034));
+UPDATE `gameobject` SET `spawntimesecsmin` = 600, `spawntimesecsmax` = 2700 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (6030, 6011, 6034));
 DELETE FROM `pool_pool` WHERE `pool_id` IN (6030, 6011, 6034);
 UPDATE `pool_template` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Dustwallow Marsh - Bloodfen Burrow' WHERE `entry` IN (6030, 6011, 6034);
 UPDATE `pool_gameobject` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Dustwallow Marsh - Bloodfen Burrow' WHERE `pool_entry` IN (6030, 6011, 6034);
@@ -48,7 +48,7 @@ INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `fla
 (6034, @PTEMPLATE+2, 0, 'Dustwallow Marsh - Bloodfen Burrow - Silver Vein / Gold Vein / Iron Deposit', 0);
 
 -- Den of Flame
-UPDATE `gameobject` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 900 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (6004, 6008, 6009, 6010, 6002, 6056, 6054, 6049, 6050, 6055));
+UPDATE `gameobject` SET `spawntimesecsmin` = 600, `spawntimesecsmax` = 2700 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (6004, 6008, 6009, 6010, 6002, 6056, 6054, 6049, 6050, 6055));
 DELETE FROM `pool_pool` WHERE `pool_id` IN (6004, 6008, 6009, 6010, 6002, 6056, 6054, 6049, 6050, 6055);
 UPDATE `pool_template` SET `description` = 'Gold Vein / Mithril Deposit / Truesilver Deposit - Dustwallow Marsh - Den of Flame' WHERE `entry` IN (6004, 6008, 6009, 6010, 6002);
 UPDATE `pool_gameobject` SET `description` = 'Gold Vein / Mithril Deposit / Truesilver Deposit - Dustwallow Marsh - Den of Flame' WHERE `pool_entry` IN (6004, 6008, 6009, 6010, 6002);
@@ -73,7 +73,7 @@ UPDATE `pool_template` SET `max_limit` = 5 WHERE `entry`=9904;
 
 -- Alterac Mountains
 -- Growless Cave
-UPDATE `gameobject` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 900 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (5037, 5073, 5050, 5074, 5069, 5022));
+UPDATE `gameobject` SET `spawntimesecsmin` = 600, `spawntimesecsmax` = 2700 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (5037, 5073, 5050, 5074, 5069, 5022));
 DELETE FROM `pool_pool` WHERE `pool_id` IN (5037, 5073, 5050, 5074, 5069, 5022);
 UPDATE `pool_template` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Alterac Mountains - Growless Cave' WHERE `entry` IN (5037, 5073, 5050, 5074, 5069, 5022);
 UPDATE `pool_gameobject` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Alterac Mountains - Growless Cave' WHERE `pool_entry` IN (5037, 5073, 5050, 5074, 5069, 5022);
@@ -88,7 +88,7 @@ INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `fla
 (5022, @PTEMPLATE+4, 0, 'Alterac Mountains - Growless Cave - Silver Vein / Gold Vein / Iron Deposit', 0);
 
 -- Slaughter Hollow
-UPDATE `gameobject` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 900 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (5060, 5047, 5061, 5049, 5013, 5009));
+UPDATE `gameobject` SET `spawntimesecsmin` = 600, `spawntimesecsmax` = 2700 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (5060, 5047, 5061, 5049, 5013, 5009));
 DELETE FROM `pool_pool` WHERE `pool_id` IN (5060, 5047, 5061, 5049, 5013, 5009);
 UPDATE `pool_template` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Alterac Mountains - Slaughter Hollow' WHERE `entry` IN (5060, 5047, 5061, 5049);
 UPDATE `pool_gameobject` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Alterac Mountains - Slaughter Hollow' WHERE `pool_entry` IN (5060, 5047, 5061, 5049);
@@ -105,7 +105,7 @@ INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `fla
 (5009, @PTEMPLATE+5, 0, 'Alterac Mountains - Slaughter Hollow - Gold Vein / Mithril Deposit / Truesilver Deposit', 0);
 
 -- Crushridge Hold
-UPDATE `gameobject` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 900 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (5023, 5043, 5084, 5035, 5083, 5082, 5029, 5081, 5034, 5054, 5028, 5044, 5021, 5004, 5010, 5015, 5014, 5012, 5007, 5019, 5017));
+UPDATE `gameobject` SET `spawntimesecsmin` = 600, `spawntimesecsmax` = 2700 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (5023, 5043, 5084, 5035, 5083, 5082, 5029, 5081, 5034, 5054, 5028, 5044, 5021, 5004, 5010, 5015, 5014, 5012, 5007, 5019, 5017));
 DELETE FROM `pool_pool` WHERE `pool_id` IN (5023, 5043, 5084, 5035, 5083, 5082, 5029, 5081, 5034, 5054, 5028, 5044, 5021, 5004, 5010, 5015, 5014, 5012, 5007, 5019, 5017);
 UPDATE `pool_template` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Alterac Mountains - Crushridge Hold' WHERE `entry` IN (5023, 5043, 5084, 5035, 5083, 5082, 5029, 5081, 5034, 5054, 5028, 5044);
 UPDATE `pool_gameobject` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Alterac Mountains - Crushridge Hold' WHERE `pool_entry` IN (5023, 5043, 5084, 5035, 5083, 5082, 5029, 5081, 5034, 5054, 5028, 5044);
@@ -145,7 +145,7 @@ UPDATE `pool_gameobject` SET `chance` = 95 WHERE `description` = "Tin Vein / Sil
 UPDATE `pool_gameobject` SET `chance` = 5 WHERE `description` = "Tin Vein / Silver Vein - Badlands" && `guid` IN (SELECT `guid` FROM `gameobject` WHERE `id` = 1733);
 
 -- Excavation Center
-UPDATE `gameobject` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 900 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (1755, 1754, 1761, 1762, 1758, 1757, 1759, 5555, 1760, 5556, 5553, 5554));
+UPDATE `gameobject` SET `spawntimesecsmin` = 600, `spawntimesecsmax` = 2700 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (1755, 1754, 1761, 1762, 1758, 1757, 1759, 5555, 1760, 5556, 5553, 5554));
 DELETE FROM `pool_pool` WHERE `pool_id` IN (1755, 1754, 1761, 1762, 1758, 1757, 1759, 5555, 1760, 5556, 5553, 5554);
 UPDATE `pool_template` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Badlands - Excavation Center' WHERE `entry` IN (1755, 1754, 1761, 1762, 1758, 1757, 1759);
 UPDATE `pool_gameobject` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Badlands - Excavation Center' WHERE `pool_entry` IN (1755, 1754, 1761, 1762, 1758, 1757, 1759);
@@ -168,7 +168,7 @@ INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `fla
 (5554, @PTEMPLATE+7, 0, 'Badlands - Excavation Center - Gold Vein / Mithril Deposit / Truesilver Deposit', 0);
 
 -- Dustbelch Grotto
-UPDATE `gameobject` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 900 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (1708, 1706, 1705, 1707, 5508, 5507, 5502, 5501, 5506, 5509, 5503, 5511));
+UPDATE `gameobject` SET `spawntimesecsmin` = 600, `spawntimesecsmax` = 2700 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (1708, 1706, 1705, 1707, 5508, 5507, 5502, 5501, 5506, 5509, 5503, 5511));
 DELETE FROM `pool_pool` WHERE `pool_id` IN (1708, 1706, 1705, 1707, 5508, 5507, 5502, 5501, 5506, 5509, 5503, 5511);
 UPDATE `pool_template` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Badlands - Dustbelch Grotto' WHERE `entry` IN (1708, 1706, 1705, 1707);
 UPDATE `pool_gameobject` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Badlands - Dustbelch Grotto' WHERE `pool_entry` IN (1708, 1706, 1705, 1707);
@@ -191,7 +191,7 @@ INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `fla
 (5511, @PTEMPLATE+8, 0, 'Badlands - Dustbelch Grotto - Gold Vein / Mithril Deposit / Truesilver Deposit', 0);
 
 -- Misc Cave
-UPDATE `gameobject` SET `spawntimesecsmin` = 300, `spawntimesecsmax` = 900 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (1747, 1748, 1749));
+UPDATE `gameobject` SET `spawntimesecsmin` = 600, `spawntimesecsmax` = 2700 WHERE `guid` IN (SELECT `guid` FROM `pool_gameobject` WHERE `pool_entry` IN (1747, 1748, 1749));
 DELETE FROM `pool_pool` WHERE `pool_id` IN (1747, 1748, 1749);
 UPDATE `pool_template` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Badlands - Misc Cave' WHERE `entry` IN (1747, 1748, 1749);
 UPDATE `pool_gameobject` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Badlands - Misc Cave' WHERE `pool_entry` IN (1747, 1748, 1749);
