1eed507fe1cea375ee8b07524ece6cfa30b3c837
FlagFlayer <76889547+FlagFlayer@users.noreply.github.com>
2025-03-08 00:48:29 +0200

    Update 20250307204042_world.sql

--- sql/migrations/20250307204042_world.sql
+++ sql/migrations/20250307204042_world.sql
@@ -8,7 +8,7 @@ IF v = 0 THEN
 INSERT INTO `migrations` VALUES ('20250307204042');
 -- Add your query below.
 
--- Update spawnpoints (credit cmangos)
+-- Prospector Khazgorm (credit cmangos)
 UPDATE `creature` SET `position_x` = -4181.31005859375, `position_y` = -2184.330078125, `position_z` = 50.26649856567383, `orientation` = 0.2923249900341034 WHERE `guid` = 13984 AND `id` = 3392;
 UPDATE `creature` SET `position_x` = -4134.08984375, `position_y` = -2190.56005859375, `position_z` = 50.93259811401367, `orientation` = 5.971350193023682 WHERE `guid` = 14326 AND `id` = 3392;
 UPDATE `creature` SET `position_x` = -4208.52001953125, `position_y` = -2274.22998046875, `position_z` = 50.26689910888672, `orientation` = 3.9258201122283936 WHERE `guid` = 14325 AND `id` = 3392;
