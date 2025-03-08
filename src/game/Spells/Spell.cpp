1eca791e2608daa91455306b322bcd91668c5e91
FlagFlayer <76889547+FlagFlayer@users.noreply.github.com>
2025-03-08 06:27:49 +0200

    Update Spell.cpp

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -7887,7 +7887,8 @@ SpellCastResult Spell::CheckItems()
                 if (!targetItem)
                     return SPELL_FAILED_ITEM_GONE;
 
-                if (targetItem->GetProto()->ItemLevel < m_spellInfo->baseLevel)
+                // Special case for Heavy Armor Kit enchant since baseLevel is 0 in spell_template
+                if (targetItem->GetProto()->ItemLevel < m_spellInfo->baseLevel || (m_spellInfo->Id = 2833 && targetItem->GetProto()->ItemLevel < 15 && sWorld.GetWowPatch() >= WOW_PATCH_107))
                     return SPELL_FAILED_LOWLEVEL;
 
                 // Not allow enchant in trade slot for some enchant type
