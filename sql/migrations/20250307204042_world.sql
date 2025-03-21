62b64a446e2615fcd053fb254550f3d502b51492
FlagFlayer <76889547+FlagFlayer@users.noreply.github.com>
2025-03-21 22:10:13 +0200

    Update 20250307204042_world.sql

--- sql/migrations/20250307204042_world.sql
+++ sql/migrations/20250307204042_world.sql
@@ -14,6 +14,12 @@ UPDATE `creature` SET `position_x` = -4134.08984375, `position_y` = -2190.560058
 UPDATE `creature` SET `position_x` = -4208.52001953125, `position_y` = -2274.22998046875, `position_z` = 50.26689910888672, `orientation` = 3.9258201122283936 WHERE `guid` = 14325 AND `id` = 3392;
 UPDATE `creature` SET `position_x` = -4176.35009765625, `position_y` = -2235.199951171875, `position_z` = 51.03120040893555, `orientation` = 5.194900035858154 WHERE `guid` = 14324 AND `id` = 3392;
 
+INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES 
+(4130,-1,4105,4018,0,0,0); -- Player is Tauren and Patch is 1.10 or later
+
+UPDATE `gossip_menu` SET `condition_id` = 4130, `text_id` = 8370 WHERE `entry` = 4015 AND `text_id` = 8368; -- Velma Warnam
+UPDATE `gossip_menu` SET `condition_id` = 4130 WHERE `entry` = 4022 AND `text_id` = 8407; -- Xar'Ti
+
 -- End of migration.
 END IF;
 END??
