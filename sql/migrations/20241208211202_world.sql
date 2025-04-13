4bd2a48d972911123f9c895dc13fa380539343a1
NickTyrer <nickyty8@gmail.com>
2025-04-13 21:30:50 +0100

    Update 20241208211202_world.sql

--- sql/migrations/20241208211202_world.sql
+++ sql/migrations/20241208211202_world.sql
@@ -415,7 +415,7 @@ DELETE FROM `gameobject` WHERE `guid` = 21;
 
 -- Remove Incorrect Pre-Quests
 UPDATE `quest_template` SET `PrevQuestId` = 0 WHERE `entry` IN (8846, 8847, 8848, 8849, 8850, 8851, 8852, 8853, 8854, 8855);
-UPDATE `quest_template` SET `PrevQuestId` = 0 WHERE `entry` IN (8811, 8811, 8812, 8812, 8813, 8813, 8814, 8814, 8815, 8815, 8816, 8816, 8817, 8817, 8818, 8818, 8819, 8819, 8820, 8820, 8821, 8821, 8822, 8822, 8823, 8823, 8824, 8824, 8825, 8825, 8826, 8826, 8830, 8831, 8832, 8833, 8834, 8835, 8836, 8837, 8838, 8839, 8840, 8841, 8842, 8843, 8844, 8845);
+UPDATE `quest_template` SET `PrevQuestId` = 0 WHERE PrevQuestId IN (8795);
 
 
 -- End of migration.
