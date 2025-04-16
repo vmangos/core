2b9ddb569050d19e5d439986c0880d314e924c9d
NickTyrer <nickyty8@gmail.com>
2025-04-16 16:32:26 +0100

    Update 20241208211202_world.sql

--- sql/migrations/20241208211202_world.sql
+++ sql/migrations/20241208211202_world.sql
@@ -684,6 +684,8 @@ DELETE FROM `gossip_scripts` WHERE `id`=5763;
 INSERT INTO `gossip_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
 (5763, 0, 0, 15, 22943, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Doc Mixilpixil - cast Player Lost Eau d\' Mixilpixil DND');
 
+-- For Future Ref: More Blizzlike Goodness From Cmangos https://github.com/cmangos/tbc-db/pull/1251
+
 
 -- End of migration.
 END IF;
