a5fb4147acb795dbdd6720d7a9b88d9ddf657184
NickTyrer <nickyty8@gmail.com>
2025-12-24 16:13:32 +0000

    Update 20251224091422_world.sql

--- sql/migrations/20251224091422_world.sql
+++ sql/migrations/20251224091422_world.sql
@@ -108,6 +108,30 @@ DELETE FROM `pool_template` WHERE `entry` = 1297;
 UPDATE `creature_questrelation` SET `patch_max` = 2 WHERE `id` = 14387 AND `quest` = 7487;
 UPDATE `creature_involvedrelation` SET `patch_max` = 2 WHERE `id` = 14387 AND `quest` = 7487;
 
+-- Daphne Stilwell 6182 (credit cmangos)
+-- custom made path corrected
+UPDATE `creature` SET `position_x` = -11482.304,  `position_y` = 1557.4106,  `position_z` = 48.70583,  `orientation` = 4.26775 WHERE `id` = 6182;
+DELETE FROM `creature_movement` WHERE `Id` = 66979;
+DELETE FROM `creature_movement_template` WHERE `entry` = 6182;
+INSERT INTO `creature_movement_template` (`entry`,  `point`,  `position_x`,  `position_y`,  `position_z`,  `orientation`,  `waittime`,  `script_id`) VALUES
+(6182, 1, -11482.304, 1557.4106, 48.70583, 100, 11000, 0), 
+(6182, 2, -11476.278, 1557.6038, 48.70583, 100, 0, 0), 
+(6182, 3, -11470.299, 1559.1139, 48.58083, 100, 2000, 0), 
+(6182, 4, -11470.382, 1563.5004, 48.58083, 100, 1000, 0), 
+(6182, 5, -11464.786, 1565.625, 48.846966, 100, 10000, 618201), 
+(6182, 6, -11465.664, 1567.7332, 48.7971, 100, 0, 0), 
+(6182, 7, -11465.914, 1569.9832, 48.7971, 100, 0, 0), 
+(6182, 8, -11465.041, 1570.8412, 48.747234, 100, 0, 0), 
+(6182, 9, -11465.041, 1570.8412, 48.747234, 6.21337, 10000, 618201), 
+(6182, 10, -11471.896, 1573.1904, 48.59549, 100, 0, 0), 
+(6182, 11, -11477.807, 1573.1018, 48.59549, 100, 0, 0), 
+(6182, 12, -11480.656, 1574.8416, 48.59549, 100, 10000, 618201), 
+(6182, 13, -11482.617, 1567.7522, 48.59549, 100, 0, 0), 
+(6182, 14, -11480.854, 1560.6074, 48.58083, 100, 0, 0);
+INSERT INTO `creature_movement_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
+(618201, 1, 0, 1, 69, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Daphne Stilwell - Emote'),
+(618201, 8, 0, 1, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Daphne Stilwell - Emote');
+
 
 -- End of migration.
 END IF;
