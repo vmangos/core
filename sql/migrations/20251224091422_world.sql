4b69f69a2398511d864b76eea08aaca1cb82b2ec
NickTyrer <nickyty8@gmail.com>
2026-01-08 08:35:49 +0000

    Update 20251224091422_world.sql

--- sql/migrations/20251224091422_world.sql
+++ sql/migrations/20251224091422_world.sql
@@ -12,6 +12,9 @@ INSERT INTO `migrations` VALUES ('20251224091422');
 -- Remove Duplicate Sharks
 DELETE FROM `creature` WHERE `guid` IN (88969, 88979, 88978, 88977, 88976, 88973, 88972, 88971, 88970);
 
+-- Frenzied Plaguehound (8598) should share spawn with Monstrous Plaguebat (8602)
+UPDATE `creature` SET `id` = 8598, `id2` = 8602 WHERE `id` IN (8598, 8602);
+
 -- Plaguehound (8597) should share spawn with Noxious Plaguebat (8601)
 UPDATE `creature` SET `id` = 8597, `id2` = 8601 WHERE `id` IN (8597, 8601);
 
