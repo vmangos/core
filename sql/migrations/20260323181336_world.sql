ea5d7ac61b4dbb39b0f2018e061f9a0af0ed3b7b
NickTyrer <nickyty8@gmail.com>
2026-03-26 16:16:15 +0000

    Update 20260323181336_world.sql

--- sql/migrations/20260323181336_world.sql
+++ sql/migrations/20260323181336_world.sql
@@ -14,7 +14,7 @@ DELETE FROM `pool_pool` WHERE `pool_id` IN (25617, 25616, 25613, 25615, 25614);
 UPDATE `pool_template` SET `description` = 'Truesilver Deposit / Small Thorium Vein - Hinterlands - Group 1' WHERE `entry` IN (25617, 25616, 25613, 25615, 25614);
 UPDATE `pool_gameobject` SET `description` = 'Truesilver Deposit / Small Thorium Vein - Hinterlands - Group 1' WHERE `pool_entry` IN (25617, 25616, 25613, 25615, 25614);
 INSERT INTO `pool_template` (`entry`, `max_limit`, `description`, `flags`, `instance`, `patch_min`, `patch_max`) VALUES
-(@PTEMPLATE+1, 1, 'Hinterlands - Group 1 - Truesilver Deposit / Small Thorium Vein', 0, 0, 0, 10);
+(@PTEMPLATE+1, 1, 'Truesilver Deposit / Small Thorium Vein - Hinterlands - Group 1 - Master', 0, 0, 0, 10);
 INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `flags`) VALUES
 (25617, @PTEMPLATE+1, 0, 'Hinterlands - Group 1 - Truesilver Deposit / Small Thorium Vein', 0),
 (25616, @PTEMPLATE+1, 0, 'Hinterlands - Group 1 - Truesilver Deposit / Small Thorium Vein', 0),
@@ -26,7 +26,7 @@ DELETE FROM `pool_pool` WHERE `pool_id` IN (25619, 3031, 25612, 25611);
 UPDATE `pool_template` SET `description` = 'Truesilver Deposit / Small Thorium Vein - Hinterlands - Group 2' WHERE `entry` IN (25619, 3031, 25612, 25611);
 UPDATE `pool_gameobject` SET `description` = 'Truesilver Deposit / Small Thorium Vein - Hinterlands - Group 2' WHERE `pool_entry` IN (25619, 3031, 25612, 25611);
 INSERT INTO `pool_template` (`entry`, `max_limit`, `description`, `flags`, `instance`, `patch_min`, `patch_max`) VALUES
-(@PTEMPLATE+2, 1, 'Hinterlands - Group 2 - Truesilver Deposit / Small Thorium Vein', 0, 0, 0, 10);
+(@PTEMPLATE+2, 1, 'Truesilver Deposit / Small Thorium Vein - Hinterlands - Group 2 - Master', 0, 0, 0, 10);
 INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `flags`) VALUES
 (25619, @PTEMPLATE+2, 0, 'Hinterlands - Group 2 - Truesilver Deposit / Small Thorium Vein', 0),
 (3031, @PTEMPLATE+2, 0, 'Hinterlands - Group 2 - Truesilver Deposit / Small Thorium Vein', 0),
@@ -37,7 +37,7 @@ DELETE FROM `pool_pool` WHERE `pool_id` IN (9440, 25618, 9441, 17206);
 UPDATE `pool_template` SET `description` = 'Truesilver Deposit / Small Thorium Vein - Hinterlands - Group 3' WHERE `entry` IN (9440, 25618, 9441, 17206);
 UPDATE `pool_gameobject` SET `description` = 'Truesilver Deposit / Small Thorium Vein - Hinterlands - Group 3' WHERE `pool_entry` IN (9440, 25618, 9441, 17206);
 INSERT INTO `pool_template` (`entry`, `max_limit`, `description`, `flags`, `instance`, `patch_min`, `patch_max`) VALUES
-(@PTEMPLATE+3, 1, 'Hinterlands - Group 3 - Truesilver Deposit / Small Thorium Vein', 0, 0, 0, 10);
+(@PTEMPLATE+3, 1, 'Truesilver Deposit / Small Thorium Vein - Hinterlands - Group 3 - Master', 0, 0, 0, 10);
 INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `flags`) VALUES
 (9440, @PTEMPLATE+3, 0, 'Hinterlands - Group 3 - Truesilver Deposit / Small Thorium Vein', 0),
 (25618, @PTEMPLATE+3, 0, 'Hinterlands - Group 3 - Truesilver Deposit / Small Thorium Vein', 0),
