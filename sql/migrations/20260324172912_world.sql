a43c07550122f89c844a97b6381431837dc6adbb
NickTyrer <nickyty8@gmail.com>
2026-03-26 16:19:40 +0000

    Update 20260324172912_world.sql

--- sql/migrations/20260324172912_world.sql
+++ sql/migrations/20260324172912_world.sql
@@ -14,7 +14,7 @@ DELETE FROM `pool_pool` WHERE `pool_id` IN (6113, 6128, 6127, 6134, 6131);
 UPDATE `pool_template` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Wetlands - Group 1' WHERE `entry` IN (6113, 6128, 6127, 6134, 6131);
 UPDATE `pool_gameobject` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Wetlands - Group 1' WHERE `pool_entry` IN (6113, 6128, 6127, 6134, 6131);
 INSERT INTO `pool_template` (`entry`, `max_limit`, `description`, `flags`, `instance`, `patch_min`, `patch_max`) VALUES
-(@PTEMPLATE+1, 1, 'Wetlands - Group 1 - Silver Vein / Gold Vein / Iron Deposit', 0, 0, 0, 10);
+(@PTEMPLATE+1, 1, 'Silver Vein / Gold Vein / Iron Deposit - Wetlands - Group 1 - Master', 0, 0, 0, 10);
 INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `flags`) VALUES
 (6113, @PTEMPLATE+1, 0, 'Wetlands - Group 1 - Silver Vein / Gold Vein / Iron Deposit', 0),
 (6128, @PTEMPLATE+1, 0, 'Wetlands - Group 1 - Silver Vein / Gold Vein / Iron Deposit', 0),
@@ -26,7 +26,7 @@ DELETE FROM `pool_pool` WHERE `pool_id` IN (6129, 6114, 6115, 6126, 6116);
 UPDATE `pool_template` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Wetlands - Group 2' WHERE `entry` IN (6129, 6114, 6115, 6126, 6116);
 UPDATE `pool_gameobject` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Wetlands - Group 2' WHERE `pool_entry` IN (6129, 6114, 6115, 6126, 6116);
 INSERT INTO `pool_template` (`entry`, `max_limit`, `description`, `flags`, `instance`, `patch_min`, `patch_max`) VALUES
-(@PTEMPLATE+2, 1, 'Wetlands - Group 2 - Silver Vein / Gold Vein / Iron Deposit', 0, 0, 0, 10);
+(@PTEMPLATE+2, 1, 'Silver Vein / Gold Vein / Iron Deposit - Wetlands - Group 2 - Master', 0, 0, 0, 10);
 INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `flags`) VALUES
 (6129, @PTEMPLATE+2, 0, 'Wetlands - Group 2 - Silver Vein / Gold Vein / Iron Deposit', 0),
 (6114, @PTEMPLATE+2, 0, 'Wetlands - Group 2 - Silver Vein / Gold Vein / Iron Deposit', 0),
@@ -38,7 +38,7 @@ DELETE FROM `pool_pool` WHERE `pool_id` IN (6117, 2820, 6118, 6119, 6132, 6120);
 UPDATE `pool_template` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Wetlands - Group 3' WHERE `entry` IN (6117, 2820, 6118, 6119, 6132, 6120);
 UPDATE `pool_gameobject` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Wetlands - Group 3' WHERE `pool_entry` IN (6117, 2820, 6118, 6119, 6132, 6120);
 INSERT INTO `pool_template` (`entry`, `max_limit`, `description`, `flags`, `instance`, `patch_min`, `patch_max`) VALUES
-(@PTEMPLATE+3, 1, 'Wetlands - Group 3 - Silver Vein / Gold Vein / Iron Deposit', 0, 0, 0, 10);
+(@PTEMPLATE+3, 1, 'Silver Vein / Gold Vein / Iron Deposit - Wetlands - Group 3 - Master', 0, 0, 0, 10);
 INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `flags`) VALUES
 (6117, @PTEMPLATE+3, 0, 'Wetlands - Group 3 - Silver Vein / Gold Vein / Iron Deposit', 0),
 (2820, @PTEMPLATE+3, 0, 'Wetlands - Group 3 - Silver Vein / Gold Vein / Iron Deposit', 0),
@@ -51,7 +51,7 @@ DELETE FROM `pool_pool` WHERE `pool_id` IN (6133, 6130, 6121, 6122, 6123, 17025)
 UPDATE `pool_template` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Wetlands - Group 4' WHERE `entry` IN (6133, 6130, 6121, 6122, 6123, 17025);
 UPDATE `pool_gameobject` SET `description` = 'Silver Vein / Gold Vein / Iron Deposit - Wetlands - Group 4' WHERE `pool_entry` IN (6133, 6130, 6121, 6122, 6123, 17025);
 INSERT INTO `pool_template` (`entry`, `max_limit`, `description`, `flags`, `instance`, `patch_min`, `patch_max`) VALUES
-(@PTEMPLATE+4, 1, 'Wetlands - Group 4 - Silver Vein / Gold Vein / Iron Deposit', 0, 0, 0, 10);
+(@PTEMPLATE+4, 1, 'Silver Vein / Gold Vein / Iron Deposit - Wetlands - Group 4 - Master', 0, 0, 0, 10);
 INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `flags`) VALUES
 (6133, @PTEMPLATE+4, 0, 'Wetlands - Group 4 - Silver Vein / Gold Vein / Iron Deposit', 0),
 (6130, @PTEMPLATE+4, 0, 'Wetlands - Group 4 - Silver Vein / Gold Vein / Iron Deposit', 0),
