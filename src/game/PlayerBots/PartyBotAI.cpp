9285372c47210f8d5d2e6d5d5ccebb03c621cf2f
IamTirion <50409752+IamTirion@users.noreply.github.com>
2026-03-29 04:58:47 +0800

    Correcting rage thresholds in checks
    The actual rage value in the code is actually 10 times the displayed value in game. You can see that here
    https://github.com/vmangos/core/blob/25b1c2ad22311b1965d225976d286b2b3bc237bb/src/game/Objects/Unit.cpp#L8461
    
    case POWER_RAGE:
                return 1000;

--- src/game/PlayerBots/PartyBotAI.cpp
+++ src/game/PlayerBots/PartyBotAI.cpp
@@ -2431,7 +2431,7 @@ void PartyBotAI::UpdateOutOfCombatAI_Warrior()
         if (CanTryToCastSpell(me, m_spells.warrior.pBattleShout))
             DoCastSpell(me, m_spells.warrior.pBattleShout);
         else if (m_spells.warrior.pBloodrage &&
-            (me->GetPower(POWER_RAGE) < 10) &&
+            (me->GetPower(POWER_RAGE) < 100) &&
             CanTryToCastSpell(me, m_spells.warrior.pBloodrage))
         {
             DoCastSpell(me, m_spells.warrior.pBloodrage);
@@ -2677,7 +2677,7 @@ void PartyBotAI::UpdateInCombatAI_Warrior()
             me->GetMotionMaster()->MoveChase(pVictim);
         }
 
-        if (me->GetPower(POWER_RAGE) > 30)
+        if (me->GetPower(POWER_RAGE) > 300)
         {
             if (m_spells.warrior.pCleave && me->GetEnemyCountInRadiusAround(pVictim, 8.0f) > 1)
             {
@@ -3309,7 +3309,7 @@ void PartyBotAI::UpdateInCombatAI_Druid()
                     return;
             }
 
-            if ((me->GetPower(POWER_RAGE) > 80) ||
+            if ((me->GetPower(POWER_RAGE) > 800) ||
                 (GetAttackersInRangeCount(10.0f) > 1))
             {
                 if (m_spells.druid.pDemoralizingRoar &&
