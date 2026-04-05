a0b57c2e26ea964c35de1641c863e55595b4a232
IamTirion <50409752+IamTirion@users.noreply.github.com>
2026-04-05 23:38:53 +0800

    Use == instead

--- src/game/PlayerBots/CombatBotBaseAI.cpp
+++ src/game/PlayerBots/CombatBotBaseAI.cpp
@@ -1065,8 +1065,7 @@ void CombatBotBaseAI::PopulateSpellData()
                     if (IsHigherRankSpell(m_spells.warrior.pDefensiveStance))
                         m_spells.warrior.pDefensiveStance = pSpellEntry;
                 }
-                else if (pSpellEntry->SpellName[0].find("Charge") != std::string::npos &&
-                         pSpellEntry->SpellFamilyName == SPELLFAMILY_WARRIOR)
+                else if (pSpellEntry->SpellName[0] == "Charge")
                 {
                     if (IsHigherRankSpell(m_spells.warrior.pCharge))
                         m_spells.warrior.pCharge = pSpellEntry;
