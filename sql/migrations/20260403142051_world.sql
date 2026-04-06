2605fce8261e3f550f51c68b6987a400628da19e
NickTyrer <nickyty8@gmail.com>
2026-04-06 09:57:35 +0100

    Update 20260403142051_world.sql

--- sql/migrations/20260403142051_world.sql
+++ sql/migrations/20260403142051_world.sql
@@ -8,7 +8,7 @@ IF v = 0 THEN
 INSERT INTO `migrations` VALUES ('20260403142051');
 -- Add your query below.
 
-SET @PTEMPLATE = 4348;
+SET @PTEMPLATE = 293;
 
 DELETE FROM `pool_pool` WHERE `pool_id` IN (990, 989, 988, 995, 987, 986);
 UPDATE `pool_template` SET `description` = 'Gold Vein / Mithril Deposit / Truesilver Deposit - Eastern Plaguelands - Group 1' WHERE `entry` IN (990, 989, 988, 995, 987, 986);
