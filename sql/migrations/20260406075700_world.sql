30ccd93b83e9606c4e66aed72fe7ead49b3dc099
NickTyrer <nickyty8@gmail.com>
2026-04-06 11:21:43 +0100

    Update 20260406075700_world.sql

--- sql/migrations/20260406075700_world.sql
+++ sql/migrations/20260406075700_world.sql
@@ -10,7 +10,7 @@ INSERT INTO `migrations` VALUES ('20260406075700');
 
 -- Wetlands
 SET @GGUID = 246;
-SET @PTEMPLATE = 3007;
+SET @PTEMPLATE = 298;
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`, `spawn_flags`, `visibility_mod`, `patch_min`, `patch_max`) VALUES
 (@GGUID+0, 1732, 0, -2596.69970703125, -1893.5811767578125, 20.57378578186035156, 1.099556446075439453, 0, 0, 0.522498130798339843, 0.852640450000762939, 300, 900, 100, 1, 0, 0, 0, 10),
 (@GGUID+1, 1733, 0, -2596.69970703125, -1893.5811767578125, 20.57378578186035156, 1.099556446075439453, 0, 0, 0.522498130798339843, 0.852640450000762939, 300, 900, 100, 1, 0, 0, 0, 10);
@@ -24,7 +24,7 @@ INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `fla
 
 -- Silverpine Forest
 SET @GGUID = 4009;
-SET @PTEMPLATE = 298;
+SET @PTEMPLATE = 325;
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`, `spawn_flags`, `visibility_mod`, `patch_min`, `patch_max`) VALUES
 (@GGUID+0, 1732, 0, -444.3253173828125, 1074.275390625, 92.75608062744140625, 5.218535900115966796, 0, 0, -0.507537841796875, 0.861629426479339599, 300, 900, 100, 1, 0, 0, 0, 10),
 (@GGUID+1, 1733, 0, -444.3253173828125, 1074.275390625, 92.75608062744140625, 5.218535900115966796, 0, 0, -0.507537841796875, 0.861629426479339599, 300, 900, 100, 1, 0, 0, 0, 10);
@@ -38,7 +38,7 @@ INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `fla
 
 -- Darkshore
 SET @GGUID = 4011;
-SET @PTEMPLATE = 325;
+SET @PTEMPLATE = 402;
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`, `spawn_flags`, `visibility_mod`, `patch_min`, `patch_max`) VALUES
 (@GGUID+0, 1732, 1, 7420.14501953125, -789.52685546875, 16.9228973388671875, 5.637413978576660156, 0, 0, -0.31730461120605468, 0.948323667049407958, 300, 900, 100, 1, 0, 0, 0, 10),
 (@GGUID+1, 1733, 1, 7420.14501953125, -789.52685546875, 16.9228973388671875, 5.637413978576660156, 0, 0, -0.31730461120605468, 0.948323667049407958, 300, 900, 100, 1, 0, 0, 0, 10);
@@ -53,7 +53,7 @@ UPDATE `gameobject` SET `position_x` = 7968.3974609375, `position_y` = -1087.410
 
 -- Windshear Mine
 SET @GGUID = 4013;
-SET @PTEMPLATE = 402;
+SET @PTEMPLATE = 406;
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`, `spawn_flags`, `visibility_mod`, `patch_min`, `patch_max`) VALUES
 (@GGUID+0, 1732, 1, 954.4083251953125, -352.142669677734375, 5.487726211547851562, 3.94444584846496582, 0, 0, -0.92050457000732421, 0.3907318115234375, 300, 900, 100, 1, 0, 0, 0, 10),
 (@GGUID+1, 1733, 1, 954.4083251953125, -352.142669677734375, 5.487726211547851562, 3.94444584846496582, 0, 0, -0.92050457000732421, 0.3907318115234375, 300, 900, 100, 1, 0, 0, 0, 10);
@@ -66,7 +66,7 @@ INSERT INTO `pool_pool` (`pool_id`, `mother_pool`, `chance`, `description`, `fla
 (@PTEMPLATE+0, 386, 0, 'Stonetalon Mountains - Windshear Mine - Tin Vein / Silver Vein', 0);
 
 SET @GGUID = 4015;
-SET @PTEMPLATE = 406;
+SET @PTEMPLATE = 412;
 INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`, `spawn_flags`, `visibility_mod`, `patch_min`, `patch_max`) VALUES
 (@GGUID+0, 1732, 1, 962.618408203125, -270.985687255859375, -1.75297904014587402, 1.134462952613830566, 0, 0, 0.537299156188964843, 0.843391716480255126, 300, 900, 100, 1, 0, 0, 0, 10),
 (@GGUID+1, 1733, 1, 962.618408203125, -270.985687255859375, -1.75297904014587402, 1.134462952613830566, 0, 0, 0.537299156188964843, 0.843391716480255126, 300, 900, 100, 1, 0, 0, 0, 10);
