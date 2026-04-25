07cd56ff1af16ea00cac9679fbd29e21d19c37ea
schell244 <schell244@web.de>
2026-04-25 18:33:15 +0200

    fix copy paste

--- src/shared/Crypto/ConstantTimeCompare.cpp
+++ src/shared/Crypto/ConstantTimeCompare.cpp
@@ -1,6 +1,4 @@
 /*
- * This file is part of the CMaNGOS Project. See AUTHORS file for Copyright information
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
