4c426172340e59ab59719b2b27f79394e00831bf
schell244 <schell244@web.de>
2026-06-12 08:54:14 +0200

    Add Naxx Teleporter trap.

--- sql/migrations/20260609162206_world.sql
+++ sql/migrations/20260609162206_world.sql
@@ -99,6 +99,9 @@ UPDATE `gameobject_template` SET `data11` = 1, `data12` = 129 WHERE `entry`=1815
 -- Naxxramas Portal
 UPDATE `gameobject_template` SET `data11` = 1, `data12` = 129 WHERE `entry`=181577;
 
+-- Naxx Teleporter trap
+INSERT INTO `gameobject_template` (`entry`, `type`, `displayid`, `name`, `size`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`) VALUES (129, 6, 0, 'Naxx Teleporter trap', 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
 -- Forge
 UPDATE `gameobject_template` SET `displayid` = 233 WHERE `entry`=144133;
 
