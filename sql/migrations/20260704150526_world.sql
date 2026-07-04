a6861b7f3c6ebc9408c3f9821605db143e5c7918
daobashun <fangshun2004@msn.com>
2026-07-04 21:41:44 +0800

    sql Modification Time

--- sql/migrations/20260704150526_world.sql
+++ sql/migrations/20260704150526_world.sql
@@ -3,9 +3,9 @@ DELIMITER ??
 CREATE PROCEDURE `add_migration`()
 BEGIN
 DECLARE v INT DEFAULT 1;
-SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20260630150526');
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20260704150526');
 IF v = 0 THEN
-INSERT INTO `migrations` VALUES ('20260630150526');
+INSERT INTO `migrations` VALUES ('20260704150526');
 -- Add your query below.
 
 UPDATE `spell_template` SET `script_name`='spell_forgiveness_dummy' WHERE `entry`=28697;
